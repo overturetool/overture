329ecb0f882def842573861e2f6893a5e5d63837
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-03-17 22:19:56 +0000

    vdm tools script
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@164 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- code/traces/src/main/resources/vdmUtility/UpdateVDMToolsCmd.bat
+++ code/traces/src/main/resources/vdmUtility/UpdateVDMToolsCmd.bat
@@ -2,4 +2,4 @@ TITLE Updating TestCase
 echo off
 cls
 echo Updating
-java -classpath "lib\VDM.jar;lib\org.overturetool.parser.jar;overtureUtility.jar;lib\org.overturetool.umltrans-1.0.0.jar" MainClass -vdmCmdEx -project "c:\tracesSpec\src\overtureTraces\src\main\vpp\CT.prj" -outputDir ..\VDMToolsCmdModel
+java -classpath "lib\StdLib-1.0.0.jar;lib\parser-1.0.0.jar;overtureUtility.jar;lib\umltrans-1.0.0.jar;lib\ast-1.0.0.jar" MainClass -vdmCmdEx -project "..\..\java\CT_vdmtools_project.prj" -outputDir "vdmt"
