01ad741e23e7d39dd1bcf78ba9487c1d7dbd097a
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-07 07:28:18 +0000

    PLUGIN_ID corrected with correct package
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@361 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- org.overturetool.eclipse/org.overturetool.eclipse.plugins/org.overturetool.eclipse.plugins.traces/src/main/java/org/overturetool/eclipse/plugins/traces/OvertureTracesPlugin.java
+++ org.overturetool.eclipse/org.overturetool.eclipse.plugins/org.overturetool.eclipse.plugins.traces/src/main/java/org/overturetool/eclipse/plugins/traces/OvertureTracesPlugin.java
@@ -11,7 +11,7 @@ import org.osgi.framework.BundleContext;
 public class OvertureTracesPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.overturetool.eclipse.traces";
+	public static final String PLUGIN_ID = "org.overturetool.eclipse.plugins.traces";
 
 	// The shared instance
 	private static OvertureTracesPlugin plugin;
