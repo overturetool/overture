334bb13c02037e6057c5344717b2a6fa20c0870a
mver <mver@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-08 10:42:14 +0000

    Added lex and yacc sources for the overture parser.
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@389 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/parser/src/main/java/org/overturetool/parser/imp/doit_yacc.bat
@@ -0,0 +1,3 @@
+del OvertureParser*.java y
+REM d:\dev\byaccj\yacc.exe -vd -Jclass=OvertureParser -Jpackage=org.overturetool.parser.imp -Jnorun -Jnoconstruct -Jnodebug -Jsemantic=OvertureToken parser.y
+d:\msvcproj\byaccj1.13_src\byaccj\Debug\byaccj.exe -d -Jencode -Jclass=OvertureParser -Jpackage=org.overturetool.parser.imp -Jthrows=jp.co.csk.vdm.toolbox.VDM.CGException -Jnorun -Jnoconstruct -Jsemantic=Object parser.y
