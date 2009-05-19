5fe42e8eb97e924e39efca503aae3ebe67d1bf18
dhmoeller <dhmoeller@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-19 11:23:37 +0000

    build interpreter now selected by interpreter id
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@461 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- org.overturetool.eclipse/org.overturetool.eclipse.plugins/org.overturetool.eclipse.plugins.editor.core/src/main/java/org/overturetool/eclipse/plugins/editor/core/internal/builder/OvertureBuilder.java
+++ org.overturetool.eclipse/org.overturetool.eclipse.plugins/org.overturetool.eclipse.plugins.editor.core/src/main/java/org/overturetool/eclipse/plugins/editor/core/internal/builder/OvertureBuilder.java
@@ -10,6 +10,7 @@ import org.eclipse.dltk.core.IScriptProject;
 import org.eclipse.dltk.core.builder.IScriptBuilder;
 import org.eclipse.dltk.launching.IInterpreterInstall;
 import org.eclipse.dltk.launching.ScriptRuntime;
+import org.overturetool.eclipse.plugins.editor.core.OvertureConstants;
 import org.overturetool.eclipse.plugins.editor.core.internal.parser.OvertureSourceParserFactory.Dialect;
 import org.overturetool.eclipse.plugins.editor.core.internal.parser.OvertureSourceParserFactory.ToolType;
 //
@@ -26,9 +27,9 @@ public class OvertureBuilder implements IScriptBuilder {
 		try {
 			interpreterInstall = ScriptRuntime.getInterpreterInstall(project);
 			interpreterInstall.getInstallLocation();
-			if (interpreterInstall.getInterpreterInstallType().getName().equals("VDMJ")) {
+			if (interpreterInstall.getInterpreterInstallType().getId().equals(OvertureConstants.VDMJ_INTERPRETER_ID)) {
 				toolType = ToolType.VDMJ;
-			} else if (interpreterInstall.getInterpreterInstallType().getName().equals("VDMTools")) {
+			} else if (interpreterInstall.getInterpreterInstallType().getId().equals(OvertureConstants.VDMTOOLS_INTERPRETER_ID)) {
 				toolType = ToolType.VDMTools;
 			}
 
