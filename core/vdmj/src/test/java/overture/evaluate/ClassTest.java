4126b18b6905df070d0924db1b00d341c25d105b
nick_battle <nick_battle@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-19 09:18:36 +0000

    Added a JUnit test framework for Overture tests in VDMJ.
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@450 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/vdmj/src/test/java/overture/evaluate/ClassTest.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2008 Fujitsu Services Ltd.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package overture.evaluate;
+
+import overture.OvertureTest;
+
+public class ClassTest extends OvertureTest
+{
+	public void test_Evaluate1() throws Exception
+	{
+		evaluate("evaluate1", ResultType.TRUE);
+	}
+
+	public void test_Evaluate2() throws Exception
+	{
+		evaluate("evaluate2", ResultType.VOID);
+	}
+}
