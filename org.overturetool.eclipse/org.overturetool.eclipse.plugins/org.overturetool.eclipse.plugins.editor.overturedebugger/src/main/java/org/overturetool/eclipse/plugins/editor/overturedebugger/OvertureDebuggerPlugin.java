5e84f58097bafbf1a25bc2d6f8d518b4eae78516
dhmoeller <dhmoeller@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-22 14:26:34 +0000

    unused preferences, constant and other stuff
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@492 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- org.overturetool.eclipse/org.overturetool.eclipse.plugins/org.overturetool.eclipse.plugins.editor.overturedebugger/src/main/java/org/overturetool/eclipse/plugins/editor/overturedebugger/OvertureDebuggerPlugin.java
+++ org.overturetool.eclipse/org.overturetool.eclipse.plugins/org.overturetool.eclipse.plugins.editor.overturedebugger/src/main/java/org/overturetool/eclipse/plugins/editor/overturedebugger/OvertureDebuggerPlugin.java
@@ -10,16 +10,12 @@ import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IThread;
-import org.eclipse.dltk.dbgp.commands.IDbgpPropertyCommands;
-import org.eclipse.dltk.dbgp.exceptions.DbgpException;
-import org.eclipse.dltk.debug.core.DLTKDebugPlugin;
 import org.eclipse.dltk.debug.core.model.IScriptThread;
 import org.eclipse.dltk.debug.ui.DLTKDebugUIPlugin;
 import org.eclipse.dltk.launching.DebuggingEngineRunner;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 import org.overturetool.eclipse.debug.internal.debug.OvertureDebugConstants;
-import org.overturetool.eclipse.plugins.editor.overturedebugger.preferences.IOvertureDebuggerPreferenceConstants;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -27,10 +23,7 @@ import org.overturetool.eclipse.plugins.editor.overturedebugger.preferences.IOve
 public class OvertureDebuggerPlugin extends AbstractUIPlugin implements
 		IDebugEventSetListener, IPropertyChangeListener {
 
-	private static final String SUSPEND_ON_ENTRY = "suspendOnEntry"; //$NON-NLS-1$
-	private static final String SUSPEND_ON_EXIT = "suspendOnExit"; //$NON-NLS-1$
-	private static final String SUSPEND_ON_EXCEPTION = "suspendOnException"; //$NON-NLS-1$
-
+	
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.overturetool.overturedebugger"; //$NON-NLS-1$
 
@@ -49,39 +42,18 @@ public class OvertureDebuggerPlugin extends AbstractUIPlugin implements
 	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		super.start(context);	
+		//super.start(context);	
 		plugin = this;
 		
-		Preferences prefs = getPluginPreferences();		
-		resetSuspendProperties(prefs);
+		//Preferences prefs = getPluginPreferences();	
+		
 
-		prefs.addPropertyChangeListener(this);
-		DebugPlugin.getDefault().addDebugEventListener(this);	
+		//prefs.addPropertyChangeListener(this);
+		//DebugPlugin.getDefault().addDebugEventListener(this);	
 		
 	}
 
-	private void resetSuspendProperties(Preferences prefs) {
-		// TODO this shouldnt be done or needed to be done!!
-		// These preferences should be remembered but whatever i try its
-		// impossible (ToggleStates, AbstractHandlerWithState, Action that is
-		// pulldown)
-//		prefs
-//		.setValue(
-//				SETUP_FILES,
-//				"hass");
-//		prefs
-//				.setValue(
-//						IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_METHOD_ENTRY,
-//						false);
-//		prefs
-//				.setValue(
-//						IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_METHOD_EXIT,
-//						false);
-//		prefs
-//				.setValue(
-//						IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_EXCEPTION,
-//						false);
-	}
+	
 
 	/*
 	 * (non-Javadoc)
@@ -147,44 +119,7 @@ public class OvertureDebuggerPlugin extends AbstractUIPlugin implements
 //		}		
 //	}
 //	
-	private void setupBreakOnMethodEntry(IScriptThread thread) {
-		IDbgpPropertyCommands propCmds = thread.getDbgpSession()
-				.getCoreCommands();
-		try {
-			Preferences prefs = getPluginPreferences();
-			String suspendOnEntry = prefs.getString(IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_METHOD_ENTRY);
-			propCmds.setProperty(SUSPEND_ON_ENTRY, -1, suspendOnEntry);
-		} catch (DbgpException e) {
-			DLTKDebugPlugin.log(e);
-		}
-	}
-
-	private void setupBreakOnMethodExit(IScriptThread thread) {
-		IDbgpPropertyCommands propCmds = thread.getDbgpSession()
-				.getCoreCommands();
-		try {
-			Preferences prefs = getPluginPreferences();
-			String suspendOnExit = prefs
-					.getString(IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_METHOD_EXIT);
-			propCmds.setProperty(SUSPEND_ON_EXIT, -1, suspendOnExit);
-		} catch (DbgpException e) {
-			DLTKDebugPlugin.log(e);
-		}
-	}
-
-	private void setupBreakOnException(IScriptThread thread) {
-		IDbgpPropertyCommands propCmds = thread.getDbgpSession()
-				.getCoreCommands();
-		try {
-			Preferences prefs = getPluginPreferences();
-			String suspendOnException = prefs
-					.getString(IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_EXCEPTION);
-			propCmds.setProperty(SUSPEND_ON_EXCEPTION, -1, suspendOnException);
-		} catch (DbgpException e) {
-			System.out.println("setupBreakOnException " + e.getMessage());
-			DLTKDebugPlugin.log(e);
-		}
-	}
+	
 
 	private boolean isThisDebuggerLaunch(ILaunch launch) {
 		String engineId = launch.getAttribute(DebuggingEngineRunner.LAUNCH_ATTR_DEBUGGING_ENGINE_ID);
@@ -195,28 +130,7 @@ public class OvertureDebuggerPlugin extends AbstractUIPlugin implements
 	}
 
 	public void propertyChange(PropertyChangeEvent event) {
-		if (IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_METHOD_ENTRY
-				.equals(event.getProperty())) {
-			runForEachDebuggerThread(new IDebuggerThreadVisitor() {
-				public void visit(IScriptThread thread) {
-					setupBreakOnMethodEntry(thread);
-				}
-			});
-		} else if (IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_METHOD_EXIT
-				.equals(event.getProperty())) {
-			runForEachDebuggerThread(new IDebuggerThreadVisitor() {
-				public void visit(IScriptThread thread) {
-					setupBreakOnMethodExit(thread);
-				}
-			});
-		} else if (IOvertureDebuggerPreferenceConstants.PREF_BREAK_ON_EXCEPTION
-				.equals(event.getProperty())) {
-			runForEachDebuggerThread(new IDebuggerThreadVisitor() {
-				public void visit(IScriptThread thread) {
-					setupBreakOnException(thread);
-				}
-			});
-		}
+		
 	}
 
 	private interface IDebuggerThreadVisitor {
