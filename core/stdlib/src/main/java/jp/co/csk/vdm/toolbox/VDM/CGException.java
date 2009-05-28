1fdc6540ee6cebbb6e05a4e077c96250199fc862
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-28 23:33:30 +0000

    CGException moved from class file to java => Removes all the read lines in Eclipse
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@541 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/stdlib/src/main/java/jp/co/csk/vdm/toolbox/VDM/CGException.java
@@ -0,0 +1,10 @@
+package jp.co.csk.vdm.toolbox.VDM;
+public class CGException extends Exception {
+	public CGException() {
+		super();
+	}
+
+	public CGException(String message) {
+		super(message);
+	}
+}
