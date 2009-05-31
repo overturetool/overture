635b0db8ca80430e18a6781a243f3f773aa63a64
pglvdm <pglvdm@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-05-31 19:56:13 +0000

    Check CT in VDMJ
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@551 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/vdmj/src/test/java/junit/overture/evaluate/CTTest.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2009 IHA
+ *
+ *	Author: Peter Gorm Larsen
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package junit.overture.evaluate;
+
+import junit.overture.OvertureTest;
+
+public class CTTest extends OvertureTest
+{
+	public void test_Eval() throws Exception
+	{
+		evaluate("VDM++\traces\tracesbind\tracesbind-01", ResultType.TRUE);
+		evaluate("VDM++\traces\tracesbind\tracesbind-02", ResultType.TRUE);
+
+
+
+	}
+}
