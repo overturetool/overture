404a8b0a514854c06ab5b691ade17026dba1ba64
nick_battle <nick_battle@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-06-02 15:20:06 +0000

    Tidy up the CT test suite.
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@561 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/vdmj/src/test/java/junit/overture/combtest/TraceBindTest.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2009 IHA
+ *
+ *	Author: Peter Gorm Larsen
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package junit.overture.combtest;
+
+import junit.overture.OvertureTest;
+
+public class TraceBindTest extends OvertureTest
+{
+	public void test_Traces1() throws Exception
+	{
+		combtest("traces/tracesbind/tracebind-01", "new UseStack().PushBeforePop()");
+	}
+
+	public void test_Traces2() throws Exception
+	{
+		combtest("traces/tracesbind/tracebind-02", "new UseStack().PushBeforePop()");
+	}
+}
