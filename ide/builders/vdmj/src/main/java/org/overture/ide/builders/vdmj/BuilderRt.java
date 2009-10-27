2e634e0fb494a2a856096f124182b53d17b6aac3
thillemann <thillemann@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2009-10-27 11:21:08 +0000

    Adding CPUClassDefinition and BUSClassDefinition
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@1095 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/builders/vdmj/src/main/java/org/overture/ide/builders/vdmj/BuilderRt.java
+++ ide/builders/vdmj/src/main/java/org/overture/ide/builders/vdmj/BuilderRt.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IStatus;
+import org.overturetool.vdmj.definitions.BUSClassDefinition;
+import org.overturetool.vdmj.definitions.CPUClassDefinition;
 import org.overturetool.vdmj.definitions.ClassDefinition;
 import org.overturetool.vdmj.definitions.ClassList;
+import org.overturetool.vdmj.lex.LexException;
+import org.overturetool.vdmj.syntax.ParserException;
 import org.overture.ide.vdmrt.core.*;
 /***
  * VDM RT builder
@@ -26,12 +30,20 @@ public class BuilderRt extends VdmjBuilder {
 
 		for (Object classDefinition : modelElements) {
 			if (classDefinition instanceof ClassDefinition)
+			{
 				modules.add((ClassDefinition) classDefinition);
+			}
+		}
+		try {
+			modules.add(new CPUClassDefinition());
+			modules.add(new BUSClassDefinition());
+		} catch (ParserException e) {
+			e.printStackTrace();
+		} catch (LexException e) {
+			e.printStackTrace();
 		}
 		IEclipseVdmj eclipseType = new EclipseVdmjRt(modules);
-
 		return buileModelElements(project, eclipseType);
-
 	}
 
 	@Override
