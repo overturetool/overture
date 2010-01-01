8dd44ce5afd449be9fc3bd5a794cd03fc49f89ac
nick_battle <nick_battle@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2010-01-01 17:25:15 +0000

    Include "let" clauses in context of trivial PO matches
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@1413 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- core/vdmj/src/main/java/org/overturetool/vdmj/pog/POTrivialProof.java
+++ core/vdmj/src/main/java/org/overturetool/vdmj/pog/POTrivialProof.java
@@ -25,9 +25,9 @@ package org.overturetool.vdmj.pog;
 
 public enum POTrivialProof
 {
-	FORALL_IN_SET("^( *\\(+forall [^\\n]+\\n)*? *\\(forall (\\w+) in set \\(([^&]+)\\) &(.+?)?\\n *\\2 in set \\3\\)+\\n$", "forall x in set s & x in set s"),
-	IMPLICATION("^( *\\(+forall [^\\n]+\\n?)*? *\\(+(\\w+) in set \\(([^&]+)\\)+ =>\\n *\\2 in set \\3\\)+\\n$", "x in set s => x in set s"),
-	NOT_EQUALITY("^( *\\(+forall [^\\n]+\\n)*? *\\(+not \\((.+?) \\= (.+?)\\) =>\\n *\\2 \\<\\> \\3\\)+\\n$", "not x = y => x <> y");
+	FORALL_IN_SET("^( *\\(+(forall|let)[^\\n]+\\n)*? *\\(forall (\\w+) in set \\(([^&]+)\\) &(.+?)?\\n *\\3 in set \\4\\)+\\n$", "forall x in set s & x in set s"),
+	IMPLICATION("^( *\\(+(forall|let)[^\\n]+\\n?)*? *\\(+(\\w+) in set \\(([^&]+)\\)+ =>\\n *\\3 in set \\4\\)+\\n$", "x in set s => x in set s"),
+	NOT_EQUALITY("^( *\\(+(forall|let)[^\\n]+\\n)*? *\\(+not \\((.+?) \\= (.+?)\\) =>\\n *\\3 \\<\\> \\4\\)+\\n$", "not x = y => x <> y");
 
 	private String pattern;
 	public String name;
