86b259f9fc6fed3f9dd9edc4b834189fab248ca9
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2010-02-15 16:01:53 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@1630 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/ui/src/main/java/org/overture/ide/ui/folding/VdmConstants.java
+++
@@ -1,8 +0,0 @@
-package org.overture.ide.ui.folding;
-
-public class VdmConstants {
-	
-		public final static String VDM_PARTITIONING = "__vdm_partitioning";
-	
-
-}
--- ide/ui/src/main/java/org/overture/ide/ui/scriptcolor/provider/IScriptColorTokenLocator.java
+++
@@ -1,8 +0,0 @@
-package org.overture.ide.ui.scriptcolor.provider;
-
-import org.eclipse.jface.text.rules.IToken;
-
-public interface IScriptColorTokenLocator
-{
-	public IToken getColorToken(String key);
-}
