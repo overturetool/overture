e3ee80c49878ce6b0fb7ee99c380539849e359a0
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2010-04-12 14:59:42 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@1955 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/vdmpp/ui/src/main/java/org/overture/ide/vdmpp/ui/editor/core/VdmPpSourceViewerConfiguration.java
+++ ide/vdmpp/ui/src/main/java/org/overture/ide/vdmpp/ui/editor/core/VdmPpSourceViewerConfiguration.java
@@ -5,6 +5,8 @@ import org.overture.ide.ui.editor.core.VdmSourceViewerConfiguration;
 import org.overture.ide.ui.editor.syntax.VdmColorProvider;
 import org.overture.ide.vdmpp.ui.editor.syntax.VdmPpCodeScanner;
 
+
+
 public class VdmPpSourceViewerConfiguration extends
 		VdmSourceViewerConfiguration {
 
