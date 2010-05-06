6d81642ec03dd31f114b1dacb5bc5f92b3217f93
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2010-05-06 15:19:21 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@2062 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/ui/src/main/java/org/overture/ide/ui/internal/util/ConsoleWriter.java
+++ ide/ui/src/main/java/org/overture/ide/ui/internal/util/ConsoleWriter.java
@@ -3,16 +3,26 @@ package org.overture.ide.ui.internal.util;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleConstants;
 import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IConsoleView;
 import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.ui.console.MessageConsoleStream;
+import org.eclipse.ui.progress.UIJob;
 import org.overture.ide.ui.VdmUIPlugin;
 
-public class ConsoleWriter extends PrintWriter
-{
+public class ConsoleWriter extends PrintWriter {
 	private String consoleName = "Overture";
 
 	public ConsoleWriter() {
@@ -25,68 +35,84 @@ public class ConsoleWriter extends PrintWriter
 	}
 
 	@Override
-	public void println(String x)
-	{
+	public void println(String x) {
 		ConsolePrint(x);
 	}
 
-	public void Show()
-	{
+	public void Show() {
 		MessageConsole myConsole = findConsole(consoleName);
 		myConsole.activate();
+
+		ConsolePlugin.getDefault().getConsoleManager().showConsoleView(
+				myConsole);
+
+		UIJob uiJob = new UIJob("Console pin") {
+
+			@Override
+			public IStatus runInUIThread(IProgressMonitor monitor) {
+				IWorkbench w = PlatformUI.getWorkbench();
+				IWorkbenchWindow[] wws = w.getWorkbenchWindows();
+				String id = IConsoleConstants.ID_CONSOLE_VIEW;
+				for (IWorkbenchWindow iWorkbenchWindow : wws) {
+					try {
+
+						IWorkbenchPage page = iWorkbenchWindow.getActivePage();
+						IConsoleView view = (IConsoleView) page.showView(id);
+//						view.display(myConsole);
+						view.setPinned(true);
+						
+					} catch (PartInitException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				return new Status(IStatus.OK, "pluginId", "OK");
+			}
+		};
+		uiJob.schedule();
+
 	}
 
-	public void ConsolePrint(final String message)
-	{
+	public void ConsolePrint(final String message) {
 		getDisplay().asyncExec(new Runnable() {
 
-			public void run()
-			{
-				try
-				{
+			public void run() {
+				try {
 					MessageConsole myConsole = findConsole(consoleName);
 					MessageConsoleStream out = myConsole.newMessageStream();
 					out.println(message);
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 
 	}
-	
-	public void clear(){
+
+	public void clear() {
 		getDisplay().asyncExec(new Runnable() {
 
-			public void run()
-			{
-				try
-				{
+			public void run() {
+				try {
 					MessageConsole myConsole = findConsole(consoleName);
 					myConsole.clearConsole();
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
 
-	public void ConsolePrint(final String message, final int color)
-	{
+	public void ConsolePrint(final String message, final int color) {
 		getDisplay().asyncExec(new Runnable() {
-			public void run()
-			{
-				try
-				{
+			public void run() {
+				try {
 					MessageConsole myConsole = findConsole(consoleName);
 					MessageConsoleStream out = myConsole.newMessageStream();
 					out.setColor(Display.getCurrent().getSystemColor(color));
 					out.println(message);
 					out.flush();
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -94,15 +120,12 @@ public class ConsoleWriter extends PrintWriter
 
 	}
 
-	public MessageConsole findConsole(String name)
-	{
+	public synchronized MessageConsole findConsole(String name) {
 		ConsolePlugin plugin = ConsolePlugin.getDefault();
 		IConsoleManager conMan = plugin.getConsoleManager();
 		IConsole[] existing = conMan.getConsoles();
-		for (int i = 0; i < existing.length; i++)
-		{
-			if (name.equals(existing[i].getName()))
-			{
+		for (int i = 0; i < existing.length; i++) {
+			if (name.equals(existing[i].getName())) {
 				return (MessageConsole) existing[i];
 			}
 		}
@@ -112,20 +135,17 @@ public class ConsoleWriter extends PrintWriter
 		return myConsole;
 	}
 
-	public void ConsolePrint(final Exception exception)
-	{
+	public void ConsolePrint(final Exception exception) {
 		ConsolePrint(getExceptionStackTraceAsString(exception));
 	}
 
-	public static String getExceptionStackTraceAsString(Exception exception)
-	{
+	public static String getExceptionStackTraceAsString(Exception exception) {
 		StringWriter sw = new StringWriter();
 		exception.printStackTrace(new PrintWriter(sw));
 		return sw.toString();
 	}
 
-	public static Display getDisplay()
-	{
+	public static Display getDisplay() {
 		return VdmUIPlugin.getDefault().getWorkbench().getDisplay();
 	}
 }
