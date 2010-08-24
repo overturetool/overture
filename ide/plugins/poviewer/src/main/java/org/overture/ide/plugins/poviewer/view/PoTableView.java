c53ae2eacaa8d729fa62a4a505d54a6bf3d35663
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2010-08-24 13:34:33 +0000

    fixed font in PO viewer
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@2243 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/plugins/poviewer/src/main/java/org/overture/ide/plugins/poviewer/view/PoTableView.java
+++ ide/plugins/poviewer/src/main/java/org/overture/ide/plugins/poviewer/view/PoTableView.java
@@ -2,6 +2,8 @@ package org.overture.ide.plugins.poviewer.view;
 
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
@@ -14,10 +16,15 @@ import org.overturetool.vdmj.pog.ProofObligation;
 public class PoTableView extends ViewPart implements ISelectionListener {
 	private Text viewer;
 	final Display display = Display.getCurrent();
+	private Font font = null;
+	private FontData defaultFont = new FontData("Courier",12, SWT.None);
 	/**
 	 * The constructor.
 	 */
 	public PoTableView() {
+		font = new Font(display,defaultFont);
+		
+		
 	}
 
 	/**
@@ -27,7 +34,7 @@ public class PoTableView extends ViewPart implements ISelectionListener {
 	@Override
 	public void createPartControl(Composite parent) {
 		viewer = new Text(parent, SWT.MULTI);
-
+		viewer.setFont(font);
 	}
 
 	/**
@@ -42,6 +49,7 @@ public class PoTableView extends ViewPart implements ISelectionListener {
 		display.asyncExec(new Runnable() {
 
 			public void run() {
+				
 				viewer.setText(data.getValue());
 			}
 
