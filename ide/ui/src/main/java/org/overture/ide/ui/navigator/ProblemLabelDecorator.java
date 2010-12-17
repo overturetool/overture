daa9aa4428295585f97a9016c6f1844aa28928ac
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2010-12-17 14:37:32 +0000

    Fixed null pointer with marker where attribute SEVERITY was not set
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@2409 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/ui/src/main/java/org/overture/ide/ui/navigator/ProblemLabelDecorator.java
+++ ide/ui/src/main/java/org/overture/ide/ui/navigator/ProblemLabelDecorator.java
@@ -10,64 +10,66 @@ import org.eclipse.jface.viewers.DecorationOverlayIcon;
 import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
 import org.eclipse.swt.graphics.Image;
+import org.overture.ide.ui.VdmUIPlugin;
 
-public class ProblemLabelDecorator extends BaseLabelProvider implements ILightweightLabelDecorator {
-	
+public class ProblemLabelDecorator extends BaseLabelProvider implements
+		ILightweightLabelDecorator
+{
 
-	public void decorate(Object element, IDecoration decoration) {		
-		if (element instanceof IResource) {
+	public void decorate(Object element, IDecoration decoration)
+	{
+		if (element instanceof IResource)
+		{
 			IResource resource = (IResource) element;
-			if(resource instanceof IProject){
-				if(!((IProject) resource).isOpen())
+			if (resource instanceof IProject)
+			{
+				if (!((IProject) resource).isOpen())
 					return;
 			}
-			
-			try {
-				IMarker[] markers = resource.findMarkers(IMarker.PROBLEM, true,
-						IResource.DEPTH_INFINITE);
-				if (markers.length > 0) {
 
-					if (checkIfOnlyWarnings(markers)) {
-						Image image = FieldDecorationRegistry.getDefault()
-								.getFieldDecoration(
-										FieldDecorationRegistry.DEC_WARNING)
-								.getImage();
-						decoration.addOverlay(DecorationOverlayIcon
-								.createFromImage(image),
-								IDecoration.BOTTOM_LEFT);
-					} else {
-						Image image = FieldDecorationRegistry.getDefault()
-								.getFieldDecoration(
-										FieldDecorationRegistry.DEC_ERROR)
-								.getImage();
-						decoration.addOverlay(DecorationOverlayIcon
-								.createFromImage(image),
-								IDecoration.BOTTOM_LEFT);
+			try
+			{
+				IMarker[] markers = resource.findMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+				if (markers.length > 0)
+				{
+
+					if (checkIfOnlyWarnings(markers))
+					{
+						Image image = FieldDecorationRegistry.getDefault().getFieldDecoration(FieldDecorationRegistry.DEC_WARNING).getImage();
+						decoration.addOverlay(DecorationOverlayIcon.createFromImage(image), IDecoration.BOTTOM_LEFT);
+					} else
+					{
+						Image image = FieldDecorationRegistry.getDefault().getFieldDecoration(FieldDecorationRegistry.DEC_ERROR).getImage();
+						decoration.addOverlay(DecorationOverlayIcon.createFromImage(image), IDecoration.BOTTOM_LEFT);
 					}
 				}
 
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			} catch (CoreException e)
+			{
+				VdmUIPlugin.log("Faild to decorate element: "+element, e);
 			}
 
 		}
 
 	}
 
-	private boolean checkIfOnlyWarnings(IMarker[] markers) {
-		for (int i = 0; i < markers.length; i++) {
-			try {
-				if (markers[i].getAttribute(IMarker.SEVERITY).equals(
-						IMarker.SEVERITY_ERROR))
+	private boolean checkIfOnlyWarnings(IMarker[] markers)
+	{
+		for (int i = 0; i < markers.length; i++)
+		{
+			try
+			{
+				Object tmp = markers[i].getAttribute(IMarker.SEVERITY);
+				if (tmp.equals(IMarker.SEVERITY_ERROR))
+				{
 					return false;
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				}
+			} catch (CoreException e)
+			{
+				VdmUIPlugin.log("Faild to check marker attribute SEVERITY", e);
 			}
 		}
 		return true;
 	}
-
 
 }
