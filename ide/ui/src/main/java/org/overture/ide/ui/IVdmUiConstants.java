48fb8fa460128f4b28508e046d15d102bf51303e
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-01-11 20:08:02 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@2440 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/ui/src/main/java/org/overture/ide/ui/IVdmUiConstants.java
+++ ide/ui/src/main/java/org/overture/ide/ui/IVdmUiConstants.java
@@ -8,6 +8,8 @@ public class IVdmUiConstants {
 	public final static String TEMPLATE_EXTENTION_POINT_ID = "org.overture.ide.ui.templateContextType";
 	public final static String TEMPLATE_EXTENTION_POINT_ID_ATTRIBUTE = "Id";
 	public final static String OUTLINE_ID = "org.overture.ide.ui.VdmOutlinePage";
+	public static final String DEFAULT_OSX_LATEX_PATH = "/usr/texbin/pdflatex";
+	public static final String OSX_LATEX_PATH_PREFERENCE = "__OSX_LATEX_PATH_PREFERENCE__";
 }
 
 
