6f3555d871fc32a387960bbfad00137a75f22214
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-07-06 11:28:42 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/branches/astV2@2848 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- core/ast/src/main/java/org/overturetool/vdmj/lex/LexIdentifierToken.java
+++ core/ast/src/main/java/org/overturetool/vdmj/lex/LexIdentifierToken.java
@@ -1,24 +1,70 @@
 package org.overturetool.vdmj.lex;
 
 
-public abstract class LexIdentifierToken extends LexToken{
+public class LexIdentifierToken extends LexToken{
+	private static final long serialVersionUID = 1L;
+	public final String name;
+	public final boolean old;
 
-	public LexIdentifierToken(LexLocation location, VDMToken type) {
-		super(location, type);
-		// TODO Auto-generated constructor stub
+	public LexIdentifierToken(String name, boolean old, LexLocation location)
+	{
+		super(location, VDMToken.IDENTIFIER);
+		this.name = name;
+		this.old = old;
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1770683334150366111L;
+	public LexNameToken getClassName()
+	{
+		// We don't know the class name of the name of a class until we've
+		// read the name. So create a new location with the right module.
 
-	public abstract LexNameToken getClassName();
+		LexLocation loc = new LexLocation(
+			location.file,
+			name,
+			location.startLine,
+			location.startPos,
+			location.endLine,
+			location.endPos);
 
-	public abstract boolean isOld();
+		return new LexNameToken("CLASS", name, loc);
+	}
 
-	public abstract String getName();
+	@Override
+	public boolean equals(Object other)
+	{
+		if (other instanceof LexIdentifierToken)
+		{
+			LexIdentifierToken tother = (LexIdentifierToken)other;
+			return this.name.equals(tother.getName()) && this.old == tother.isOld();
+		}
 
-	public abstract LexLocation getLocation();
+		return false;
+	}
 
+	@Override
+	public int hashCode()
+	{
+		return name.hashCode() + (old ? 1 : 0);
+	}
+
+	@Override
+	public String toString()
+	{
+		return name + (old ? "~" : "");
+	}
+
+	
+	public boolean isOld() {
+		return old;
+	}
+
+	
+	public String getName() {
+		return name;
+	}
+
+	
+	public LexLocation getLocation() {
+		return location;
+	}
 }
--- core/ast/src/main/java/org/overturetool/vdmj/lex/LexNameToken.java
+++ core/ast/src/main/java/org/overturetool/vdmj/lex/LexNameToken.java
@@ -1,63 +1,289 @@
 package org.overturetool.vdmj.lex;
 
+import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
+import org.overture.ast.analysis.IAnalysis;
+import org.overture.ast.analysis.IAnswer;
+import org.overture.ast.analysis.IQuestion;
+import org.overture.ast.analysis.IQuestionAnswer;
 import org.overture.ast.node.Node;
+import org.overture.ast.node.NodeEnum;
 import org.overture.ast.types.PType;
+import org.overturetool.vdmj.messages.InternalException;
 
-public abstract class LexNameToken extends LexToken{
+public class LexNameToken extends LexToken implements Serializable, Comparable<LexNameToken>
+{
+	private static final long serialVersionUID = 1L;
 
-	public LexNameToken(LexLocation location, VDMToken type) {
-		super(location, type);
-		// TODO Auto-generated constructor stub
+	public final String module;
+	public final String name;
+	public final boolean old;
+	public final boolean explicit;	// Name has an explicit module/class
+
+	public List<PType> typeQualifier = null;
+
+	private int hashcode = 0;
+
+	public LexNameToken(
+		String module, String name, LexLocation location,
+		boolean old, boolean explicit)
+	{
+		super(location, VDMToken.NAME);
+		this.module = module;
+		this.name = name;
+		this.old = old;
+		this.explicit = explicit;
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6021528487608461917L;
+	public LexNameToken(String module, String name, LexLocation location)
+	{
+		this(module, name, location, false, false);
+	}
 
-	public abstract LexIdentifierToken getIdentifier();
+	public LexNameToken(String module, LexIdentifierToken id)
+	{
+		super(id.getLocation(), VDMToken.NAME);
+		this.module = module;
+		this.name = id.getName();
+		this.old = id.isOld();
+		this.explicit = false;
+	}
 
-	public abstract LexNameToken getExplicit(boolean ex);
+	public LexIdentifierToken getIdentifier()
+	{
+		return new LexIdentifierToken(name, old, location);
+	}
 
-	public abstract LexNameToken getOldName();
+	public LexNameToken getExplicit(boolean ex)
+	{
+		return new LexNameToken(module, name, location, old, ex);
+	}
 
-	public abstract String getName();
+	public LexNameToken getOldName()
+	{
+		return new LexNameToken(module,
+			new LexIdentifierToken(name, true, location));
+	}
 
-	public abstract LexNameToken getPreName(LexLocation l);
+	public String getName()
+	{
+		// Flat specifications have blank module names
+		return (explicit ? (module.length() > 0 ? module + "`" : "") : "") +
+				name + (old ? "~" : "");	// NB. No qualifier
+	}
 
-	public abstract LexNameToken getPostName(LexLocation l);
+	public LexNameToken getPreName(LexLocation l)
+	{
+		return new LexNameToken(module, "pre_" + name, l);
+	}
 
-	public abstract LexNameToken getInvName(LexLocation l);
+	public LexNameToken getPostName(LexLocation l)
+	{
+		return new LexNameToken(module, "post_" + name, l);
+	}
 
-	public abstract LexNameToken getInitName(LexLocation l);
+	public LexNameToken getInvName(LexLocation l)
+	{
+		return new LexNameToken(module, "inv_" + name, l);
+	}
 
-	public abstract LexNameToken getModifiedName(String classname);
+	public LexNameToken getInitName(LexLocation l)
+	{
+		return new LexNameToken(module, "init_" + name, l);
+	}
 
-	public abstract LexNameToken getSelfName();
+	public LexNameToken getModifiedName(String classname)
+	{
+		LexNameToken mod = new LexNameToken(classname, name, location);
+		mod.setTypeQualifier(typeQualifier);
+		return mod;
+	}
 
-	public abstract LexNameToken getThreadName();
+	public LexNameToken getSelfName()
+	{
+		if (module.equals("CLASS"))
+		{
+			return new LexNameToken(name, "self", location);
+		}
+		else
+		{
+			return new LexNameToken(module, "self", location);
+		}
+	}
 
-	public abstract LexNameToken getPerName(LexLocation loc);
+	public LexNameToken getThreadName()
+	{
+		if (module.equals("CLASS"))
+		{
+			return new LexNameToken(name, "thread", location);
+		}
+		else
+		{
+			return new LexNameToken(module, "thread", location);
+		}
+	}
 
-	public abstract LexNameToken getClassName();
+	public static LexNameToken getThreadName(LexLocation loc)
+	{
+		return new LexNameToken(loc.module, "thread", loc);
+	}
 
-	public abstract void setTypeQualifier(List<PType> types);
+	public LexNameToken getPerName(LexLocation loc)
+	{
+		return new LexNameToken(module, "per_" + name, loc);
+	}
 
-	public abstract boolean equals(Object other);
+	public LexNameToken getClassName()
+	{
+		return new LexNameToken("CLASS", name, location);
+	}
 
-	public abstract boolean matches(LexNameToken other);
+	public void setTypeQualifier(List<PType> types)
+	{
+		if (hashcode != 0)
+		{
+			if ((typeQualifier == null && types != null) ||
+				(typeQualifier != null && !typeQualifier.equals(types)))
+			{
+				throw new InternalException(
+					2, "Cannot change type qualifier: " + this + " to " + types);
+			}
+		}
+
+		typeQualifier = types;
+	}
+
+	@Override
+	public boolean equals(Object other)
+	{
+		if (!(other instanceof LexNameToken))
+		{
+			return false;
+		}
+
+		LexNameToken lother = (LexNameToken)other;
+
+		if (typeQualifier != null && lother.getTypeQualifier() != null)
+		{
+			throw new InternalException(-1, "Use HelpLexNameToken.isEqual to compare");
+//			if (!TypeComparator.compatible(typeQualifier, lother.getTypeQualifier()))
+//			{
+//				return false;
+//			}
+		}
+		else if ((typeQualifier != null && lother.getTypeQualifier() == null) ||
+				 (typeQualifier == null && lother.getTypeQualifier() != null))
+		{
+			return false;
+		}
+
+		return matches(lother);
+	}
+
+	public boolean matches(LexNameToken other)
+	{
+		return module.equals(other.getModule()) &&
+				name.equals(other.getName()) &&
+				old == other.isOld();
+	}
+
+	@Override
+	public int hashCode()
+	{
+		if (hashcode == 0)
+		{
+			hashcode = module.hashCode() + name.hashCode() + (old ? 1 : 0) +
+				(typeQualifier == null ? 0 : typeQualifier.hashCode());
+		}
+
+		return hashcode;
+	}
+
+	@Override
+	public String toString()
+	{
+		return getName() + (typeQualifier == null ? "" : typeQualifier);
+	}
+
+	public LexNameToken copy()
+	{
+		LexNameToken c = new LexNameToken(module, name, location, old, explicit);
+		c.setTypeQualifier(typeQualifier);
+		return c;
+	}
+
+	public int compareTo(LexNameToken o)
+	{
+		return toString().compareTo(o.toString());
+	}
+
 	
-	public abstract LexLocation getLocation();
-
-	public abstract String getModule();
-
-	public abstract List<PType> getTypeQualifier();
-
-	public abstract boolean isOld();
+	public LexLocation getLocation() {
+		return location;
+	}
 
 	
+	public String getModule() {
+		return module;
+	}
 
+	@Override
+	public Object clone() {
+		return copy();
+	}
 
+	@Override
+	public Node clone(Map<Node, Node> oldToNewMap) {
+		Node n = copy();
+		oldToNewMap.put(this, n);
+		return n;
+	}
+
+	@Override
+	public NodeEnum kindNode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeChild(Node child) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void apply(IAnalysis analysis) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <A> A apply(IAnswer<A> caller) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <Q> void apply(IQuestion<Q> caller, Q question) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <Q, A> A apply(IQuestionAnswer<Q, A> caller, Q question) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public List<PType> getTypeQualifier() {
+		return typeQualifier;
+	}
+
+	
+	public boolean isOld() {
+		return old;
+	}
 }
