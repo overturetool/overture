45887e062e9b8168a57d5f5ad358c320a20f6daa
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-07-21 11:36:48 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/branches/astV2@3014 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/vdmjtypecheckertest/src/test/java/org/overture/typechecker/tests/ExpressionTypeCheckTestSuite.java
@@ -0,0 +1,22 @@
+package org.overture.typechecker.tests;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.overture.typechecker.tests.framework.BaseTestSuite;
+import org.overture.typechecker.tests.framework.GeneratedTestCase;
+import org.overture.typechecker.tests.framework.BasicTypeCheckTestCase.ParserType;
+
+public class ExpressionTypeCheckTestSuite extends BaseTestSuite
+{
+	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
+	{
+		String name = "Type Check Expression TestSuite";
+		String root = "src\\test\\resources\\expressions";
+		TestSuite test = createTestSingleLineFile(ParserType.Expression,name,root+"\\singleline",GeneratedTestCase.class);
+		return test;
+	}
+}
---
+++ core/vdmjtypecheckertest/src/test/java/org/overture/typechecker/tests/framework/GeneratedTestCase.java
@@ -0,0 +1,187 @@
+package org.overture.typechecker.tests.framework;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Vector;
+
+
+import org.overturetool.vdmj.definitions.Definition;
+import org.overturetool.vdmj.definitions.DefinitionList;
+import org.overturetool.vdmj.expressions.Expression;
+import org.overturetool.vdmj.lex.LexException;
+import org.overturetool.vdmj.syntax.ParserException;
+import org.overturetool.vdmj.typechecker.Environment;
+import org.overturetool.vdmj.typechecker.FlatCheckedEnvironment;
+import org.overturetool.vdmj.typechecker.NameScope;
+import org.overturetool.vdmj.typechecker.TypeChecker;
+import org.overturetool.vdmj.typechecker.TypeComparator;
+import org.overturetool.vdmj.types.Type;
+
+public class GeneratedTestCase extends BasicTypeCheckTestCase
+{
+	File file;
+	String name;
+	String content;
+	String expectedType;
+	ParserType parserType;
+	private boolean showWarnings;
+	private boolean generateResultOutput = true;
+
+	public GeneratedTestCase()
+	{
+		super("test");
+	}
+
+	public GeneratedTestCase(ParserType type, File file)
+	{
+		super("test");
+		this.parserType = type;
+		this.file = file;
+		this.content = file.getName();
+		this.expectedType = null;
+	}
+
+	public GeneratedTestCase(ParserType type, String name, File file, String content,
+			String expectedType)
+	{
+		super("test");
+		this.parserType = type;
+		this.file = file;
+		this.content = content;
+		this.expectedType = expectedType;
+		this.name = name;
+	}
+
+	@Override
+	public String getName()
+	{
+		if (name != null)
+		{
+			return name;
+		} else if (file != null)
+		{
+			String name = file.getName();
+			if (name.contains("."))
+			{
+				return name.substring(0, name.indexOf("."));
+			}
+			return file.getName();
+		}
+		return "Generic Base Test";
+	}
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		TypeChecker.clearErrors();
+	}
+
+	public void test() throws ParserException, LexException
+	{
+		if (content != null)
+		{
+			expressionTc(content, expectedType);
+		}
+	}
+
+	private void expressionTc(String expressionString, String expectedTypeString)
+			throws ParserException, LexException
+	{
+		System.out.flush();
+		System.err.flush();
+
+		Expression exp = parse(parserType, expressionString);
+		Type expectedType = getResultType(expectedTypeString);
+
+		System.out.println(exp.toString().replace('\n', ' ').replace('\r', ' '));
+
+		Environment env = new FlatCheckedEnvironment(new DefinitionList(), NameScope.NAMESANDSTATE);
+//		TypeCheckVisitor tc = new TypeCheckVisitor();
+		Type type = exp.typeCheck(env, null, null);//exp.apply(tc, new TypeCheckInfo(env));
+
+		//
+
+		String errorMessages = null;
+		if (TypeChecker.getErrorCount() > 0)
+		{
+			// perrs += reader.getErrorCount();
+			StringWriter s = new StringWriter();
+			TypeChecker.printErrors(new PrintWriter(s));// new PrintWriter(System.out));
+			errorMessages = "\n" + s.toString() + "\n";
+			System.out.println(s.toString());
+		}
+
+		assertEquals(errorMessages, 0, TypeChecker.getErrorCount());
+
+		if (showWarnings && TypeChecker.getWarningCount() > 0)
+		{
+			// perrs += reader.getErrorCount();
+			StringWriter s = new StringWriter();
+			TypeChecker.printWarnings(new PrintWriter(s));// new PrintWriter(System.out));
+			// String warningMessages = "\n" + s.toString() + "\n";
+			System.out.println(s.toString());
+		}
+		//
+
+		if (expectedType != null)
+		{
+			assertEquals("Type of \n"
+					+ exp.toString().replace('\n', ' ').replace('\r', ' ')
+					+ " \nis: " + type + " \nexpected: " + expectedType + "\n", true, TypeComparator.compatible(expectedType, type));
+		}
+
+		if (type != null)
+		{
+			System.out.println("Type of \""
+					+ exp.toString().replace('\n', ' ').replace('\r', ' ')
+					+ "\" is: " + type);
+		} else
+		{
+			System.err.println("Type of \""
+					+ exp.toString().replace('\n', ' ').replace('\r', ' ')
+					+ "\" is: " + type);
+		}
+		System.out.flush();
+		System.err.flush();
+
+		if (generateResultOutput )
+		{
+			writeTestWithResult(type, expressionString);
+		}
+	}
+
+	private void writeTestWithResult(Type type, String test)
+	{
+		File outputFile = new File(file.getAbsolutePath()+ "_generated");
+		FileWriter outFile;
+		String typeName = "Error";
+		if(type != null)
+		{
+			typeName = type.toString();
+		}
+		try
+		{
+			outFile = new FileWriter(outputFile, true);
+			PrintWriter out = new PrintWriter(outFile);
+			out.println(pad(typeName,20 )+"$" + test);
+			out.close();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+	
+	private static String pad(String text, int length)
+	{
+		while (text.length() < length)
+		{
+			text += " ";
+		}
+		return text;
+	}
+}
