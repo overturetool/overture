9b684fe1401a137a3e9b0c212a45d23a4af3ffec
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-07-26 12:12:22 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/branches/astV2@3032 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ core/interpreter/src/main/java/org/overturetool/vdmj/messages/StderrRedirector.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ *
+ *	Copyright (c) 2009 Fujitsu Services Ltd.
+ *
+ *	Author: Nick Battle
+ *
+ *	This file is part of VDMJ.
+ *
+ *	VDMJ is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	VDMJ is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+package org.overturetool.vdmj.messages;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class StderrRedirector extends Redirector
+{
+	public StderrRedirector(OutputStreamWriter out)
+	{
+		super(out);
+	}
+
+	@Override
+	public void print(String line)
+	{
+		try
+		{
+    		switch (type)
+    		{
+    			case DISABLE:
+    				super.print(line);
+    				break;
+
+    			case COPY:
+    				super.print(line);
+    				dbgp.stderr(line);
+    				break;
+
+    			case REDIRECT:
+    				dbgp.stderr(line);
+    				break;
+    		}
+		}
+		catch (IOException e)
+		{
+			super.print(line);		// Better than ignoring it??
+		}
+	}
+}
