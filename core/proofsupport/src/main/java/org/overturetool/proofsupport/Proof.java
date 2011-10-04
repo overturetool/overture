539d7f9a4cc5a489c2f9b35248bd9530ffa282fe
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-10-04 06:55:26 +0000

    changed by Miguel
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3141 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- core/proofsupport/src/main/java/org/overturetool/proofsupport/Proof.java
+++ core/proofsupport/src/main/java/org/overturetool/proofsupport/Proof.java
@@ -7,49 +7,54 @@ import org.overturetool.proofsupport.external_tools.Utilities;
 
 public class Proof {
 
-	private static final String VDM_ADDITIONAL_TAC = "VDM_ADDITIONAL_TAC";
-	private static final String VDM_GENERIC_TAC = "VDM_GENERIC_TAC";
-	
-	private static final String proofCommand = "can TAC_PROOF(([]:(term list), ``%1$s``), %2$s);";
-	protected String theory = null;
-	protected ArrayList<String> holProofExpressions = null;
-	protected ArrayList<String> vdmProofExpressions = null;
+    private static final String VDM_ADDITIONAL_TAC = "VDM_ADDITIONAL_TAC";
+    private static final String VDM_GENERIC_TAC = "VDM_GENERIC_TAC";
 
-	public Proof(String theory, List<String> holProofExpressions, List<String> vdmProofExpressions) {
-		this.theory = theory.trim() + Utilities.LINE_SEPARATOR;
-		this.holProofExpressions = new ArrayList<String>(holProofExpressions);
-		this.vdmProofExpressions = new ArrayList<String>(vdmProofExpressions);
-	}
+    private static final String proofCommand = "can TAC_PROOF(([]:(term list), ``%1$s``), %2$s);";
+    protected String theory = null;
+    protected ArrayList<String> holProofExpressions = null;
+    protected ArrayList<String> vdmProofExpressions = null;
 
-	public String getProofCommand(int index) {
-		String predicate = holProofExpressions.get(index);
-		return buildProofCommand(predicate);
-	}
-	
-	public String getProofExpression(int index) {
-		return vdmProofExpressions.get(index);
-	}
-	
-	private String buildProofCommand(String predicate) {
-		// TODO: replace VDM_GEENRIC_TAC by VDM_ADDITIONAL_TAC
-		//       when some kind of timer is in place to make the proofs timeout
-		//       if needed.
-		return String.format(proofCommand, predicate, VDM_ADDITIONAL_TAC);
-	}
+    public Proof(String theory, List<String> holProofExpressions, List<String> vdmProofExpressions) {
+        this.theory = theory.trim() + Utilities.LINE_SEPARATOR;
+        this.holProofExpressions = new ArrayList<String>(holProofExpressions);
+        this.vdmProofExpressions = new ArrayList<String>(vdmProofExpressions);
+    }
 
-	public int proofCommandsSize() {
-		return holProofExpressions.size();
-	}
+    public String getProofCommand(int index) {
+        String predicate = holProofExpressions.get(index);
+        return buildProofCommand(predicate);
+    }
 
-	public String getTheory() {
-		return theory;
-	}
+    public String getProofExpression(int index) {
+        return vdmProofExpressions.get(index);
+    }
 
-	public String getProofScript() {
-		StringBuffer sb = new StringBuffer(theory);
-		for(String predicate : holProofExpressions)
-			sb.append(buildProofCommand(predicate)).append(Utilities.LINE_SEPARATOR);
-		return sb.toString();
-	}
+    private String buildProofCommand(String predicate) {
+        // TODO: replace VDM_GEENRIC_TAC by VDM_ADDITIONAL_TAC
+        //       when some kind of timer is in place to make the proofs timeout
+        //       if needed.
+        return String.format(proofCommand, predicate, VDM_GENERIC_TAC);
+    }
+
+    public int proofCommandsSize() {
+        return holProofExpressions.size();
+    }
+
+    public String getTheory() {
+        return theory;
+    }
+
+    public String getProofScript() {
+        StringBuffer sb = new StringBuffer(theory);
+        for (String predicate : holProofExpressions)
+            sb.append(buildProofCommand(predicate)).append(Utilities.LINE_SEPARATOR);
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return getTheory();
+    }
 
 }
