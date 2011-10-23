320a06fb11f786afb8908b4cca0afaec1e94b741
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-10-23 18:17:19 +0000

    fixed warnings and added License
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3164 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/plugins/csk/src/main/java/org/overture/ide/plugins/csk/internal/WorkbenchPreferencePageCsk.java
+++ ide/plugins/csk/src/main/java/org/overture/ide/plugins/csk/internal/WorkbenchPreferencePageCsk.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Overture Team and others.
+ *
+ * Overture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Overture is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Overture.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ * The Overture Tool web-site: http://overturetool.org/
+ *******************************************************************************/
 package org.overture.ide.plugins.csk.internal;
 
 import org.eclipse.core.runtime.Platform;
@@ -10,8 +28,8 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.overture.ide.plugins.csk.Activator;
 import org.overture.ide.plugins.csk.ICskConstants;
 
-public class WorkbenchPreferencePageCsk extends FieldEditorPreferencePage implements
-IWorkbenchPreferencePage 
+public class WorkbenchPreferencePageCsk extends FieldEditorPreferencePage
+		implements IWorkbenchPreferencePage
 {
 
 	@Override
@@ -19,20 +37,17 @@ IWorkbenchPreferencePage
 	{
 		if (!Platform.getOS().equalsIgnoreCase(Platform.OS_MACOSX))
 		{
-		addField(new FileFieldEditor(ICskConstants.VPPGDE_PATH, "Path to VDM Tools for VDM-PP (vppgde):", getFieldEditorParent()));
-		addField(new FileFieldEditor(ICskConstants.VRTGDE_PATH, "Path to VDM Tools for VICE  (vicegde):", getFieldEditorParent()));
-		addField(new FileFieldEditor(ICskConstants.VSLGDE_PATH, "Path to VDM Tools for VDM-SL (vdmgde):", getFieldEditorParent()));
-		}else
+			addField(new FileFieldEditor(ICskConstants.VPPGDE_PATH, "Path to VDM Tools for VDM-PP (vppgde):", getFieldEditorParent()));
+			addField(new FileFieldEditor(ICskConstants.VRTGDE_PATH, "Path to VDM Tools for VICE  (vicegde):", getFieldEditorParent()));
+			addField(new FileFieldEditor(ICskConstants.VSLGDE_PATH, "Path to VDM Tools for VDM-SL (vdmgde):", getFieldEditorParent()));
+		} else
 		{
 			addField(new DirectoryFieldEditor(ICskConstants.VPPGDE_PATH, "Path to VDM Tools for VDM-PP (vppgde):", getFieldEditorParent()));
 			addField(new DirectoryFieldEditor(ICskConstants.VRTGDE_PATH, "Path to VDM Tools for VICE  (vicegde):", getFieldEditorParent()));
 			addField(new DirectoryFieldEditor(ICskConstants.VSLGDE_PATH, "Path to VDM Tools for VDM-SL (vdmgde):", getFieldEditorParent()));
-		
-			
+
 		}
 	}
-
-	
 
 	@Override
 	protected IPreferenceStore doGetPreferenceStore()
@@ -47,10 +62,10 @@ IWorkbenchPreferencePage
 		store.setDefault(ICskConstants.VPPGDE_PATH, ICskConstants.DEFAULT_VPPGDE_PATH);
 		super.performDefaults();
 	}
-	
+
 	public void init(IWorkbench workbench)
 	{
-		
+
 	}
 
 }
--- ide/plugins/showtrace/src/main/java/org/overture/ide/plugins/showtrace/viewer/TracefileMarker.java
+++ ide/plugins/showtrace/src/main/java/org/overture/ide/plugins/showtrace/viewer/TracefileMarker.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Overture Team and others.
+ *
+ * Overture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Overture is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Overture.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ * The Overture Tool web-site: http://overturetool.org/
+ *******************************************************************************/
 // Decompiled by DJ v3.7.7.81 Copyright 2004 Atanas Neshkov Date: 31-07-2009 16:17:14
 // Home Page : http://members.fortunecity.com/neshkov/dj.html - Check often for new version!
 // Decompiler options: packimports(3)
