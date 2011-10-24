b9faef43a7d30fb3b8d6fc8f2346298d5ae33b18
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-10-24 20:29:50 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3178 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- core/vdmexamples/src/test/java/org/overturetool/test/examples/InterpreterPpTestCase.java
+++ core/vdmexamples/src/test/java/org/overturetool/test/examples/InterpreterPpTestCase.java
@@ -1,15 +1,27 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 Overture Team and others. Overture is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version. Overture is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details. You should have received a copy of the GNU General Public
- * License along with Overture. If not, see <http://www.gnu.org/licenses/>. The Overture Tool web-site:
- * http://overturetool.org/
+ * Copyright (c) 2009, 2011 Overture Team and others.
+ *
+ * Overture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Overture is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Overture.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ * The Overture Tool web-site: http://overturetool.org/
  *******************************************************************************/
 package org.overturetool.test.examples;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Vector;
@@ -21,6 +33,9 @@ import org.overturetool.test.framework.examples.VdmReadme;
 import org.overturetool.vdmj.Settings;
 import org.overturetool.vdmj.definitions.ClassList;
 import org.overturetool.vdmj.lex.Dialect;
+import org.overturetool.vdmj.messages.Console;
+import org.overturetool.vdmj.messages.StderrRedirector;
+import org.overturetool.vdmj.messages.StdoutRedirector;
 import org.overturetool.vdmj.runtime.ClassInterpreter;
 import org.overturetool.vdmj.values.Value;
 
@@ -97,5 +112,14 @@ return;
 	{
 		super.setUp();
 		Settings.dialect = Dialect.VDM_PP;
+		Console.out = new StdoutRedirector(new OutputStreamWriter(new NullOutputStream()));
+		Console.err = new StderrRedirector(new OutputStreamWriter(new NullOutputStream()));
+	}
+	
+	/**Writes to nowhere*/
+	public class NullOutputStream extends OutputStream {
+	  @Override
+	  public void write(int b) throws IOException {
+	  }
 	}
 }
---
+++ core/vdmexamples/src/test/java/org/overturetool/test/examples/testsuites/PdfLatexExamplesTestSuite.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 Overture Team and others.
+ *
+ * Overture is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Overture is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Overture.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ * The Overture Tool web-site: http://overturetool.org/
+ *******************************************************************************/
+package org.overturetool.test.examples.testsuites;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Enumeration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.overturetool.test.examples.LatexPpTestCase;
+import org.overturetool.test.examples.TypeCheckPpTestCase;
+import org.overturetool.test.examples.TypeCheckRtTestCase;
+import org.overturetool.test.examples.TypeCheckSlTestCase;
+import org.overturetool.test.framework.BaseTestSuite;
+
+
+public class PdfLatexExamplesTestSuite extends BaseTestSuite
+{
+	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
+	{
+		String name = "PdfLatex Examples TestSuite";
+		String root = "../../documentation/examples/";
+		TestSuite test = createTestCompleteDirectory(name,root+"VDM++",LatexPpTestCase.class);
+//		add(test,createTestCompleteDirectory(name,root+"VDMSL",TypeCheckSlTestCase.class));
+//		add(test,createTestCompleteDirectory(name,root+"VDMRT",TypeCheckRtTestCase.class));
+		return test;
+	}
+
+	private static void add(TestSuite test, TestSuite test2)
+	{
+		@SuppressWarnings("unchecked")
+		Enumeration<Test> e = test2.tests();
+		while(e.hasMoreElements())
+		{
+			test.addTest(e.nextElement());
+		}
+	}
+	
+	
+}
