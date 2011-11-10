f3b2ee84298723b3a8054f4e22f0a6004afe342e
rasmuswl <rasmuswl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-11-10 08:38:59 +0000

    PreName utility function added
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/branches/astV2@3240 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- core/typechecker/src/main/java/org/overture/ast/expressions/assistants/PExpAssistant.java
+++ core/typechecker/src/main/java/org/overture/ast/expressions/assistants/PExpAssistant.java
@@ -1,8 +1,42 @@
 package org.overture.ast.expressions.assistants;
 
+import org.overture.ast.definitions.AExplicitFunctionDefinition;
+import org.overture.ast.definitions.AImplicitFunctionDefinition;
+import org.overture.ast.definitions.PDefinition;
+import org.overture.ast.expressions.AFuncInstatiationExp;
+import org.overture.ast.expressions.AVariableExp;
+import org.overture.ast.expressions.PExp;
 
 public class PExpAssistant {
 
+	public static String getPreName(PExp root) {
+		String result = null;
+		switch (root.kindPExp()) {
+		case FUNCINSTATIATION: {
+			AFuncInstatiationExp func = AFuncInstatiationExp.class.cast(root);
+			result = getPreName(func.getFunction());
+		}
+			break;
+		case VARIABLE: {
+			AVariableExp var = AVariableExp.class.cast(root);
 
+			PDefinition def = var.getVardef();
 
+			if (def instanceof AExplicitFunctionDefinition) {
+				AExplicitFunctionDefinition ex = AExplicitFunctionDefinition.class
+						.cast(def);
+				PDefinition predef = ex.getPredef();
+				result = predef == null ? "" : predef.getName().name;
+
+			} else if (def instanceof AImplicitFunctionDefinition) {
+				AImplicitFunctionDefinition im = AImplicitFunctionDefinition.class
+						.cast(def);
+				PDefinition predef = im.getPredef();
+				result = predef == null ? "" : predef.getName().name;
+			}
+			break;
+		}
+		}
+		return result;
+	}
 }
