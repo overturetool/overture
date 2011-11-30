8ed3484633196849c8ee1c46a9b2ae9092c5a829
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-11-30 12:34:29 +0000

    Fixed bug id: 3427211 - Func/ops with pattern parameters confuse the launch dialog
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3382 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/ui/src/main/java/org/overture/ide/ui/outline/DisplayNameCreator.java
+++ ide/ui/src/main/java/org/overture/ide/ui/outline/DisplayNameCreator.java
@@ -18,20 +18,16 @@
  *******************************************************************************/
 package org.overture.ide.ui.outline;
 
-import java.util.List;
-import java.util.Vector;
-
 import org.overturetool.vdmj.definitions.ClassDefinition;
 import org.overturetool.vdmj.definitions.Definition;
 import org.overturetool.vdmj.definitions.ExplicitFunctionDefinition;
 import org.overturetool.vdmj.definitions.ExplicitOperationDefinition;
-import org.overturetool.vdmj.definitions.LocalDefinition;
 import org.overturetool.vdmj.modules.Module;
 import org.overturetool.vdmj.patterns.IdentifierPattern;
 import org.overturetool.vdmj.patterns.IgnorePattern;
 import org.overturetool.vdmj.patterns.Pattern;
-import org.overturetool.vdmj.patterns.PatternList;
 import org.overturetool.vdmj.patterns.RecordPattern;
+import org.overturetool.vdmj.types.FunctionType;
 import org.overturetool.vdmj.types.Type;
 
 public class DisplayNameCreator {
@@ -47,77 +43,66 @@ public class DisplayNameCreator {
 				return ((Module) element).name.name;
 			else if (element instanceof Definition)
 			{
-				sb.append(((Definition) element).name.name);
+				//sb.append(((Definition) element).name.name);
 				if (element instanceof ExplicitOperationDefinition)
 				{
+					
 					ExplicitOperationDefinition def = (ExplicitOperationDefinition) element;
-					sb.append("(");
-					for (int i = 0; i < def.parameterPatterns.size(); i++)
-					{
-						if (def.parameterPatterns.size() <= i)
-							sb.append("UNRESOLVED");
-						else
-						{
-							if (def.paramDefinitions != null
-									&& def.paramDefinitions.get(i) instanceof LocalDefinition)
-								sb.append(((LocalDefinition) def.paramDefinitions.get(i)).type
-										+ " " + def.parameterPatterns.get(i));
-							else if (def.paramDefinitions != null)
-								sb.append(def.paramDefinitions.get(i).name.name
-										+ " " + def.parameterPatterns.get(i));
-							else 	
-							{
-								
-								if(def.type!=null)
-								{
-									StringBuilder parametersSb= new StringBuilder();
-									for(Type t : def.type.parameters)
-									{
-										sb.append(t.toString().replace("unresolved ", "")+ " %s");
-									}
-									List<String> patterns = new Vector<String>();
-									for(Pattern pattern: def.parameterPatterns)
-									{
-										patterns.add(pattern.toString());
-									}
-									sb.append(String.format(parametersSb.toString(),patterns));
-									
-								}
-								else
-									sb.append("UNRESOLVED");
-							}
-						}
-						if (i + 1 < def.parameterPatterns.size())
-							sb.append(", ");
-					}
-					sb.append(")");
+					sb.append(def.name);
+//					sb.append("(");
+//					for (int i = 0; i < def.parameterPatterns.size(); i++)
+//					{
+//						if (def.parameterPatterns.size() <= i)
+//							sb.append("UNRESOLVED");
+//						else
+//						{
+//							if (def.paramDefinitions != null
+//									&& def.paramDefinitions.get(i) instanceof LocalDefinition)
+//								sb.append(((LocalDefinition) def.paramDefinitions.get(i)).type
+//										+ " " + def.parameterPatterns.get(i));
+//							else if (def.paramDefinitions != null)
+//								sb.append(def.paramDefinitions.get(i).name.name
+//										+ " " + def.parameterPatterns.get(i));
+//							else 	
+//							{
+//								
+//								if(def.type!=null)
+//								{
+//									StringBuilder parametersSb= new StringBuilder();
+//									for(Type t : def.type.parameters)
+//									{
+//										sb.append(t.toString().replace("unresolved ", "")+ " %s");
+//									}
+//									List<String> patterns = new Vector<String>();
+//									for(Pattern pattern: def.parameterPatterns)
+//									{
+//										patterns.add(pattern.toString());
+//									}
+//									sb.append(String.format(parametersSb.toString(),patterns));
+//									
+//								}
+//								else
+//									sb.append("UNRESOLVED");
+//							}
+//						}
+//						if (i + 1 < def.parameterPatterns.size())
+//							sb.append(", ");
+//					}
+//					sb.append(")");
 				} else if (element instanceof ExplicitFunctionDefinition)
 				{
 					ExplicitFunctionDefinition def = (ExplicitFunctionDefinition) element;
-					sb.append("(");
-					for (int i = 0; i < def.paramPatternList.size(); i++)
-					{
-						if (def.paramPatternList.get(i) instanceof PatternList)
-						{
-							PatternList patternList = def.paramPatternList.get(i);
+					
+					
+					sb.append(def.getName());
 
-							for (int j = 0; j < patternList.size(); j++)
-							{
-								Pattern pattern=patternList.get(j); 
-								sb.append(def.type.parameters.get(j)
-										.toString()
-										.replace("unresolved ", "")
-										+ " ");
-								sb.append(	print(pattern));
-								if (j + 1 < patternList.size())
-									sb.append(", ");
-							}
-						}
+					if (def.getType() instanceof FunctionType) {
+						FunctionType type = (FunctionType) def.getType();
+						sb.append(getFunctionTypeString(type));
 					}
-					sb.append(")");
-				}
-
+				}		
 			}
+			
 
 			return sb.toString();
 		} catch (Exception e)
@@ -125,9 +110,32 @@ public class DisplayNameCreator {
 			e.printStackTrace();
 			return "UNRESOLVED_NAME";
 		}
-		
-		
 	}
+	
+	public static String getFunctionTypeString(FunctionType type)
+	{
+		StringBuffer sb = new StringBuffer();
+		if (type.parameters.size() == 0) {
+			sb.append("() ");
+		} else {
+			sb.append("(");
+			int i = 0;
+			while (i < type.parameters.size() - 1) {
+				Type definition = (Type) type.parameters.elementAt(i);
+				sb.append(getSimpleTypeString(definition) + ", ");
+
+				i++;
+			}
+			Type definition = (Type) type.parameters.elementAt(i);
+			sb.append(getSimpleTypeString(definition) + ")");
+			if(type.result instanceof FunctionType)
+			{
+				sb.append(getFunctionTypeString((FunctionType) type.result));
+			}
+		}
+		return sb.toString();
+	}
+	
 	public static String print(Pattern pattern)
 		{
 			
@@ -149,4 +157,24 @@ public class DisplayNameCreator {
 			
 			return sb.toString();
 		}
+	
+	private static String processUnresolved(Type definition) {
+
+		String defString = definition.toString();
+
+		if (defString.contains("unresolved ")) {
+			defString = defString.replace("(", "");
+			defString = defString.replace(")", "");
+			defString = defString.replace("unresolved ", "");
+			return defString;
+		}
+		return definition.toString();
+	}
+
+	private static String getSimpleTypeString(Type type) {
+		String typeName = processUnresolved(type);
+		typeName = typeName.replace("(", "");
+		typeName = typeName.replace(")", "");
+		return typeName;
+	}
 }
