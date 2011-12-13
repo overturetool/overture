e3709b2f98ad92b95a618cef183dd6cfd3ed5fe3
augusto_silva <augusto_silva@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-12-13 11:30:09 +0000

    Cleaning up the VDM Perspective
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3460 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/ui/src/main/java/org/overture/ide/ui/perspective/VdmPerspectiveFactory.java
+++ ide/ui/src/main/java/org/overture/ide/ui/perspective/VdmPerspectiveFactory.java
@@ -29,15 +29,8 @@ public class VdmPerspectiveFactory implements IPerspectiveFactory {
 	public void createInitialLayout(IPageLayout layout) {
 
 		String editorArea = layout.getEditorArea();
-//		VdmUIPlugin.println("Editor Area: " + editorArea );
-//		
-//		IFolderLayout folder= layout.createFolder("left", IPageLayout.LEFT, (float)0.2, editorArea); //$NON-NLS-1$
-//		String navigator = IVdmIdeUiConstants.NAVIGATOR;
-//		folder.addView(navigator);		
-//		
-//		
+
 		IFolderLayout outputfolder= layout.createFolder("bottom", IPageLayout.BOTTOM, (float)0.75, editorArea); //$NON-NLS-1$
-//
 		if(VdmUIPlugin.DEBUG)
 			outputfolder.addView("org.eclipse.pde.runtime.LogView");
 		
@@ -46,31 +39,6 @@ public class VdmPerspectiveFactory implements IPerspectiveFactory {
 		outputfolder.addView(IConsoleConstants.ID_CONSOLE_VIEW);
 		
 		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, (float)0.75, editorArea);
-//		
-//		//outputfolder.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
-//		outputfolder.addPlaceholder(IPageLayout.ID_BOOKMARKS);
-//		outputfolder.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
-//		
-//		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, (float)0.75, editorArea);
-//				
-//		layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
-//		
-//		
-//		// views - standard workbench
-//		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
-//		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
-//		//layout.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-		//layout.addShowViewShortcut(navigator);
-//		layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
-//		layout.addShowViewShortcut(IProgressConstants.PROGRESS_VIEW_ID);
-//		
-//		// General
-//		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
-//		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
-//		layout.addNewWizardShortcut("org.eclipse.ui.editors.wizards.UntitledTextFileWizard");//$NON-NLS-1$
-//
-//		//VDM Stuff
-//		layout.addNewWizardShortcut("org.overture.ide.vdmpp.ui.projectWizard");//$NON-NLS-1$
 
 	}
 
