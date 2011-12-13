90746a1cba6cc45a6b12c5953aa91432c40fe4a1
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-12-13 09:09:14 +0000

    inserted space removal from vdm tools project file name
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3456 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/plugins/csk/src/main/java/org/overture/ide/plugins/csk/internal/VdmTools.java
+++ ide/plugins/csk/src/main/java/org/overture/ide/plugins/csk/internal/VdmTools.java
@@ -90,7 +90,7 @@ public class VdmTools
 		// "generated");
 		generated.mkdirs();
 		
-		String projectFileName = vdmProject.getName().trim();
+		String projectFileName = vdmProject.getName().trim().replace(" ", "");
 
 		PluginFolderInclude.writeFile(generated, projectFileName + ".prj", sb.toString());
 		VdmToolsOptions options = new VdmToolsOptions();
