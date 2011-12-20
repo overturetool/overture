b2113bd02c00d34df018d3a5863599ca71828963
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-12-20 12:45:26 +0000

    new overall Maven configuration. Main change is that the IDE now uses only the maven eclipse plug-in. A lot of unused configurations have also been removed (mainly because they are inherited from super poms and dont need to be repeated).
    The IDE can now be build from the ide/build artifact by deploy which produces target/RCP with all platforms *Required an Eclipse installation with delta package and settings configuration in .m2
    
    The new Maven configuration supports Maven 3.
    
    General setup instructions are:
    1. check out trunk
    2. core/ run mvn install
    3. ide/ run mvn install -P nocompile
    4. trunk run mvn eclipse:eclipse
    5. ide/ run mvn eclipse:eclipse
    
    Now the projects can be imported in Eclipse through the import existing Maven projects. The reason for the repeated eclipse:eclipse is that it second one uses the .classpath to sync the runtime config in the MANIFEST.MF.
    
    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/trunk@3509 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- ide/debug/src/main/java/org/overture/ide/debug/core/IDebugConstants.java
+++ ide/debug/src/main/java/org/overture/ide/debug/core/IDebugConstants.java
@@ -18,6 +18,8 @@
  *******************************************************************************/
 package org.overture.ide.debug.core;
 
+import org.overture.ide.core.ICoreConstants;
+
 public interface IDebugConstants
 {
 
@@ -66,7 +68,7 @@ public interface IDebugConstants
 	public static final String BREAKPOINT_MARKER_ID = "org.eclipse.debug.core.lineBreakpointMarker";
 	
 	//debugger
-	public static final String DEBUG_ENGINE_BUNDLE_ID = "org.overture.ide.generated.vdmj";
+	public static final String DEBUG_ENGINE_BUNDLE_ID = ICoreConstants.PLUGIN_ID;//"org.overture.ide.generated.vdmj";
 	public static final String DEBUG_ENGINE_CLASS = "org.overturetool.vdmj.debug.DBGPReaderV2";
 	static final String EXTENSION_POINT_VDM_BREAKPOINT_LISTENERS = "breakpointListeners";
 	
