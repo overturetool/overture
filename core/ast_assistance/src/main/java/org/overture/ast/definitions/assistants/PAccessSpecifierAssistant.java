ec6779908260b8c55360fa6e556874ec6bd69a84
anderskm <anderskm@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2011-12-22 14:02:21 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/branches/astV2@3541 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

--- core/ast_assistance/src/main/java/org/overture/ast/definitions/assistants/PAccessSpecifierAssistant.java
+++ core/ast_assistance/src/main/java/org/overture/ast/definitions/assistants/PAccessSpecifierAssistant.java
@@ -1,6 +1,7 @@
 package org.overture.ast.definitions.assistants;
 
 import org.overture.ast.definitions.APrivateAccess;
+import org.overture.ast.definitions.APublicAccess;
 import org.overture.ast.types.AAccessSpecifierAccessSpecifier;
 
 public class PAccessSpecifierAssistant {
@@ -11,5 +12,14 @@ public class PAccessSpecifierAssistant {
 		return new AAccessSpecifierAccessSpecifier(new APrivateAccess(), null, null);
 	}
 	
+	public static boolean isStatic(AAccessSpecifierAccessSpecifier access) {
+		
+		return access != null && access.getStatic() != null;
+	}
+	
+	public static boolean isPublic(AAccessSpecifierAccessSpecifier access) {
+		
+		return access != null && access.getAccess() instanceof APublicAccess;
+	}
 	
 }
