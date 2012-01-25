df19d02eeb3349ceb34e68a08ade756cb55fdcbb
lausdahl <lausdahl@2b2d4d09-c93c-414a-ad04-6e973f5c1cff>
2012-01-25 13:47:21 +0000

    git-svn-id: https://overture.svn.sourceforge.net/svnroot/overture/branches/astV2@3584 2b2d4d09-c93c-414a-ad04-6e973f5c1cff

---
+++ tools/astCreator/src/main/java/com/lausdahl/ast/creator/CmdMain.java
@@ -0,0 +1,49 @@
+package com.lausdahl.ast.creator;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CmdMain
+{
+
+	/**
+	 * @param args
+	 * @throws AstCreatorException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, AstCreatorException
+	{
+		if (args.length == 0)
+		{
+			help();
+		}
+		
+		if(args.length==2)
+		{
+			String grammarFilePath = args[0];
+			grammarFilePath= grammarFilePath.replace('/', File.separatorChar).replace('\\', File.separatorChar);
+			String outputPath = args[1];
+			outputPath=outputPath.replace('/', File.separatorChar).replace('\\', File.separatorChar);
+			Main.create(grammarFilePath, new File(outputPath), true);
+		}
+	
+		
+	}
+
+	private static void help()
+	{
+		StringBuffer buf = new StringBuffer();
+		buf.append("Generates a AST from a grammar file.\n");
+		buf.append("generator [grammer file] [output path]\n\n");
+		buf.append("grammer file: The file path to the grammar file to be generated.\n");
+		buf.append("output path: The output path to the folder where the ");
+		buf.append("              generated sources should be placed\n");
+		//buf.append("tostring grammar file: Optional argument for a toString grammar file.\n");
+
+		System.out.println(buf.toString());
+
+	}
+
+}
