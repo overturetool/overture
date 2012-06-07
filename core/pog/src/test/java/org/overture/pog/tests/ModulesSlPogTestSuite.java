adad320d349f4af061a80e0cadbbd6a16bb67106
Augusto Ribeiro <ari@iha.dk>
2012-06-07 12:58:18 +0200

    Changing LexLocation to be only filename the tests

--- core/pog/src/test/java/org/overture/pog/tests/ModulesSlPogTestSuite.java
+++ core/pog/src/test/java/org/overture/pog/tests/ModulesSlPogTestSuite.java
@@ -8,11 +8,13 @@ import junit.framework.TestSuite;
 
 import org.overture.pog.tests.framework.ModuleSlPoTestCase;
 import org.overturetool.test.framework.BaseTestSuite;
+import org.overturetool.vdmj.lex.LexLocation;
 
 public class ModulesSlPogTestSuite extends BaseTestSuite
 {
 	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
 	{
+		LexLocation.absoluteToStringLocation = false;
 		String name = "POG Modules TestSuite";
 		String root = "src\\test\\resources\\modules";
 		TestSuite test =  createTestCompleteFile(name, root, ModuleSlPoTestCase.class);
--- core/typechecker/src/test/java/org/overture/typechecker/tests/ModulesSlTypeCheckTest.java
+++ core/typechecker/src/test/java/org/overture/typechecker/tests/ModulesSlTypeCheckTest.java
@@ -8,11 +8,13 @@ import junit.framework.TestSuite;
 
 import org.overture.typechecker.tests.framework.ModuleTestCase;
 import org.overturetool.test.framework.BaseTestSuite;
+import org.overturetool.vdmj.lex.LexLocation;
 
 public class ModulesSlTypeCheckTest extends BaseTestSuite
 {
 	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
 	{
+		LexLocation.absoluteToStringLocation = false;
 		String name = "Type Check Modules TestSuite";
 		String root = "src\\test\\resources\\modules";
 		TestSuite test =  createTestCompleteFile(name, root, ModuleTestCase.class);
