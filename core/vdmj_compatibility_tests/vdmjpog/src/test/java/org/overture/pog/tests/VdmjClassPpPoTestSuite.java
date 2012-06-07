d87d80a9006a2e156006ece87b43f8a9ddf1d0df
Augusto Ribeiro <ari@iha.dk>
2012-06-07 12:53:26 +0200

    Changing LexLocation to be only filename the tests

--- core/vdmj_compatibility_tests/vdmjpog/src/test/java/org/overture/pog/tests/VdmjClassPpPoTestSuite.java
+++ core/vdmj_compatibility_tests/vdmjpog/src/test/java/org/overture/pog/tests/VdmjClassPpPoTestSuite.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.overture.pog.tests.framework.BaseTestSuite;
 import org.overture.pog.tests.framework.VdmjClassPpPoTestCase;
+import org.overturetool.vdmj.lex.LexLocation;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -15,6 +16,7 @@ public class VdmjClassPpPoTestSuite extends BaseTestSuite {
 	
 	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
 	{
+		LexLocation.absoluteToStringLocation = false;
 		String name = "VDMJ PO Class PP TestSuite";
 		String root = "src\\test\\resources\\classesPP\\";
 		TestSuite test = createTestCompleteFile(name, root, VdmjClassPpPoTestCase.class);
