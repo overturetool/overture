7bb82c0fec4573cf91724e061853659ac6fb2868
Augusto Ribeiro <augusto@aribeiro.net>
2012-06-18 23:14:56 +0200

    Forgot to commit one file in the previous commits

--- core/interpreter/src/main/java_/org/overture/interpreter/assistant/type/AUnionTypeAssistantInterpreter.java
+++ core/interpreter/src/main/java_/org/overture/interpreter/assistant/type/AUnionTypeAssistantInterpreter.java
@@ -1,17 +1,25 @@
 package org.overture.interpreter.assistant.type;
 
 import org.overture.ast.types.AUnionType;
+import org.overture.ast.types.PType;
 import org.overture.interpreter.runtime.Context;
+import org.overture.interpreter.runtime.ValueException;
 import org.overture.interpreter.values.ValueList;
 import org.overture.typechecker.assistant.type.AUnionTypeAssistantTC;
 
 public class AUnionTypeAssistantInterpreter extends AUnionTypeAssistantTC
 {
 
-	public static ValueList getAllValues(AUnionType type, Context ctxt)
+	public static ValueList getAllValues(AUnionType utype, Context ctxt) throws ValueException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		ValueList v = new ValueList();
+
+		for (PType type: utype.getTypes())
+		{
+			v.addAll(PTypeAssistantInterpreter.getAllValues(type,ctxt));
+		}
+
+		return v;
 	}
 
 }
