99de41dd3ce740992b1045f94b65ee401b856d67
Kenneth Lausdahl <kel@iha.dk>
2012-06-20 22:31:34 +0200

    added interpreter sl tests

--- core/interpreter/src/main/java/org/overture/interpreter/util/InterpreterUtil.java
+++ core/interpreter/src/main/java/org/overture/interpreter/util/InterpreterUtil.java
@@ -1,9 +1,15 @@
 package org.overture.interpreter.util;
 
+import java.io.File;
+import java.util.List;
+
+import org.overture.ast.modules.AModuleModules;
 import org.overture.interpreter.runtime.ClassInterpreter;
 import org.overture.interpreter.runtime.Interpreter;
 import org.overture.interpreter.runtime.ModuleInterpreter;
 import org.overture.interpreter.values.Value;
+import org.overture.typechecker.util.TypeCheckerUtil;
+import org.overture.typechecker.util.TypeCheckerUtil.TypeCheckResult;
 
 public class InterpreterUtil
 {
@@ -18,12 +24,30 @@ public class InterpreterUtil
 	{
 		return new ClassInterpreter(classes);
 	}
-	
+
 	public static Value interpret(String content) throws Exception
 	{
-		Interpreter interpreter =getInterpreter(new ModuleListInterpreter());
+		
+		Interpreter interpreter = getInterpreter(new ModuleListInterpreter());
 		interpreter.init(null);
 		Value val = interpreter.execute(content, null);
 		return val;
+	}
+
+	public static Value interpret(String entry, File file) throws Exception
+	{
+		TypeCheckResult<List<AModuleModules>> result = TypeCheckerUtil.typeCheckSl(file);
+
+		if (result.errors.isEmpty())
+		{
+			ModuleListInterpreter list = new ModuleListInterpreter();
+			list.addAll(result.result);
+			Interpreter interpreter = getInterpreter(list);
+			interpreter.init(null);
+			interpreter.setDefaultName(list.get(0).getName().name);
+			Value val = interpreter.execute(entry, null);
+			return val;
+		}
+		return null;
 	}
 }
---
+++ core/interpreter/src/test/java/org/overture/interpreter/tests/ModulesSlInterpreterTestSuite.java
@@ -0,0 +1,22 @@
+package org.overture.interpreter.tests;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.overture.ast.lex.LexLocation;
+import org.overturetool.test.framework.BaseTestSuite;
+
+public class ModulesSlInterpreterTestSuite extends BaseTestSuite
+{
+	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
+	{
+		LexLocation.absoluteToStringLocation = false;
+		String name = "Interpreter Modules TestSuite";
+		String root = "src\\test\\resources\\modules";
+		TestSuite test =  createTestCompleteFile(name, root, InterpreterSlTestCase.class);
+		return test;
+	}
+}
