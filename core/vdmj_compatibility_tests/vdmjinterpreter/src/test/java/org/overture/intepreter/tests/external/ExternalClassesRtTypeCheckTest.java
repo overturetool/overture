ee112f70c184be15e765bf94f74fa6158fb067b2
Augusto Ribeiro <ari@iha.dk>
2012-07-03 15:56:35 +0200

    Adding test cases for VDMRT

--- core/vdmj_compatibility_tests/vdmjinterpreter/src/test/java/org/overture/intepreter/tests/external/ExternalClassesRtTypeCheckTest.java
+++ core/vdmj_compatibility_tests/vdmjinterpreter/src/test/java/org/overture/intepreter/tests/external/ExternalClassesRtTypeCheckTest.java
@@ -1,34 +1,34 @@
-//package org.overture.intepreter.tests.external;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.lang.reflect.InvocationTargetException;
-//
-//import junit.framework.Test;
-//import junit.framework.TestSuite;
-//
-//import org.overturetool.test.framework.BaseTestSuite;
-//import org.overturetool.test.framework.Properties;
-//import org.overturetool.vdmj.lex.LexLocation;
-//
-//public class ExternalClassesRtTypeCheckTest extends BaseTestSuite
-//{
-//	public static Test suite() throws IllegalArgumentException,
-//			SecurityException, InstantiationException, IllegalAccessException,
-//			InvocationTargetException, NoSuchMethodException, IOException
-//	{
-//		LexLocation.absoluteToStringLocation = false;
-//		Properties.recordTestResults = true;
-//		String name = "Type_Check_RT_Classes_TestSuite_External";
-//		File root = ExternalTestSettings.getBasePath("rttest/tc");
-//		TestSuite test = null;
-//		if (root != null && root.exists())
-//		{
-//			test = createTestCompleteFile(name, root.getAbsolutePath(), NewClassRtTestCase.class);
-//		} else
-//		{
-//			test = new TestSuite("Empty Skipped Test Suite");
-//		}
-//		return test;
-//	}
-//}
+package org.overture.intepreter.tests.external;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.overturetool.test.framework.BaseTestSuite;
+import org.overturetool.test.framework.Properties;
+import org.overturetool.vdmj.lex.LexLocation;
+
+public class ExternalClassesRtTypeCheckTest extends BaseTestSuite
+{
+	public static Test suite() throws IllegalArgumentException,
+			SecurityException, InstantiationException, IllegalAccessException,
+			InvocationTargetException, NoSuchMethodException, IOException
+	{
+		LexLocation.absoluteToStringLocation = false;
+		Properties.recordTestResults = true;
+		String name = "Interpreter_RT_Classes_TestSuite_External";
+		File root = ExternalTestSettings.getBasePath("rttest/cgip");
+		TestSuite test = null;
+		if (root != null && root.exists())
+		{
+			test = createTestCompleteFile(name, root.getAbsolutePath(), NewClassRtTestCase.class,"vpp");
+		} else
+		{
+			test = new TestSuite("Empty Skipped Test Suite");
+		}
+		return test;
+	}
+}
--- core/vdmj_compatibility_tests/vdmjinterpreter/src/test/java/org/overture/intepreter/tests/external/NewClassRtTestCase.java
+++ core/vdmj_compatibility_tests/vdmjinterpreter/src/test/java/org/overture/intepreter/tests/external/NewClassRtTestCase.java
@@ -1,74 +1,73 @@
-//package org.overture.intepreter.tests.external;
-//
-//import java.io.File;
-//
-//import org.overture.vdmj.typechecker.tests.OvertureTestHelper;
-//import org.overturetool.test.framework.TestResourcesResultTestCase;
-//import org.overturetool.vdmj.Release;
-//import org.overturetool.vdmj.Settings;
-//import org.overturetool.vdmj.lex.Dialect;
-//import org.overturetool.vdmj.typechecker.TypeChecker;
-//import org.w3c.dom.Document;
-//import org.w3c.dom.Element;
-//import org.w3c.dom.Node;
-//
-//public class NewClassRtTestCase extends TestResourcesResultTestCase<Boolean>
-//{
-//	public NewClassRtTestCase()
-//	{
-//		super();
-//	}
-//
-//	public NewClassRtTestCase(File file)
-//	{
-//		super(file);
-//	}
-//
-//	public NewClassRtTestCase(File rootSource,String name, String content)
-//	{
-//		super(rootSource,name, content);
-//	}
-//
-//	public NewClassRtTestCase(File file, String suiteName, File testSuiteRoot)
-//	{
-//		super(file, suiteName, testSuiteRoot);
-//	}
-//
-//	@Override
-//	public void test() throws Exception
-//	{
-//		assertNotNull("File not set", file);
-//		compareResults(new OvertureTestHelper().typeCheckRt(file), file.getName() + ".result");
-//
-//	}
-//
-//	@Override
-//	protected void setUp() throws Exception
-//	{
-//		super.setUp();
-//		Settings.dialect = Dialect.VDM_RT;
-//		Settings.release = Release.VDM_10;
-//		TypeChecker.clearErrors();
-//	}
-//
-//	public void encondeResult(Boolean result, Document doc,
-//			Element resultElement)
-//	{
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	public Boolean decodeResult(Node node)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	protected boolean assertEqualResults(Boolean expected, Boolean actual)
-//	{
-//		// TODO Auto-generated method stub
-//		return true;
-//	}
-//
-//}
+package org.overture.intepreter.tests.external;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.overture.interpreter.tests.framework.ClassTestCase;
+import org.overture.interpreter.tests.framework.ModuleTestCase;
+import org.overturetool.vdmj.Release;
+import org.overturetool.vdmj.Settings;
+import org.overturetool.vdmj.lex.Dialect;
+import org.overturetool.vdmj.typechecker.TypeChecker;
+
+public class NewClassRtTestCase extends ClassTestCase
+{
+	public NewClassRtTestCase()
+	{
+		super();
+	}
+
+	public NewClassRtTestCase(File file)
+	{
+		super(file);
+	}
+
+	public NewClassRtTestCase(File rootSource,String name, String content)
+	{
+		super(rootSource,name, content);
+	}
+
+	public NewClassRtTestCase(File file, String suiteName, File testSuiteRoot)
+	{
+		super(file, suiteName, testSuiteRoot);
+	}
+
+	
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		Settings.dialect = Dialect.VDM_RT;
+		Settings.release = Release.VDM_10;
+		TypeChecker.clearErrors();
+	}
+
+	@Override
+	protected String baseExamplePath()
+	{
+		return file.getParentFile().getAbsolutePath();
+	}
+
+
+	protected String search(File file, String name) throws IOException
+	{
+		File readme = new File(file,(name.contains(".")?name.substring(0,name.lastIndexOf('.'))+".assert": name+".assert"));
+		if (readme.exists())
+		{
+			BufferedReader reader = new BufferedReader(new FileReader(readme));
+			String text = null;
+			String entry ="";
+			while ((text = reader.readLine()) != null)
+			{
+				entry += text;
+			}
+			entry = entry.replace('\n', ' ');
+			reader.close();
+			return entry;
+		}
+		return null;
+	}
+}
