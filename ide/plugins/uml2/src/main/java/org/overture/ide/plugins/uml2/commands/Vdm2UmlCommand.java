ded562d7d3d68b3dc31b81e81ac6f11958bb3e53
Kenneth Lausdahl <kel@iha.dk>
2012-07-12 11:19:51 +0200

    added refresh for uml2

--- ide/plugins/uml2/src/main/java/org/overture/ide/plugins/uml2/commands/Vdm2UmlCommand.java
+++ ide/plugins/uml2/src/main/java/org/overture/ide/plugins/uml2/commands/Vdm2UmlCommand.java
@@ -7,6 +7,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -76,7 +79,12 @@ public class Vdm2UmlCommand extends AbstractHandler
 						try
 						{
 							vdm2uml.save(uri);
+							project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
 						} catch (IOException e)
+						{
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (CoreException e)
 						{
 							// TODO Auto-generated catch block
 							e.printStackTrace();
