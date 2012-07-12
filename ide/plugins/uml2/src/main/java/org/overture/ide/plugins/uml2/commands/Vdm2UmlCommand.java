bd1c36f0a23075aa33d4b46a9ec1709f328aba72
Kenneth Lausdahl <kel@iha.dk>
2012-07-12 10:26:28 +0200

    added uml2 til the ide build fixed TC in uml2 to use the core API

--- ide/plugins/uml2/src/main/java/org/overture/ide/plugins/uml2/commands/Vdm2UmlCommand.java
+++ ide/plugins/uml2/src/main/java/org/overture/ide/plugins/uml2/commands/Vdm2UmlCommand.java
@@ -1,6 +1,5 @@
 package org.overture.ide.plugins.uml2.commands;
 
-
 import java.io.IOException;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -12,61 +11,73 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.overture.ast.util.definitions.ClassList;
 import org.overture.ide.core.IVdmModel;
 import org.overture.ide.core.ast.NotAllowedException;
 import org.overture.ide.core.resources.IVdmProject;
 import org.overture.ide.plugins.uml2.vdm2uml.Vdm2Uml;
-import org.overture.typechecker.ClassTypeChecker;
+import org.overture.ide.ui.utility.VdmTypeCheckerUi;
 
-public class Vdm2UmlCommand extends AbstractHandler {
+//import org.overture.typechecker.ClassTypeChecker;
 
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
+public class Vdm2UmlCommand extends AbstractHandler
+{
+
+	public Object execute(ExecutionEvent event) throws ExecutionException
+	{
+
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
-		
-		if(selection instanceof IStructuredSelection)
+
+		if (selection instanceof IStructuredSelection)
 		{
 			IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-			
+
 			Object firstElement = structuredSelection.getFirstElement();
-			if(firstElement instanceof IProject)
+			if (firstElement instanceof IProject)
 			{
 				IProject project = ((IProject) firstElement);
 				IVdmProject vdmProject = (IVdmProject) project.getAdapter(IVdmProject.class);
-				
-				if(vdmProject == null)
+
+				if (vdmProject == null)
 				{
 					return null;
 				}
 				
-				IVdmModel model = vdmProject.getModel();
-				if(model.isParseCorrect())
+				final IVdmModel model = vdmProject.getModel();
+				if (model.isParseCorrect())
 				{
-					ClassList classes = null;
-					try {
-						classes = model.getClassList();
-					} catch (NotAllowedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					if(!model.isTypeChecked())
+					
+
+					if (!model.isParseCorrect())
 					{
-						
-						ClassTypeChecker tc = new ClassTypeChecker(classes);
-						tc.typeCheck();
+						return null;
+						//return new Status(Status.ERROR, IPoviewerConstants.PLUGIN_ID, "Project contains parse errors");
 					}
 
-					if (ClassTypeChecker.getErrorCount() == 0) {
+					if (model == null || !model.isTypeCorrect())
+					{
+						VdmTypeCheckerUi.typeCheck(HandlerUtil.getActiveShell(event), vdmProject);
+					}
+
+					if (model.isTypeCorrect())
+					{
 						Vdm2Uml vdm2uml = new Vdm2Uml();
-						vdm2uml.init(classes);
+						try
+						{
+							vdm2uml.init(model.getClassList());
+						} catch (NotAllowedException e1)
+						{
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 
 						IFile iFile = project.getFile(project.getName());
 						java.net.URI absolutePath = iFile.getLocationURI();
 						URI uri = URI.createFileURI(absolutePath.getPath());
-						try {
+						try
+						{
 							vdm2uml.save(uri);
-						} catch (IOException e) {
+						} catch (IOException e)
+						{
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
@@ -78,10 +89,8 @@ public class Vdm2UmlCommand extends AbstractHandler {
 			}
 
 		}
-		
+
 		return null;
 	}
-
-	
 
 }
