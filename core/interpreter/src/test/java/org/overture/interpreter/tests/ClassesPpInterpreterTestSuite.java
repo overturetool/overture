64345b9c691b66344aad8fd4e392f52091b21c60
Augusto Ribeiro <ari@iha.dk>
2012-08-06 16:41:29 +0200

    LexLocation was absolute for the ClassPpInterpreter

--- core/interpreter/src/test/java/org/overture/interpreter/tests/ClassesPpInterpreterTestSuite.java
+++ core/interpreter/src/test/java/org/overture/interpreter/tests/ClassesPpInterpreterTestSuite.java
@@ -6,12 +6,14 @@ import java.lang.reflect.InvocationTargetException;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
+import org.overture.ast.lex.LexLocation;
 import org.overturetool.test.framework.BaseTestSuite;
 
 public class ClassesPpInterpreterTestSuite extends BaseTestSuite
 {
 	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
 	{
+		LexLocation.absoluteToStringLocation = false;
 		org.overturetool.test.framework.Properties.recordTestResults = false;
 		String name = "Interpreter Class PP TestSuite";
 		String root = "src\\test\\resources\\classes";
