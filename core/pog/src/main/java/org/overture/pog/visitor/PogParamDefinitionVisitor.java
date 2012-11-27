53cf404fd52fe2568b9091be659f00200a115c7a
Luis D Couto <lcouto@iha.dk>
2012-11-27 17:21:52 +0100

    incorporated visitor chanegs in compasss ast.

---
+++ core/pog/src/main/java/org/overture/pog/visitor/PogParamDefinitionVisitor.java
@@ -0,0 +1,14 @@
+package org.overture.pog.visitor;
+
+import org.overture.ast.analysis.QuestionAnswerAdaptor;
+import org.overture.pog.obligation.POContextStack;
+import org.overture.pog.obligation.ProofObligationList;
+
+public class PogParamDefinitionVisitor<Q extends POContextStack , A extends ProofObligationList> extends QuestionAnswerAdaptor<Q, A>  
+
+{
+
+    
+	
+    
+}
