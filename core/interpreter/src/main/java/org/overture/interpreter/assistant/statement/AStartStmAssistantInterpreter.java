c51a8954b964ca01af8d44389b9cb7625bc78019
Peter Joergensen <pvj@iha.dk>
2013-03-18 21:54:34 +0100

    Fixes for bugs revealed by the ConcGridManagerPP model. Following fixes have been made:
    1) The type check of the periodic statement has been corrected
    
    The typechecker now handles the VDMPP and VDMRT periodic statements (4 args and 1 arg, respectively) correctly.
    
    In addition, the typecheck works for the the general version of the statement with expressions rather than literals only.
    
    2) The evaluation of the periodic statement has been implemented (it was missing)
    
    Now period, jitter, delay and offset are set properly.
    
    3) The evaluation of the start statement has been corrected so that it now evaluates the periodic statement

--- core/interpreter/src/main/java/org/overture/interpreter/assistant/statement/AStartStmAssistantInterpreter.java
+++ core/interpreter/src/main/java/org/overture/interpreter/assistant/statement/AStartStmAssistantInterpreter.java
@@ -1,5 +1,6 @@
 package org.overture.interpreter.assistant.statement;
 
+import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.expressions.PExp;
 import org.overture.ast.statements.APeriodicStm;
 import org.overture.ast.statements.AStartStm;
@@ -8,7 +9,7 @@ import org.overture.interpreter.runtime.ClassInterpreter;
 import org.overture.interpreter.runtime.Context;
 import org.overture.interpreter.runtime.ObjectContext;
 import org.overture.interpreter.runtime.RootContext;
-import org.overture.interpreter.runtime.ValueException;
+import org.overture.interpreter.runtime.VdmRuntime;
 import org.overture.interpreter.scheduler.ObjectThread;
 import org.overture.interpreter.scheduler.PeriodicThread;
 import org.overture.interpreter.values.ObjectValue;
@@ -17,21 +18,26 @@ import org.overture.interpreter.values.OperationValue;
 public class AStartStmAssistantInterpreter
 {
 
-	public static void start(AStartStm node, ObjectValue target, OperationValue op, Context ctxt) throws ValueException
+	public static void start(AStartStm node, ObjectValue target, OperationValue op, Context ctxt) throws AnalysisException
 	{
 		if (op.body instanceof APeriodicStm)
 		{
     		RootContext global = ClassInterpreter.getInstance().initialContext;
     		Context pctxt = new ObjectContext(op.name.location, "async", global, target);
-
     		APeriodicStm ps = (APeriodicStm)op.body;
+    		
+    		// We disable the swapping and time (RT) as periodic evaluation should be "free".
+    		pctxt.threadState.setAtomic(true);
+    		ps.apply(VdmRuntime.getStatementEvaluator(), pctxt); // Ignore return value
+    		pctxt.threadState.setAtomic(false);
+    		
 			OperationValue pop = pctxt.lookup(ps.getOpname()).operationValue(pctxt);
 
 			long period = ps.getPeriod();
 			long jitter = ps.getJitter();
 			long delay  = ps.getDelay();
 			long offset = ps.getOffset();
-
+			
 			// Note that periodic threads never set the stepping flag
 
 			new PeriodicThread(
