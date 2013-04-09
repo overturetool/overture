dfe689e96a4d546022944ff2a08ddb72fa957ce9
Peter W. V. Joergensen <pvj@iha.dk>
2013-04-09 15:38:32 +0200

    Bug fix for break point toggling. Now the executable analysis properly determines if the break point line in fact holds executable code.

--- ide/debug/src/main/java/org/overture/ide/debug/utils/ExecutableAnalysis.java
+++ ide/debug/src/main/java/org/overture/ide/debug/utils/ExecutableAnalysis.java
@@ -1,7 +1,10 @@
 package org.overture.ide.debug.utils;
 
+import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.analysis.DepthFirstAnalysisAdaptor;
+import org.overture.ast.definitions.SClassDefinition;
 import org.overture.ast.expressions.PExp;
+import org.overture.ast.modules.AModuleModules;
 import org.overture.ast.node.INode;
 import org.overture.ast.statements.PStm;
 
@@ -11,46 +14,86 @@ public class ExecutableAnalysis extends DepthFirstAnalysisAdaptor
 	 * 
 	 */
 	private static final long serialVersionUID = -5353696074294132014L;
-	protected boolean executable = false;
 	private final int searchLine;
-
-	private ExecutableAnalysis(int searchLine)
+	
+	private String module = null;
+	
+	private ExecutableAnalysis(int searchLine, String module)
 	{
 		this.searchLine = searchLine;
+		this.module = module;
 	}
-
-	public static boolean isExecutable(INode node, int line)
+	
+	public static boolean isExecutable(INode node, int line, boolean findModule)
 	{
-		ExecutableAnalysis analysis = new ExecutableAnalysis(line);
+		
+		String nodeModule = findModule ? searchForModule(node) : null;
+				
+		ExecutableAnalysis analysis = new ExecutableAnalysis(line, nodeModule);
 
 		try
 		{
 			node.apply(analysis);
-		} catch (Throwable e)
+		} catch (ExecutableAnalysisException e)
 		{
-			// TODO Auto-generated catch block
+			return e.isExecutable();
+			
+		} catch (AnalysisException e)
+		{
 			e.printStackTrace();
 			return false;
 		}
 
-		return analysis.executable;
+		return false;
+	}
+	
+	private static String searchForModule(INode node)
+	{
+		
+		String nodeModule = null;
+		
+		SClassDefinition classDef = node.getAncestor(SClassDefinition.class); 
+		
+		if(classDef != null)
+		{
+			nodeModule = classDef.getLocation().module;
+		}
+		else
+		{
+			AModuleModules slModule = node.getAncestor(AModuleModules.class);
+			
+			if(slModule != null)
+			{
+				nodeModule = slModule.getName().name;
+			}
+		}
+		
+		return nodeModule;
+	}
+	
+	private boolean isValidModule(PStm node){
+		
+		return module == null || (node.getLocation().startLine == searchLine &&  module.equals(node.getLocation().module));
+	}
+	
+	private boolean isValidModule(PExp node){
+		
+		return module == null || (node.getLocation().startLine == searchLine &&  module.equals(node.getLocation().module));
 	}
 
-	public void defaultInPStm(PStm node)
+	public void defaultInPStm(PStm node) throws ExecutableAnalysisException
 	{
-		if (node.getLocation().startLine == searchLine)
+		if (isValidModule(node))
 		{
-			executable = true;
-			return;
+			throw new ExecutableAnalysisException(true);
 		}
 	}
 
-	public void defaultInPExp(PExp node)
+	public void defaultInPExp(PExp node) throws ExecutableAnalysisException
 	{
-		if (node.getLocation().startLine == searchLine)
+		if (isValidModule(node))
 		{
-			executable = true;
-			return;
+			throw new ExecutableAnalysisException(true);
 		}
 	}
 }
