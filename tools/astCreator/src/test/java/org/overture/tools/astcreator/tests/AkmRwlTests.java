18f4137b48b7b0b70ff5e638d6660d42a0c2048f
Kenneth Lausdahl <kel@iha.dk>
2013-07-03 15:20:15 +0200

    updated ast creator to version 1.2.1 now ignoring the enumeration classes which are empty

--- tools/astCreator/src/test/java/org/overture/tools/astcreator/tests/AkmRwlTests.java
+++ tools/astCreator/src/test/java/org/overture/tools/astcreator/tests/AkmRwlTests.java
@@ -1,59 +1,59 @@
-package org.overture.tools.astcreator.tests;
-
-import java.io.File;
-import java.io.InputStream;
-
-import junit.framework.TestCase;
-
-import org.overture.tools.astcreator.Main;
-
-public class AkmRwlTests extends TestCase {
-
-	//	private String src = "Packages\n" + "base org.overture.ast.node;\n"
-	//			+ "analysis org.overture.ast.analysis;\n" + "Tokens\n" + "\n"
-	//			+ "Abstract Syntax Tree\n"
-	//			+ "exp {-> package='org.overture.ast.expressions'}\n"
-	//			+ "    =   {binary} [left]:exp [op]:binop [right]:exp\n"
-	//			+ "    ;\n" + "binop {-> package='org.overture.ast.expressions'}\n"
-	//			+ "      = {and}" + "      " + "      | {or} \n" + "      ;\n"
-	//			+ "Aspect Declaration\n";
-	//
-	//	private String src2 = "Packages\n" + "base eu.compassresearch.ast.node;\n"
-	//			+ "analysis eu.compassresearch.ast.analysis;\n" + "Tokens\n" + "\n"
-	//			+ "Abstract Syntax Tree\n"
-	//			+ "cml {-> package='eu.compassresearch.ast.expressions'}\n"
-	//			+ "    =   {binary} [left]:exp [op]:binop [right]:exp\n"
-	//			+ "    ;\n" + "Aspect Declaration\n";
-
-	/*
-	 * public void testBasic() throws Exception { System.out.println(new
-	 * File(".").getAbsolutePath()); File output = new File(
-	 * FilePathUtil.getPlatformPath("target/testData/simple/srcsrc2"));
-	 * 
-	 * ByteArrayInputStream input1 = new ByteArrayInputStream(src.getBytes());
-	 * ByteArrayInputStream input2 = new ByteArrayInputStream(src2.getBytes());
-	 * 
-	 * Main.create(input1, input2, output, "Cml", false);
-	 * 
-	 * }
-	 */
-
-	public void testCmlAndOvt() throws Exception {
-		File output = new File(
-				FilePathUtil.getPlatformPath("target/testData/akmGeneratedCode"));
-		InputStream ovt = getClass().getResourceAsStream("/cml.ast");
-		InputStream cml = getClass().getResourceAsStream("/overtureII.astv2");
-		InputStream ovtToString = getClass().getResourceAsStream(
-				"/overtureII.astv2.tostring");
-		InputStream cmlToString = getClass().getResourceAsStream(
-				"/cml.ast.tostring");
-
-		Main.create(ovtToString, cmlToString, ovt, cml, output, "Cml", false, true);
-
-		// Main.create(ovt, output, true, false);
-	}
-
-
-
-	volatile boolean b;
-}
+//package org.overture.tools.astcreator.tests;
+//
+//import java.io.File;
+//import java.io.InputStream;
+//
+//import junit.framework.TestCase;
+//
+//import org.overture.tools.astcreator.Main;
+//
+//public class AkmRwlTests extends TestCase {
+//
+//	//	private String src = "Packages\n" + "base org.overture.ast.node;\n"
+//	//			+ "analysis org.overture.ast.analysis;\n" + "Tokens\n" + "\n"
+//	//			+ "Abstract Syntax Tree\n"
+//	//			+ "exp {-> package='org.overture.ast.expressions'}\n"
+//	//			+ "    =   {binary} [left]:exp [op]:binop [right]:exp\n"
+//	//			+ "    ;\n" + "binop {-> package='org.overture.ast.expressions'}\n"
+//	//			+ "      = {and}" + "      " + "      | {or} \n" + "      ;\n"
+//	//			+ "Aspect Declaration\n";
+//	//
+//	//	private String src2 = "Packages\n" + "base eu.compassresearch.ast.node;\n"
+//	//			+ "analysis eu.compassresearch.ast.analysis;\n" + "Tokens\n" + "\n"
+//	//			+ "Abstract Syntax Tree\n"
+//	//			+ "cml {-> package='eu.compassresearch.ast.expressions'}\n"
+//	//			+ "    =   {binary} [left]:exp [op]:binop [right]:exp\n"
+//	//			+ "    ;\n" + "Aspect Declaration\n";
+//
+//	/*
+//	 * public void testBasic() throws Exception { System.out.println(new
+//	 * File(".").getAbsolutePath()); File output = new File(
+//	 * FilePathUtil.getPlatformPath("target/testData/simple/srcsrc2"));
+//	 * 
+//	 * ByteArrayInputStream input1 = new ByteArrayInputStream(src.getBytes());
+//	 * ByteArrayInputStream input2 = new ByteArrayInputStream(src2.getBytes());
+//	 * 
+//	 * Main.create(input1, input2, output, "Cml", false);
+//	 * 
+//	 * }
+//	 */
+//
+//	public void testCmlAndOvt() throws Exception {
+//		File output = new File(
+//				FilePathUtil.getPlatformPath("target/testData/akmGeneratedCode"));
+//		InputStream ovt = getClass().getResourceAsStream("/cml.ast");
+//		InputStream cml = getClass().getResourceAsStream("/overtureII.astv2");
+//		InputStream ovtToString = getClass().getResourceAsStream(
+//				"/overtureII.astv2.tostring");
+//		InputStream cmlToString = getClass().getResourceAsStream(
+//				"/cml.ast.tostring");
+//
+//		Main.create(ovtToString, cmlToString, ovt, cml, output, "Cml", false, true);
+//
+//		// Main.create(ovt, output, true, false);
+//	}
+//
+//
+//
+//	volatile boolean b;
+//}
