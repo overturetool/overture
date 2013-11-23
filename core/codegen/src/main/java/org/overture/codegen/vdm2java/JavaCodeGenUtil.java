c7f042ca0937af59adccb48039c231cdb9bd8485
Peter Joergensen <pvj@iha.dk>
2013-11-23 01:51:18 +0100

    Improved Java codegen util by adding new interfaces for generating Java from VDM

--- core/codegen/src/main/java/org/overture/codegen/vdm2java/JavaCodeGenUtil.java
+++ core/codegen/src/main/java/org/overture/codegen/vdm2java/JavaCodeGenUtil.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.overture.ast.analysis.AnalysisException;
@@ -17,6 +18,9 @@ import org.overture.codegen.utils.GeneratedData;
 import org.overture.codegen.utils.GeneratedModule;
 import org.overture.codegen.utils.InvalidNamesException;
 import org.overture.codegen.utils.NameViolation;
+import org.overture.interpreter.VDMRT;
+import org.overture.interpreter.util.ClassListInterpreter;
+import org.overture.interpreter.util.ExitStatus;
 import org.overture.typechecker.util.TypeCheckerUtil.TypeCheckResult;
 
 import de.hunsicker.io.FileFormat;
@@ -42,7 +46,61 @@ public class JavaCodeGenUtil
 					+ e.getMessage());
 		}
 	}
+	
+	public static GeneratedData generateJavaFromFiles(List<File> files) throws AnalysisException, InvalidNamesException
+	{
+		VDMRT vdmrt = new VDMRT();
+		
+		ExitStatus status = vdmrt.parse(files);
+		
+		if(status != ExitStatus.EXIT_OK)
+			throw new AnalysisException("Could not parse files!");
+		
+		status = vdmrt.typeCheck();
 
+		if(status != ExitStatus.EXIT_OK)
+			throw new AnalysisException("Could not type check files!");
+		
+		ClassListInterpreter classes;
+		try
+		{
+			classes = vdmrt.getInterpreter().getClasses();
+		} catch (Exception e)
+		{
+			throw new AnalysisException("Could not get classes from class list interpreter!");
+		}
+		
+		JavaCodeGen vdmCodGen = new JavaCodeGen();
+		List<SClassDefinition> mergedParseList = new LinkedList<SClassDefinition>();
+		
+		for (SClassDefinition vdmClass : classes)
+			mergedParseList.add(vdmClass);
+		
+		List<GeneratedModule> generatedModules = generateJavaFromVdm(mergedParseList, vdmCodGen);
+		
+		GeneratedModule quoteValues = vdmCodGen.generateJavaFromVdmQuotes();
+		
+		GeneratedData dataToReturn = new GeneratedData(generatedModules, quoteValues);
+		
+		return dataToReturn;
+		
+	}
+
+	public static List<GeneratedModule> generateJavaFromVdm(
+			List<SClassDefinition> mergedParseLists) throws AnalysisException, InvalidNamesException
+	{
+		JavaCodeGen vdmCodGen = new JavaCodeGen();
+		return vdmCodGen.generateJavaFromVdm(mergedParseLists);
+	}
+	
+	
+	public static List<GeneratedModule> generateJavaFromVdm(
+			List<SClassDefinition> mergedParseLists, JavaCodeGen vdmCodGen) throws AnalysisException, InvalidNamesException
+	{
+		return vdmCodGen.generateJavaFromVdm(mergedParseLists);
+	}
+
+	
 	public static GeneratedData generateJavaFromFile(File file) throws AnalysisException, InvalidNamesException
 	{
 		return generateJavaFromFiles(new String[]{"", file.getAbsolutePath()});
@@ -50,17 +108,17 @@ public class JavaCodeGenUtil
 	
 	public static GeneratedData generateJavaFromFiles(String[] args) throws AnalysisException, InvalidNamesException
 	{		
-		JavaCodeGen vvdmCodGen = new JavaCodeGen();
+		JavaCodeGen vdmCodGen = new JavaCodeGen();
 		List<GeneratedModule> data = new ArrayList<GeneratedModule>();
 		
 		for (int i = 1; i < args.length; i++)
 		{
 			String fileName = args[i];
 			File file = new File(fileName);
-			data.addAll(generateJava(file, vvdmCodGen));
+			data.addAll(generateJava(file, vdmCodGen));
 		}
 		
-		GeneratedModule quoteValues = vvdmCodGen.generateJavaFromVdmQuotes();
+		GeneratedModule quoteValues = vdmCodGen.generateJavaFromVdmQuotes();
 		
 		GeneratedData dataToReturn = new GeneratedData(data, quoteValues);
 		
