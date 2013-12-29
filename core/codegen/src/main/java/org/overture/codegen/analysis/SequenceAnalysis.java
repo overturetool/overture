554029ab114f46a88f6cc13ce763a7628ceb8586
Peter Joergensen <pvj@iha.dk>
2013-12-29 20:17:13 +0100

    Added minor fix for sequence analysis

--- core/codegen/src/main/java/org/overture/codegen/analysis/SequenceAnalysis.java
+++ core/codegen/src/main/java/org/overture/codegen/analysis/SequenceAnalysis.java
@@ -2,6 +2,7 @@ package org.overture.codegen.analysis;
 
 import org.overture.codegen.cgast.INode;
 import org.overture.codegen.cgast.analysis.AnalysisException;
+import org.overture.codegen.cgast.expressions.AEnumSeqExpCG;
 import org.overture.codegen.cgast.types.ASeqSeqTypeCG;
 
 public class SequenceAnalysis extends AbstractAnalysis
@@ -9,7 +10,7 @@ public class SequenceAnalysis extends AbstractAnalysis
 	@Override
 	public void defaultInINode(INode node) throws AnalysisException
 	{
-		if(node instanceof ASeqSeqTypeCG)
+		if(node instanceof ASeqSeqTypeCG || node instanceof AEnumSeqExpCG)
 		{
 			setFound();
 			throw new AnalysisException();
