839b2e8791831a88e2d61f7fcc15ced6d07001d5
Peter Joergensen <pvj@iha.dk>
2014-01-21 20:44:59 +0100

    Fixes for handling of value semantics for value types (record and tuples) in utility class. Also the code has been improved with additional defensive code

--- core/codegen/src/main/resources/JavaUtils/Utils.java
+++ core/codegen/src/main/resources/JavaUtils/Utils.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -5,7 +6,6 @@ import java.util.Vector;
 
 public class Utils
 {
-
 	public static List seq()
 	{
 		return new Vector();
@@ -13,33 +13,86 @@ public class Utils
 	
 	public static List seq(Object... elements)
 	{
-		Vector vector = new Vector();
+		if(elements == null)
+			throw new IllegalArgumentException("Cannot instantiate sequence from null");
 		
-		for (Object element : elements)
+		return addAll(seq(), elements);
+	}
+	
+	private static List addAll(List to, Object... from)
+	{
+		if(to == null || from == null)
+			throw new IllegalArgumentException("Arguments to addAll cannot be null");
+		
+		for(Object element : from)
 		{
-			vector.add(element);
+			if(element instanceof ValueType)
+			{
+				ValueType valueType = (ValueType) element;
+				element = valueType.clone();
+			}
+			
+			to.add(element);
 		}
 		
-		return vector;
+		return to;
+	}
+	
+	public static List reverse(List seq)
+	{
+		if(seq == null)
+			throw new IllegalArgumentException("Cannot reverse null");
+		
+		List result = seq();
+		
+		addAll(result, seq);
+
+		Collections.reverse(result);
+		
+		return result;
 	}
 	
 	public static List seqTail(List seq)
 	{
+		if(seq == null)
+			throw new IllegalArgumentException("Cannot take tail of null");
+		
 		return seq.subList(1,seq.size());
+	}
+	
+	public static boolean seqEquals(List left, List right)
+	{
+		if(left == null || right == null)
+			throw new IllegalArgumentException("A sequences cannot be compared to null");
+		
+		if(left.size() != right.size())
+			return false;
+		
+		for(int i = 0; i < left.size(); i++)
+			if(!left.get(i).equals(right.get(i)))
+				return false;
+				
+		return true;
 	}
 	
 	public static List seqConc(List left, List right)
 	{
-		List result = new Vector();
-		
-		result.addAll(left);
-		result.addAll(right);
+		if(left == null || right == null)
+			throw new IllegalArgumentException("Cannot concatenate null");
+
+		List result = seq();
+
+		addAll(result, left.toArray());
+		addAll(result, right.toArray());
 		
 		return result;
 	}
 	
 	public static int hashCode(Object... fields)
 	{
+		if(fields == null)
+			throw new IllegalArgumentException("Fields cannot be null");
+
 		int hashcode = 0;
 		
 		for(int i = 0; i < fields.length; i++)
@@ -51,8 +104,11 @@ public class Utils
 		return hashcode;
 	}
 	
-	public static Set<Long> inds(List<? extends Object> seq)
+	public static Set<Long> inds(List seq)
 	{
+		if(seq == null)
+			throw new IllegalArgumentException("Cannot get indices of null");
+		
 		Set<Long> indices = new HashSet<Long>();
 		
 		for(long i = 0; i < seq.size(); i++)
@@ -61,21 +117,6 @@ public class Utils
 		}
 		
 		return indices;
-	}
-	
-	public static boolean seqEquals(List left, List right)
-	{
-		if(left == null || right == null)
-			throw new IllegalArgumentException("Sequence arguments cannot be null");
-		
-		if(left.size() != right.size())
-			return false;
-		
-		for(int i = 0; i < left.size(); i++)
-			if(!left.get(i).equals(right.get(i)))
-				return false;
-				
-		return true;
 	}
 	
 	public static int index(long value)
@@ -93,31 +134,15 @@ public class Utils
 	    }
 	    return (int) value;
 	}
-	
-	@SuppressWarnings("unchecked")
-	public static List reverse(List list)
-	{
-		Vector reversed = new Vector();
 		
-		for(int i = list.size() - 1; i >= 0; i--)
-		{
-			Object current = list.get(i);
-			
-			if(current instanceof ValueType)
-			{
-				ValueType valueType = ((ValueType) current).clone();
-				reversed.add(valueType);
-			}
-			else
-				reversed.add(current);
-				
-		}
-		
-		return reversed;
-	}
-	
 	public static String recordToString(Record record, Object... fields)
 	{
+		if(record == null)
+			throw new IllegalArgumentException("Record cannot be null in recordToString");
+		
+		if(fields == null)
+			throw new IllegalArgumentException("Fields cannot be null in recordToString");
+		
 		StringBuilder str = new StringBuilder();
 
 		str.append(fields[0]);
