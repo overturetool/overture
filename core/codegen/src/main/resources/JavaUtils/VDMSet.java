3d1e357b0d20c3b4ad0c8dd22ec026bb27fcf516
Peter Joergensen <pvj@iha.dk>
2014-01-30 00:37:45 +0100

    Added a VDMSet Java class representing generated VDM sets

---
+++ core/codegen/src/main/resources/JavaUtils/VDMSet.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+public class VDMSet extends HashSet implements ValueType, VDMCollection
+{
+	public VDMSet clone()
+	{
+		VDMSet setClone = new VDMSet();
+
+		for (Object element: this)
+		{
+			if (element instanceof ValueType)
+				element = ((ValueType)element).clone();
+			
+			setClone.add(element);
+		}
+
+		return setClone;
+	}
+}
