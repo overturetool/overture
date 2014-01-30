68327dd02460e6556f988722fe70cd11b9c62bf0
Peter Joergensen <pvj@iha.dk>
2014-01-30 13:01:40 +0100

    Minor import cleanup

--- core/codegen/src/main/java/org/overture/codegen/visitor/DeclVisitorCG.java
+++ core/codegen/src/main/java/org/overture/codegen/visitor/DeclVisitorCG.java
@@ -34,7 +34,6 @@ import org.overture.codegen.cgast.types.PTypeCG;
 import org.overture.codegen.constants.IOoAstConstants;
 import org.overture.codegen.ooast.OoAstInfo;
 import org.overture.codegen.utils.AnalysisExceptionCG;
-import org.overture.typechecker.assistant.definition.PDefinitionAssistantTC;
 
 public class DeclVisitorCG extends AbstractVisitorCG<OoAstInfo, PDeclCG>
 {
