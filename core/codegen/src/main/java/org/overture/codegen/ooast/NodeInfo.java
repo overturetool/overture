96d069c4fe734f0d02a0c1e1fa823a46182a1f20
Peter Joergensen <pvj@iha.dk>
2014-02-12 00:01:32 +0100

    Added equals and hashcode implementations for 'NodeInfo' to prevent duplicate objects in collections

--- core/codegen/src/main/java/org/overture/codegen/ooast/NodeInfo.java
+++ core/codegen/src/main/java/org/overture/codegen/ooast/NodeInfo.java
@@ -30,4 +30,44 @@ public class NodeInfo
 	{
 		return reason;
 	}
+	
+	@Override
+	public int hashCode()
+	{
+		int hash = 0;
+		
+		if(node != null)
+			hash += node.hashCode();
+		
+		if(reason != null)
+			hash += reason.hashCode();
+		
+		return hash;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj == null)
+			return false;
+		
+		if(!(obj instanceof NodeInfo))
+			return false;
+		
+		NodeInfo other = (NodeInfo) obj;
+		
+		if(this.node == null && other.node != null ||
+		   this.node != null && !this.node.equals(other.node))
+		{
+		   return false;
+		}
+		
+		if (this.reason == null && other.reason != null || 
+			this.reason != null && !this.reason.equals(other.reason))
+		{
+			return false;
+		}
+		
+		return true;
+	}
 }
