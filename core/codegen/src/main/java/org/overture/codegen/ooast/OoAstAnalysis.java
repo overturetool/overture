4f3653a3d92c8784ca36f8ead053ada842d4fb93
Peter Joergensen <pvj@iha.dk>
2014-02-18 12:34:48 +0100

    Removed dependency analysis code that is not needed anymore

--- core/codegen/src/main/java/org/overture/codegen/ooast/OoAstAnalysis.java
+++ core/codegen/src/main/java/org/overture/codegen/ooast/OoAstAnalysis.java
@@ -2,24 +2,12 @@ package org.overture.codegen.ooast;
 
 import org.overture.codegen.analysis.vdm.AbstractAnalysis;
 import org.overture.codegen.analysis.vdm.QuoteAnalysis;
-import org.overture.codegen.analysis.vdm.SequenceAnalysis;
-import org.overture.codegen.analysis.vdm.SetAnalysis;
 import org.overture.codegen.analysis.vdm.UtilAnalysis;
 import org.overture.codegen.cgast.analysis.AnalysisException;
 import org.overture.codegen.cgast.declarations.AClassDeclCG;
 
 public class OoAstAnalysis
-{
-	public static boolean usesSets(AClassDeclCG classDecl)
-	{
-		return hasDependency(classDecl, new SetAnalysis());
-	}
-	
-	public static boolean usesSequences(AClassDeclCG classDecl)
-	{
-		return hasDependency(classDecl, new SequenceAnalysis());
-	}
-	
+{	
 	public static boolean usesQuoteLiterals(AClassDeclCG classDecl)
 	{
 		return hasDependency(classDecl, new QuoteAnalysis());
@@ -37,8 +25,8 @@ public class OoAstAnalysis
 			classDecl.apply(analysis);
 		}catch(AnalysisException e)
 		{
-			//If found an exception will be thrown to terminate
-			//the visitor analysis
+			//If we find what we are looking for an exception will
+			//be thrown to terminate the visitor analysis
 		}
 		
 		return analysis.isFound();
