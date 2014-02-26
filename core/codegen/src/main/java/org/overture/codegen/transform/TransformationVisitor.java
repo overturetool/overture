7382c3af09e77da747f639cb896f028e5e3f2a96
Peter Joergensen <pvj@iha.dk>
2014-02-26 22:10:36 +0100

    Changed generation of the 'let be st' statement and sequence comprehensions to use a for loop instead of a while loop

--- core/codegen/src/main/java/org/overture/codegen/transform/TransformationVisitor.java
+++ core/codegen/src/main/java/org/overture/codegen/transform/TransformationVisitor.java
@@ -8,8 +8,8 @@ import org.overture.codegen.cgast.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.cgast.declarations.ALocalVarDeclCG;
 import org.overture.codegen.cgast.expressions.ACompSeqExpCG;
 import org.overture.codegen.cgast.statements.ABlockStmCG;
+import org.overture.codegen.cgast.statements.AForLoopStmCG;
 import org.overture.codegen.cgast.statements.ALetBeStStmCG;
-import org.overture.codegen.cgast.statements.AWhileStmCG;
 import org.overture.codegen.cgast.statements.PStmCG;
 import org.overture.codegen.constants.IJavaCodeGenConstants;
 import org.overture.codegen.constants.JavaTempVarPrefixes;
@@ -38,20 +38,21 @@ public class TransformationVisitor extends DepthFirstAnalysisAdaptor
 		String iteratorName = info.getTempVarNameGen().nextVarName(JavaTempVarPrefixes.ITERATOR_NAME_PREFIX);
 		String successVarName = info.getTempVarNameGen().nextVarName(JavaTempVarPrefixes.SUCCESS_VAR_NAME_PREFIX);
 		
-		AWhileStmCG whileStm = new AWhileStmCG();
-		whileStm.setExp(letBeStStmAssistant.consWhileCondition(node, iteratorName, successVarName));
-		whileStm.setBody(letBeStStmAssistant.consWhileBody(node, iteratorName, successVarName));
+		AForLoopStmCG forLoop = new AForLoopStmCG();
+		forLoop.setInit(letBeStStmAssistant.consIteratorDecl(iteratorName, setName));
+		forLoop.setCond(letBeStStmAssistant.consWhileCondition(node, iteratorName, successVarName));
+		forLoop.setInc(null);
+		forLoop.setBody(letBeStStmAssistant.consWhileBody(node, iteratorName, successVarName));
 		
 		ABlockStmCG block = new ABlockStmCG();
 		
 		LinkedList<ALocalVarDeclCG> blockLocalDefs = block.getLocalDefs();
 		blockLocalDefs.add(letBeStStmAssistant.consSetBindDecl(setName, node));
-		blockLocalDefs.add(letBeStStmAssistant.consIteratorDecl(iteratorName, setName));
 		blockLocalDefs.add(letBeStStmAssistant.consChosenElemenDecl(node));
 		blockLocalDefs.add(letBeStStmAssistant.consSuccessVarDecl(successVarName));
 		
 		LinkedList<PStmCG> blockStms = block.getStatements();
-		blockStms.add(whileStm);
+		blockStms.add(forLoop);
 		blockStms.add(letBeStStmAssistant.consIfCheck(successVarName));
 		
 		parent.replaceChild(node, block);
@@ -73,20 +74,21 @@ public class TransformationVisitor extends DepthFirstAnalysisAdaptor
 		String iteratorName = info.getTempVarNameGen().nextVarName(JavaTempVarPrefixes.ITERATOR_NAME_PREFIX);
 		String resSeqName = node.getVar();
 
-		AWhileStmCG whileStm = new AWhileStmCG();
-		whileStm.setExp(compAssistant.consInstanceCall(compAssistant.consIteratorType(), iteratorName, compAssistant.getSeqTypeCloned(node).getSeqOf(), IJavaCodeGenConstants.HAS_NEXT_ELEMENT_ITERATOR, null));
-		whileStm.setBody(compAssistant.consWhileBody(node, iteratorName, resSeqName));
+		AForLoopStmCG forLoop = new AForLoopStmCG();
+		forLoop.setInit(compAssistant.consIteratorDecl(iteratorName, setName));
+		forLoop.setCond(compAssistant.consInstanceCall(compAssistant.consIteratorType(), iteratorName, compAssistant.getSeqTypeCloned(node).getSeqOf(), IJavaCodeGenConstants.HAS_NEXT_ELEMENT_ITERATOR, null));
+		forLoop.setInc(null);
+		forLoop.setBody(compAssistant.consWhileBody(node, iteratorName, resSeqName));
 
 		//Construct and set up block containing sequence comprehension
 		ABlockStmCG block = new ABlockStmCG();
 		
 		LinkedList<ALocalVarDeclCG> blockLocalDefs = block.getLocalDefs();
 		blockLocalDefs.add(compAssistant.consSetBindDecl(setName, node));
-		blockLocalDefs.add(compAssistant.consIteratorDecl(iteratorName, setName));
 		blockLocalDefs.add(compAssistant.consResultSeqDecl(resSeqName, node));
 		
 		LinkedList<PStmCG> blockStms = block.getStatements();
-		blockStms.add(whileStm);
+		blockStms.add(forLoop);
 
 		//Now replace the statement with the result of the sequence comprehension with the
 		//block statement doing the sequence comprehension and add to this block statement
