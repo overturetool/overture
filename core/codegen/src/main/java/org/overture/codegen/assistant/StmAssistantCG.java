6961b18b19107981be77019c0d9041bbe1920290
Peter Joergensen <pvj@iha.dk>
2014-02-27 17:24:46 +0100

    Added utility functionality for injecting a declaration into a statement block

--- core/codegen/src/main/java/org/overture/codegen/assistant/StmAssistantCG.java
+++ core/codegen/src/main/java/org/overture/codegen/assistant/StmAssistantCG.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.expressions.PExp;
+import org.overture.codegen.cgast.declarations.ALocalVarDeclCG;
 import org.overture.codegen.cgast.expressions.ALetDefExpCG;
 import org.overture.codegen.cgast.expressions.PExpCG;
+import org.overture.codegen.cgast.statements.ABlockStmCG;
 import org.overture.codegen.cgast.statements.ALetDefStmCG;
 import org.overture.codegen.cgast.statements.AReturnStmCG;
 import org.overture.codegen.ooast.OoAstInfo;
@@ -31,5 +33,14 @@ public class StmAssistantCG
 			PExpCG arg = args.get(i).apply(question.getExpVisitor(), question);
 			generatedArgs.add(arg);
 		}
+	}
+	
+	public static void injectDeclAsStm(ABlockStmCG block, ALocalVarDeclCG decl)
+	{
+		ABlockStmCG wrappingBlock = new ABlockStmCG();
+		
+		wrappingBlock.getLocalDefs().add(decl);
+		
+		block.getStatements().add(wrappingBlock);
 	}
 }
