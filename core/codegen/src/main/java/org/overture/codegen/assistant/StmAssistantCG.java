f2369b1b22dd880837a2c8da418015d960cb4e9d
Peter Joergensen <pvj@iha.dk>
2014-02-27 20:15:53 +0100

    Added utility functionality for converting a 'let be st' expression to a 'let be st' statement

--- core/codegen/src/main/java/org/overture/codegen/assistant/StmAssistantCG.java
+++ core/codegen/src/main/java/org/overture/codegen/assistant/StmAssistantCG.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.expressions.PExp;
 import org.overture.codegen.cgast.declarations.ALocalVarDeclCG;
+import org.overture.codegen.cgast.expressions.ALetBeStExpCG;
 import org.overture.codegen.cgast.expressions.ALetDefExpCG;
 import org.overture.codegen.cgast.expressions.PExpCG;
 import org.overture.codegen.cgast.statements.ABlockStmCG;
+import org.overture.codegen.cgast.statements.ALetBeStStmCG;
 import org.overture.codegen.cgast.statements.ALetDefStmCG;
 import org.overture.codegen.cgast.statements.AReturnStmCG;
 import org.overture.codegen.ooast.OoAstInfo;
@@ -16,14 +18,28 @@ public class StmAssistantCG
 {
 	public static ALetDefStmCG convertToLetDefStm(ALetDefExpCG letDefExp)
 	{
-		ALetDefStmCG letDefStm = new ALetDefStmCG();
-		letDefStm.setLocalDefs(letDefExp.getLocalDefs());
-		
 		AReturnStmCG returnStm = new AReturnStmCG();
 		returnStm.setExp(letDefExp.getExp());
+
+		
+		ALetDefStmCG letDefStm = new ALetDefStmCG();
+		
+		letDefStm.setLocalDefs(letDefExp.getLocalDefs());
 		letDefStm.setStm(returnStm);
 		
 		return letDefStm;
+	}
+	
+	public static ALetBeStStmCG convertToLetBeStStm(ALetBeStExpCG letBeStExp)
+	{
+		AReturnStmCG returnStm = new AReturnStmCG();
+		returnStm.setExp(letBeStExp.getValue());
+		
+		ALetBeStStmCG letBeStStm = new ALetBeStStmCG();
+		letBeStStm.setHeader(letBeStExp.getHeader());
+		letBeStStm.setStatement(returnStm);
+		
+		return letBeStStm;
 	}
 	
 	public static void generateArguments(List<PExp> args, List<PExpCG> generatedArgs, OoAstInfo question) throws AnalysisException
