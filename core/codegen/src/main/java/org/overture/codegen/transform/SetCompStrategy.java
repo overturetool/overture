2727e770c98ff3acb0632a69539981efb7779b80
Peter Joergensen <pvj@iha.dk>
2014-03-02 18:18:50 +0100

    Extended set comprehension strategy to support transformation of set comprehensions with any number of multiple set binds.

--- core/codegen/src/main/java/org/overture/codegen/transform/SetCompStrategy.java
+++ core/codegen/src/main/java/org/overture/codegen/transform/SetCompStrategy.java
@@ -1,16 +1,37 @@
 package org.overture.codegen.transform;
 
+import java.util.List;
+
 import org.overture.codegen.cgast.analysis.AnalysisException;
+import org.overture.codegen.cgast.declarations.ALocalVarDeclCG;
 import org.overture.codegen.cgast.expressions.PExpCG;
+import org.overture.codegen.cgast.pattern.AIdentifierPatternCG;
+import org.overture.codegen.cgast.statements.PStmCG;
 import org.overture.codegen.cgast.types.PTypeCG;
 import org.overture.codegen.constants.IJavaCodeGenConstants;
 
 public class SetCompStrategy extends CompStrategy
 {
+	private boolean firstBind;
+	private boolean lastBind;
+	
 	public SetCompStrategy(TransformationAssistantCG transformationAssitant,
 			PExpCG first, PExpCG predicate, PExpCG set, String var)
 	{
 		super(transformationAssitant, first, predicate, set, var);
+		
+		this.firstBind = false;
+		this.lastBind = false;
+	}
+ 
+	
+	public SetCompStrategy(TransformationAssistantCG transformationAssitant,
+			PExpCG first, PExpCG predicate, PExpCG set, String var, boolean firstBind, boolean lastBind)
+	{
+		super(transformationAssitant, first, predicate, set, var);
+		
+		this.firstBind = firstBind;
+		this.lastBind = lastBind;
 	}
 
 	@Override
@@ -30,5 +51,28 @@ public class SetCompStrategy extends CompStrategy
 	{
 		
 		return transformationAssitant.getSetTypeCloned(set);
+	}
+	
+	@Override
+	public List<ALocalVarDeclCG> getOuterBlockDecls(
+			List<AIdentifierPatternCG> ids) throws AnalysisException
+	{
+		return firstBind ? super.getOuterBlockDecls(ids) : null;
+	}
+	
+	@Override
+	public List<PStmCG> getLastForLoopStms()
+	{
+		return lastBind ? super.getLastForLoopStms() : null;
+	}
+
+	public void setLastBind(boolean lastBind)
+	{
+		this.lastBind = lastBind;
+	}
+
+	public void setFirstBind(boolean firstBind)
+	{
+		this.firstBind = firstBind;
 	}
 }
