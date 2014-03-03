98d0403871218e030f12df51a124733629667820
Peter Joergensen <pvj@iha.dk>
2014-03-03 17:35:49 +0100

    Added strategy for transforming map comprehensions

---
+++ core/codegen/src/main/java/org/overture/codegen/transform/MapCompStrategy.java
@@ -0,0 +1,48 @@
+package org.overture.codegen.transform;
+
+import java.util.List;
+
+import org.overture.codegen.cgast.analysis.AnalysisException;
+import org.overture.codegen.cgast.expressions.AMapletExpCG;
+import org.overture.codegen.cgast.expressions.PExpCG;
+import org.overture.codegen.cgast.statements.PStmCG;
+import org.overture.codegen.cgast.types.PTypeCG;
+import org.overture.codegen.constants.IJavaCodeGenConstants;
+
+public class MapCompStrategy extends ComplexCompStrategy
+{
+	protected AMapletExpCG first;
+	
+	public MapCompStrategy(TransformationAssistantCG transformationAssitant,
+			AMapletExpCG first, PExpCG predicate, String var, PTypeCG compType)
+	{
+		super(transformationAssitant, predicate, var, compType);
+		
+		this.first = first;
+	}
+
+	@Override
+	public String getClassName()
+	{
+		return IJavaCodeGenConstants.MAP_UTIL_FILE;
+	}
+
+	@Override
+	public String getMemberName()
+	{
+		return IJavaCodeGenConstants.MAP_UTIL_EMPTY_MAP_CALL;
+	}
+
+	@Override
+	public PTypeCG getCollectionType() throws AnalysisException
+	{
+		return transformationAssitant.getMapTypeCloned(compType);
+	}
+
+	@Override
+	protected List<PStmCG> getConditionalAdd()
+	{
+		return packStm(transformationAssitant.consConditionalAdd(IJavaCodeGenConstants.ADD_ELEMENT_TO_MAP, var, predicate, first.getLeft(), first.getRight()));
+	}
+
+}
