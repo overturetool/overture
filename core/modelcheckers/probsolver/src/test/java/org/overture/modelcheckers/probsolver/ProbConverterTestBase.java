f4b0352d171c031cb40a5176e8be0210de32b853
Kenneth Lausdahl <lausdahl@eng.au.dk>
2014-03-05 13:26:16 +0100

    added proper setup to test case

--- core/modelcheckers/probsolver/src/test/java/org/overture/modelcheckers/probsolver/ProbConverterTestBase.java
+++ core/modelcheckers/probsolver/src/test/java/org/overture/modelcheckers/probsolver/ProbConverterTestBase.java
@@ -4,20 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.overture.ast.analysis.AnalysisException;
+import org.overture.ast.definitions.AImplicitFunctionDefinition;
 import org.overture.ast.definitions.AImplicitOperationDefinition;
 import org.overture.ast.definitions.PDefinition;
 import org.overture.ast.definitions.SClassDefinition;
 import org.overture.ast.lex.Dialect;
 import org.overture.ast.modules.AModuleModules;
+import org.overture.ast.node.INode;
+import org.overture.ast.patterns.APatternListTypePair;
+import org.overture.ast.patterns.PPattern;
 import org.overture.ast.statements.PStm;
+import org.overture.ast.types.PType;
 import org.overture.config.Release;
 import org.overture.config.Settings;
 import org.overture.modelcheckers.probsolver.ProbSolverUtil.SolverException;
+import org.overture.modelcheckers.probsolver.visitors.VdmToBConverter;
 import org.overture.test.framework.ConditionalIgnoreMethodRule;
 import org.overture.typechecker.util.TypeCheckerUtil;
 import org.overture.typechecker.util.TypeCheckerUtil.TypeCheckResult;
@@ -39,6 +46,7 @@ public class ProbConverterTestBase
 	{
 		Settings.dialect = Dialect.VDM_SL;
 		Settings.release = Release.VDM_10;
+		VdmToBConverter.USE_INITIAL_FIXED_STATE = true;
 	}
 
 	@Rule
@@ -49,13 +57,21 @@ public class ProbConverterTestBase
 	{
 		try
 		{
+			INode result = null;
+			PDefinition def = findFunctionOrOperation(name);
 
-			AImplicitOperationDefinition opDef = findOperation(name);
+			if (def instanceof AImplicitOperationDefinition)
+			{
+				HashMap<String, String> emptyMap = new HashMap<String, String>();
+				result = ProbSolverUtil.solve(def.getName().getName(), (AImplicitOperationDefinition) def, emptyMap, emptyMap, getArgTypes(def), new SolverConsole());
 
-			HashMap<String, String> emptyMap = new HashMap<String, String>();
-			PStm stm = ProbSolverUtil.solve(opDef.getName().getName(), opDef, emptyMap, emptyMap, new SolverConsole());
-
-			System.out.println("Result=" + stm);
+			} else
+			{
+				AImplicitFunctionDefinition funDef = (AImplicitFunctionDefinition) def;
+				HashMap<String, String> emptyMap = new HashMap<String, String>();
+				result = ProbSolverUtil.solve(def.getName().getName(), funDef.getPostcondition(), funDef.getResult(), emptyMap, emptyMap, getArgTypes(def), new SolverConsole());
+			}
+			System.out.println("Result=" + result);
 
 		} catch (SolverException e)
 		{
@@ -73,7 +89,45 @@ public class ProbConverterTestBase
 		}
 	}
 
-	protected AImplicitOperationDefinition findOperation(String name)
+	public static Map<String, PType> getArgTypes(PDefinition def)
+	{
+		Map<String, PType> argTypes = new HashMap<String, PType>();
+
+		if (def instanceof AImplicitOperationDefinition)
+		{
+			final AImplicitOperationDefinition op = (AImplicitOperationDefinition) def;
+			for (APatternListTypePair pl : op.getParameterPatterns())
+			{
+				for (PPattern p : pl.getPatterns())
+				{
+					argTypes.put(p.toString(), pl.getType());
+				}
+			}
+			if (op.getResult() != null)
+			{
+				argTypes.put(op.getResult().getPattern().toString(), op.getResult().getType());
+			}
+		} else if (def instanceof AImplicitFunctionDefinition)
+		{
+			AImplicitFunctionDefinition fun = (AImplicitFunctionDefinition) def;
+			for (APatternListTypePair pl : fun.getParamPatterns())
+			{
+				for (PPattern p : pl.getPatterns())
+				{
+					argTypes.put(p.toString(), pl.getType());
+				}
+			}
+
+			if (fun.getResult() != null)
+			{
+				argTypes.put(fun.getResult().getPattern().toString(), fun.getResult().getType());
+			}
+		}
+
+		return argTypes;
+	}
+
+	protected PDefinition findFunctionOrOperation(String name)
 			throws AnalysisException
 	{
 
@@ -88,14 +142,14 @@ public class ProbConverterTestBase
 			defs = parsePP(file).get(0).getDefinitions();
 		}
 
-		AImplicitOperationDefinition opDef = null;
+		PDefinition opDef = null;
 
 		for (PDefinition d : defs)
 		{
-			if (d instanceof AImplicitOperationDefinition
+			if ((d instanceof AImplicitOperationDefinition || d instanceof AImplicitFunctionDefinition)
 					&& d.getName().getName().equals(name))
 			{
-				opDef = (AImplicitOperationDefinition) d;
+				opDef = d;
 				break;
 			}
 		}
