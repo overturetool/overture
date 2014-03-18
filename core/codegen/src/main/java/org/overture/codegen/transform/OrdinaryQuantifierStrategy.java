c6676e72a1896d5bafb39077df61f7f7b614640d
Peter Joergensen <pvj@iha.dk>
2014-03-18 19:35:16 +0100

    Prepared strategies related tranformation of the forall and the exist expression for the introduction of the exist1 expression strategy

--- core/codegen/src/main/java/org/overture/codegen/transform/OrdinaryQuantifierStrategy.java
+++ core/codegen/src/main/java/org/overture/codegen/transform/OrdinaryQuantifierStrategy.java
@@ -3,26 +3,19 @@ package org.overture.codegen.transform;
 import java.util.List;
 
 import org.overture.codegen.cgast.analysis.AnalysisException;
-import org.overture.codegen.cgast.declarations.AVarLocalDeclCG;
 import org.overture.codegen.cgast.declarations.SLocalDeclCG;
 import org.overture.codegen.cgast.expressions.PExpCG;
 import org.overture.codegen.cgast.pattern.AIdentifierPatternCG;
-import org.overture.codegen.cgast.statements.ABlockStmCG;
 import org.overture.codegen.cgast.statements.PStmCG;
-import org.overture.codegen.cgast.types.PTypeCG;
 
-public class QuantifierStrategy extends AbstractIterationStrategy
+public class OrdinaryQuantifierStrategy extends QuantifierBaseStrategy
 {
-	protected PExpCG predicate;
-	protected String resultVarName;
 	protected OrdinaryQuantifier quantifier;
 	
-	public QuantifierStrategy(TransformationAssistantCG transformationAssistant,
+	public OrdinaryQuantifierStrategy(TransformationAssistantCG transformationAssistant,
 			PExpCG predicate, String resultVarName, OrdinaryQuantifier quantifier)
 	{
-		super(transformationAssistant);
-		this.predicate = predicate;
-		this.resultVarName = resultVarName;
+		super(transformationAssistant, predicate, resultVarName);
 		this.quantifier = quantifier;
 	}
 
@@ -40,22 +33,8 @@ public class QuantifierStrategy extends AbstractIterationStrategy
 	}
 
 	@Override
-	public ABlockStmCG getForLoopBody(PTypeCG setElementType,
-			AIdentifierPatternCG id, String iteratorName)
-			throws AnalysisException
-	{
-		return transformationAssistant.consForBodyNextElementDeclared(setElementType, id.getName(), iteratorName);
-	}
-
-	@Override
 	public List<PStmCG> getLastForLoopStms()
 	{
 		return lastBind ? packStm(transformationAssistant.consBoolVarAssignment(predicate, resultVarName)) : null;
-	}
-
-	@Override
-	public List<PStmCG> getOuterBlockStms()
-	{
-		return null;
 	}
 }
---
+++ core/codegen/src/main/java/org/overture/codegen/transform/QuantifierBaseStrategy.java
@@ -0,0 +1,39 @@
+package org.overture.codegen.transform;
+
+import java.util.List;
+
+import org.overture.codegen.cgast.analysis.AnalysisException;
+import org.overture.codegen.cgast.expressions.PExpCG;
+import org.overture.codegen.cgast.pattern.AIdentifierPatternCG;
+import org.overture.codegen.cgast.statements.ABlockStmCG;
+import org.overture.codegen.cgast.statements.PStmCG;
+import org.overture.codegen.cgast.types.PTypeCG;
+
+public abstract class QuantifierBaseStrategy extends
+		AbstractIterationStrategy
+{
+	protected PExpCG predicate;
+	protected String resultVarName;
+
+	public QuantifierBaseStrategy(
+			TransformationAssistantCG transformationAssistant, PExpCG predicate, String resultVarName)
+	{
+		super(transformationAssistant);
+		this.predicate = predicate;
+		this.resultVarName = resultVarName;
+	}
+	
+	@Override
+	public ABlockStmCG getForLoopBody(PTypeCG setElementType,
+			AIdentifierPatternCG id, String iteratorName)
+			throws AnalysisException
+	{
+		return transformationAssistant.consForBodyNextElementDeclared(setElementType, id.getName(), iteratorName);
+	}
+	
+	@Override
+	public List<PStmCG> getOuterBlockStms()
+	{
+		return null;
+	}
+}
