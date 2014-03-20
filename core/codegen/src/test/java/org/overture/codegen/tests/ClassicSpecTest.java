4eece741524f6dcb36ec56b8202a26b46e2296b0
Peter Joergensen <pvj@iha.dk>
2014-03-20 10:37:54 +0100

    Added test case for specifications written in VDM classic

---
+++ core/codegen/src/test/java/org/overture/codegen/tests/ClassicSpecTest.java
@@ -0,0 +1,26 @@
+package org.overture.codegen.tests;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.overture.ast.lex.LexLocation;
+import org.overture.test.framework.BaseTestSuite;
+import org.overture.test.framework.Properties;
+
+public class ClassicSpecTest extends BaseTestSuite
+{
+	public static final String ROOT = "src\\test\\resources\\classic_specs";
+	
+	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
+	{
+		LexLocation.absoluteToStringLocation = false;
+		Properties.recordTestResults = false;
+		
+		String name = "Classic specifications test case";
+		TestSuite test =  createTestCompleteFile(name, ROOT, ClassicSpecTestCase.class,"");
+		return test;
+	}
+}
