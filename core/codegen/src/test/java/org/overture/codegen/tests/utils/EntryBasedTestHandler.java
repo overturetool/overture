ef505508f9ea6661b5b5159e8f06c7ef304d4265
Peter Joergensen <pvj@iha.dk>
2014-03-28 22:40:13 +0100

    Changed entry based test handler to use the VDMPP dialect by default

--- core/codegen/src/test/java/org/overture/codegen/tests/utils/EntryBasedTestHandler.java
+++ core/codegen/src/test/java/org/overture/codegen/tests/utils/EntryBasedTestHandler.java
@@ -22,6 +22,6 @@ public abstract class EntryBasedTestHandler extends ExecutableTestHandler
 	@Override
 	public Value interpretVdm(File intputFile) throws Exception
 	{
-		return InterpreterUtil.interpret(Dialect.VDM_RT, VDM_ENTRY_CALL, intputFile);
+		return InterpreterUtil.interpret(Dialect.VDM_PP, VDM_ENTRY_CALL, intputFile);
 	}
 }
