0438aafe7f6ca241c882db20c4fb9ed515479a02
George Kanakis <gkanos@gmail.com>
2014-04-04 13:15:13 +0200

    Creation of ValuesDefinitionLocator.

--- core/interpreter/src/main/java/org/overture/interpreter/utilities/definition/ValuesDefinitionLocator.java
+++ core/interpreter/src/main/java/org/overture/interpreter/utilities/definition/ValuesDefinitionLocator.java
@@ -1,8 +1,19 @@
 package org.overture.interpreter.utilities.definition;
 
+import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.analysis.QuestionAnswerAdaptor;
+import org.overture.ast.definitions.AAssignmentDefinition;
+import org.overture.ast.definitions.AEqualsDefinition;
+import org.overture.ast.definitions.AInstanceVariableDefinition;
+import org.overture.ast.definitions.AValueDefinition;
+import org.overture.ast.node.INode;
 import org.overture.interpreter.assistant.IInterpreterAssistantFactory;
+import org.overture.interpreter.assistant.definition.AAssignmentDefinitionAssistantInterpreter;
+import org.overture.interpreter.assistant.definition.AEqualsDefinitionAssistantInterpreter;
+import org.overture.interpreter.assistant.definition.AInstanceVariableDefinitionAssistantInterpreter;
+import org.overture.interpreter.assistant.definition.AValueDefinitionAssistantInterpreter;
 import org.overture.interpreter.runtime.ObjectContext;
+import org.overture.interpreter.values.ValueList;
 
 /***************************************
  * 
@@ -18,6 +29,53 @@ public class ValuesDefinitionLocator extends QuestionAnswerAdaptor<ObjectContext
 	public ValuesDefinitionLocator(IInterpreterAssistantFactory af)
 	{
 		this.af = af;
+	}
+	
+	@Override
+	public ValueList caseAAssignmentDefinition(AAssignmentDefinition def,
+			ObjectContext ctxt) throws AnalysisException
+	{
+		return AAssignmentDefinitionAssistantInterpreter.getValues((AAssignmentDefinition) def, ctxt);
+	}
+	
+	@Override
+	public ValueList caseAEqualsDefinition(AEqualsDefinition def,
+			ObjectContext ctxt) throws AnalysisException
+	{
+		return AEqualsDefinitionAssistantInterpreter.getValues((AEqualsDefinition) def, ctxt);
+	}
+	
+//	if (def instanceof AAssignmentDefinition)
+//	{
+//		
+//	} else if (def instanceof AEqualsDefinition)
+//	{
+//		
+//	} else if (def instanceof AInstanceVariableDefinition)
+//	{
+//		return AInstanceVariableDefinitionAssistantInterpreter.getValues((AInstanceVariableDefinition) def, ctxt);
+//	} else if (def instanceof AValueDefinition)
+//	{
+//		return AValueDefinitionAssistantInterpreter.getValues((AValueDefinition) def, ctxt);
+//	} else
+//	{
+//		return new ValueList();
+//	}
+
+	@Override
+	public ValueList createNewReturnValue(INode node, ObjectContext question)
+			throws AnalysisException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValueList createNewReturnValue(Object node, ObjectContext question)
+			throws AnalysisException
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
