5ddf5f4201206c3a805e0ee99a3535b69f148cda
George Kanakis <gkanos@gmail.com>
2014-04-10 11:42:29 +0200

    Finish visitor CoreTraceExpander. 2nd level unfold.

--- core/interpreter/src/main/java/org/overture/interpreter/assistant/definition/ABracketedExpressionTraceCoreDefinitionAssitantInterpreter.java
+++ core/interpreter/src/main/java/org/overture/interpreter/assistant/definition/ABracketedExpressionTraceCoreDefinitionAssitantInterpreter.java
@@ -1,11 +1,6 @@
 package org.overture.interpreter.assistant.definition;
 
-import org.overture.ast.definitions.traces.ABracketedExpressionTraceCoreDefinition;
-import org.overture.ast.definitions.traces.ATraceDefinitionTerm;
 import org.overture.interpreter.assistant.IInterpreterAssistantFactory;
-import org.overture.interpreter.runtime.Context;
-import org.overture.interpreter.traces.SequenceTraceNode;
-import org.overture.interpreter.traces.TraceNode;
 
 public class ABracketedExpressionTraceCoreDefinitionAssitantInterpreter
 {
@@ -18,17 +13,17 @@ public class ABracketedExpressionTraceCoreDefinitionAssitantInterpreter
 		this.af = af;
 	}
 
-	public static TraceNode expand(
-			ABracketedExpressionTraceCoreDefinition core, Context ctxt)
-	{
-		SequenceTraceNode node = new SequenceTraceNode();
-
-		for (ATraceDefinitionTerm term : core.getTerms())
-		{
-			node.nodes.add(ATraceDefinitionTermAssistantInterpreter.expand(term, ctxt));
-		}
-
-		return node;
-	}
+//	public static TraceNode expand(
+//			ABracketedExpressionTraceCoreDefinition core, Context ctxt)
+//	{
+//		SequenceTraceNode node = new SequenceTraceNode();
+//
+//		for (ATraceDefinitionTerm term : core.getTerms())
+//		{
+//			node.nodes.add(ATraceDefinitionTermAssistantInterpreter.expand(term, ctxt));
+//		}
+//
+//		return node;
+//	}
 
 }
--- core/interpreter/src/main/java/org/overture/interpreter/utilities/definition/CoreTraceExpander.java
+++ core/interpreter/src/main/java/org/overture/interpreter/utilities/definition/CoreTraceExpander.java
@@ -1,18 +1,42 @@
 package org.overture.interpreter.utilities.definition;
 
+import java.util.List;
+import java.util.Vector;
+
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.analysis.QuestionAnswerAdaptor;
 import org.overture.ast.definitions.traces.AApplyExpressionTraceCoreDefinition;
 import org.overture.ast.definitions.traces.ABracketedExpressionTraceCoreDefinition;
 import org.overture.ast.definitions.traces.AConcurrentExpressionTraceCoreDefinition;
+import org.overture.ast.definitions.traces.ATraceDefinitionTerm;
 import org.overture.ast.definitions.traces.PTraceCoreDefinition;
+import org.overture.ast.definitions.traces.PTraceDefinition;
+import org.overture.ast.expressions.PExp;
+import org.overture.ast.factory.AstFactory;
+import org.overture.ast.lex.LexIdentifierToken;
 import org.overture.ast.node.INode;
+import org.overture.ast.statements.ACallObjectStm;
+import org.overture.ast.statements.ACallStm;
+import org.overture.ast.statements.PStm;
+import org.overture.config.Settings;
 import org.overture.interpreter.assistant.IInterpreterAssistantFactory;
 import org.overture.interpreter.assistant.definition.AApplyExpressionTraceCoreDefinitionAssistantInterpreter;
 import org.overture.interpreter.assistant.definition.ABracketedExpressionTraceCoreDefinitionAssitantInterpreter;
 import org.overture.interpreter.assistant.definition.AConcurrentExpressionTraceCoreDefinitionAssistantInterpreter;
+import org.overture.interpreter.assistant.definition.ATraceDefinitionTermAssistantInterpreter;
+import org.overture.interpreter.assistant.definition.PTraceDefinitionAssistantInterpreter;
 import org.overture.interpreter.runtime.Context;
+import org.overture.interpreter.runtime.VdmRuntime;
+import org.overture.interpreter.traces.ConcurrentTraceNode;
+import org.overture.interpreter.traces.SequenceTraceNode;
+import org.overture.interpreter.traces.StatementTraceNode;
 import org.overture.interpreter.traces.TraceNode;
+import org.overture.interpreter.values.ObjectValue;
+import org.overture.interpreter.values.Value;
+import org.overture.parser.lex.LexException;
+import org.overture.parser.lex.LexTokenReader;
+import org.overture.parser.syntax.ExpressionReader;
+import org.overture.parser.syntax.ParserException;
 
 /***************************************
  * 
@@ -36,7 +60,77 @@ public class CoreTraceExpander extends QuestionAnswerAdaptor<Context, TraceNode>
 			AApplyExpressionTraceCoreDefinition core, Context ctxt)
 			throws AnalysisException
 	{
-		return AApplyExpressionTraceCoreDefinitionAssistantInterpreter.expand(core, ctxt);
+		//return AApplyExpressionTraceCoreDefinitionAssistantInterpreter.expand(core, ctxt);
+		List<PExp> newargs = new Vector<PExp>();
+		List<PExp> args = null;
+
+		if (core.getCallStatement() instanceof ACallStm)
+		{
+			ACallStm stmt = (ACallStm) core.getCallStatement();
+			args = stmt.getArgs();
+		} else
+		{
+			ACallObjectStm stmt = (ACallObjectStm) core.getCallStatement();
+			args = stmt.getArgs();
+		}
+
+		for (PExp arg : args)
+		{
+			Value v = null;
+			try
+			{
+				v = arg.apply(VdmRuntime.getExpressionEvaluator(), ctxt).deref();
+			} catch (AnalysisException e1)
+			{
+				e1.printStackTrace();
+			}
+
+			if (v instanceof ObjectValue)
+			{
+				newargs.add(arg.clone());
+			} else
+			{
+				// TODO This rewrites the source code and enables stepping when evaluating the
+				// arguments where the location is off since the new arguments do not exist in the source
+				// file. What to do? Use the same location as the call statement? or..
+				String value = v.toString();
+				LexTokenReader ltr = new LexTokenReader(value, Settings.dialect, arg.getLocation());
+				ExpressionReader er = new ExpressionReader(ltr);
+				er.setCurrentModule(core.getCurrentModule());
+
+				try
+				{
+					newargs.add(er.readExpression());
+				} catch (ParserException e)
+				{
+					newargs.add(arg.clone()); // Give up!
+				} catch (LexException e)
+				{
+					newargs.add(arg.clone()); // Give up!
+				}
+			}
+		}
+
+		PStm newStatement = null;
+
+		if (core.getCallStatement() instanceof ACallStm)
+		{
+			ACallStm stmt = (ACallStm) core.getCallStatement();
+			newStatement = AstFactory.newACallStm(stmt.getName().clone(), newargs);
+		} else
+		{
+			ACallObjectStm stmt = (ACallObjectStm) core.getCallStatement();
+
+			if (stmt.getClassname() != null)
+			{
+				newStatement = AstFactory.newACallObjectStm(stmt.getDesignator().clone(), stmt.getClassname().clone(), newargs);
+			} else
+			{
+				newStatement = AstFactory.newACallObjectStm(stmt.getDesignator().clone(), (LexIdentifierToken) stmt.getFieldname().clone(), newargs);
+			}
+		}
+
+		return new StatementTraceNode(newStatement);
 	}
 	
 	@Override
@@ -44,7 +138,15 @@ public class CoreTraceExpander extends QuestionAnswerAdaptor<Context, TraceNode>
 			ABracketedExpressionTraceCoreDefinition core, Context ctxt)
 			throws AnalysisException
 	{
-		return ABracketedExpressionTraceCoreDefinitionAssitantInterpreter.expand(core, ctxt);
+		//return ABracketedExpressionTraceCoreDefinitionAssitantInterpreter.expand(core, ctxt);
+		SequenceTraceNode node = new SequenceTraceNode();
+
+		for (ATraceDefinitionTerm term : core.getTerms())
+		{
+			node.nodes.add(ATraceDefinitionTermAssistantInterpreter.expand(term, ctxt));
+		}
+
+		return node;
 	}
 	
 	@Override
@@ -52,7 +154,15 @@ public class CoreTraceExpander extends QuestionAnswerAdaptor<Context, TraceNode>
 			AConcurrentExpressionTraceCoreDefinition core, Context ctxt)
 			throws AnalysisException
 	{
-		return AConcurrentExpressionTraceCoreDefinitionAssistantInterpreter.expand(core, ctxt);
+		//return AConcurrentExpressionTraceCoreDefinitionAssistantInterpreter.expand(core, ctxt);
+		ConcurrentTraceNode node = new ConcurrentTraceNode();
+
+		for (PTraceDefinition term : core.getDefs())
+		{
+			node.nodes.add(PTraceDefinitionAssistantInterpreter.expand(term, ctxt));
+		}
+
+		return node;
 	}
 	
 	@Override
