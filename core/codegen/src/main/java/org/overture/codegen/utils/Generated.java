d26c5bf7f9a4312300057ad214691cf03393c22a
Peter Joergensen <pvj@iha.dk>
2014-04-13 11:30:04 +0200

    Extended the classes used for storing the result from the code generation of a module (e.g. class) to also store potential errors experience while merging that module

--- core/codegen/src/main/java/org/overture/codegen/utils/Generated.java
+++ core/codegen/src/main/java/org/overture/codegen/utils/Generated.java
@@ -1,5 +1,8 @@
 package org.overture.codegen.utils;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import org.overture.codegen.ooast.NodeInfo;
@@ -8,11 +11,28 @@ public class Generated
 {
 	protected String content;
 	protected Set<NodeInfo> unsupportedNodes;
-	
-	public Generated(String content, Set<NodeInfo> unsupportedNodes)
+	protected List<Exception> mergeErrors;
+		
+	public Generated(String content, Set<NodeInfo> unsupportedNodes, List<Exception> mergeErrors)
 	{
 		this.content = content;
 		this.unsupportedNodes = unsupportedNodes;
+		this.mergeErrors = mergeErrors;
+	}
+	
+	public Generated(String content)
+	{
+		this(content, new HashSet<NodeInfo>(), new LinkedList<Exception>());
+	}
+	
+	public Generated(Set<NodeInfo> unsupportedNodes)
+	{
+		this(null, unsupportedNodes, new LinkedList<Exception>());
+	}
+	
+	public Generated(List<Exception> mergeErrrors)
+	{
+		this(null, new HashSet<NodeInfo>(), mergeErrrors);
 	}
 
 	public String getContent()
@@ -25,8 +45,18 @@ public class Generated
 		return unsupportedNodes;
 	}
 	
+	public List<Exception> getMergeErrors()
+	{
+		return mergeErrors;
+	}
+	
 	public boolean canBeGenerated()
 	{
-		return unsupportedNodes.size() == 0;
+		return unsupportedNodes.isEmpty();
+	}
+	
+	public boolean hasMergeErrors()
+	{
+		return !mergeErrors.isEmpty();
 	}
 }
