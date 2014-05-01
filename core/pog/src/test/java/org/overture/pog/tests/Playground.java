c90d48a298be4bb76160034b84027448f7ee6936
Luis Diogo Couto <ldc@eng.au.dk>
2014-05-01 15:22:40 +0200

    Fix and test for #292

--- core/pog/src/test/java/org/overture/pog/tests/Playground.java
+++ core/pog/src/test/java/org/overture/pog/tests/Playground.java
@@ -15,34 +15,40 @@ import org.overture.pog.pub.IProofObligationList;
 import org.overture.pog.pub.ProofObligationGenerator;
 
 import com.google.gson.Gson;
+
 /**
  * Class for quick testing and work on the pog
+ * 
  * @author ldc
- *
+ * 
  */
 public class Playground {
 
-	
 	// switch this flag to update a test result file
-	static boolean WRITE_RESULT = true;
-	
+	static boolean WRITE_RESULT = false;
+
 	// comment this annotation out when done!
-	@Test
+	//@Test
 	public void quickTest() throws AnalysisException, IOException, URISyntaxException{
+		String model = "src/test/resources/adhoc/sandbox.vdmsl";
+		//String model = "src/test/resources/adhoc/sandbox.vdmpp";
+		
+		
 		List<INode> ast = TestHelper
-				.getAstFromName("src/test/resources/adhoc/sandbox.vdmpp");
+				.getAstFromName(model);
+		
 		IProofObligationList ipol = ProofObligationGenerator
 				.generateProofObligations(ast);
 		
 		for (IProofObligation po : ipol) {
-			System.out.println(po.getValue());
+			System.out.println(po.getKindString() + " / " +po.getValue());
 		}
 		
 		if (WRITE_RESULT){
 			this.update(ipol);
 		}
 	}
-	
+
 	private void update(IProofObligationList ipol) throws AnalysisException,
 			IOException, URISyntaxException {
 
