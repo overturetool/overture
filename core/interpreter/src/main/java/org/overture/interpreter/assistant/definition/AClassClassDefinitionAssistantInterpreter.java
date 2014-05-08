26f4624b4a9762a6be10e7d5b0b2e09d29ab5e0b
Nick Battle <nick.battle@gmail.com>
2014-05-08 16:26:38 +0100

    Prevent inheritance of private constructors, fixes #7

--- core/interpreter/src/main/java/org/overture/interpreter/assistant/definition/AClassClassDefinitionAssistantInterpreter.java
+++ core/interpreter/src/main/java/org/overture/interpreter/assistant/definition/AClassClassDefinitionAssistantInterpreter.java
@@ -33,7 +33,7 @@ public class AClassClassDefinitionAssistantInterpreter
 					+ node.getName(), ctxt);
 		}
 
-		return SClassDefinitionAssistantInterpreter.makeNewInstance(node, ctorDefinition, argvals, ctxt, new HashMap<ILexNameToken, ObjectValue>());
+		return SClassDefinitionAssistantInterpreter.makeNewInstance(node, ctorDefinition, argvals, ctxt, new HashMap<ILexNameToken, ObjectValue>(), false);
 	}
 
 }
