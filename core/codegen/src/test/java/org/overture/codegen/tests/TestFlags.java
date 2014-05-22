6ce35e7c3329d09ca74b2176e8fb963a9b9cd5dd
Peter Joergensen <pvj@iha.dk>
2014-05-22 15:19:52 +0200

    Missing 'final' on a test flag

--- core/codegen/src/test/java/org/overture/codegen/tests/TestFlags.java
+++ core/codegen/src/test/java/org/overture/codegen/tests/TestFlags.java
@@ -2,7 +2,7 @@ package org.overture.codegen.tests;
 
 public class TestFlags
 {
-	public static  boolean SPEC_TESTS_ON = false;
+	public static final boolean SPEC_TESTS_ON = false;
 	public static final boolean CLASSIC_SPEC_TESTS_ON = false;
 	public static final boolean COMPLEX_EXP_TESTS_ON = false;
 	public static final boolean EXP_TESTS_ON = false;
