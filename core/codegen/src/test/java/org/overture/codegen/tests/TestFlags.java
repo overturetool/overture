1c1a9bcafecf60efe47679270864e9b3d18fe14b
Peter Joergensen <pvj@iha.dk>
2014-05-25 12:18:17 +0200

    Added unit test to exercise configuration of cloning as used by the Java code generator

--- core/codegen/src/test/java/org/overture/codegen/tests/TestFlags.java
+++ core/codegen/src/test/java/org/overture/codegen/tests/TestFlags.java
@@ -8,4 +8,5 @@ public class TestFlags
 	public static final boolean EXP_TESTS_ON = false;
 	public static final boolean FUNC_VALUE_TESTS_ON = false;
 	public static final boolean STRING_CONFIG_TESTS_ON = false;
+	public static final boolean CLONING_CONFIG_TESTS_ON = false;
 }
