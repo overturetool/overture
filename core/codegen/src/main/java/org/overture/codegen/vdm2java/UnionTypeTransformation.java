b94ac43b374c95c41e98fcb1a6c813274c2b2f16
Peter Joergensen <pvj@iha.dk>
2014-06-08 18:48:52 +0200

    Updated the union type transformation to use the new functionality in the base transformaton assistant

--- core/codegen/src/main/java/org/overture/codegen/vdm2java/UnionTypeTransformation.java
+++ core/codegen/src/main/java/org/overture/codegen/vdm2java/UnionTypeTransformation.java
@@ -1,12 +1,10 @@
 package org.overture.codegen.vdm2java;
 
-import org.overture.codegen.cgast.INode;
 import org.overture.codegen.cgast.SExpCG;
 import org.overture.codegen.cgast.STypeCG;
 import org.overture.codegen.cgast.analysis.AnalysisException;
 import org.overture.codegen.cgast.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.cgast.expressions.ACastUnaryExpCG;
-import org.overture.codegen.cgast.expressions.SBinaryExpCG;
 import org.overture.codegen.cgast.expressions.SNumericBinaryExpCG;
 import org.overture.codegen.cgast.types.AUnionTypeCG;
 import org.overture.codegen.transform.BaseTransformationAssistant;
@@ -43,17 +41,7 @@ public class UnionTypeTransformation extends DepthFirstAnalysisAdaptor
 		SExpCG newLeft = wrap(node.getLeft(), expectedType);
 		SExpCG newRight = wrap(node.getRight(), expectedType);
 
-		if(newLeft != node.getLeft())
-		{
-			baseAssistant.replaceNodeWith(node.getLeft(), newLeft);
-			newLeft.apply(this);
-		}
-		
-		if(newRight != node.getRight())
-		{
-			baseAssistant.replaceNodeWith(node.getRight(), newRight);
-			newRight.apply(this);
-		}
-			
+		baseAssistant.replaceNodeWithRecursively(node.getLeft(), newLeft, this);
+		baseAssistant.replaceNodeWithRecursively(node.getRight(), newRight, this);
 	}
 }
