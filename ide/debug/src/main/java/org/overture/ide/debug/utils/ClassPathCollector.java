89bc852f022b7da3f5231053a5d1ad34c0ee315a
Kenneth Lausdahl <lausdahl@eng.au.dk>
2014-06-12 22:29:42 +0200

    fixed #255 changed the way the debugger is launched. Now the classpath is set as en env var instead of the -cp arg.
    A similar issue was discovered for system properties -Dprob.home= with path with space. This is solved through a new overture.properties file which respects special entries system.prob.home (the system. prefix) these are copied in Properties.init to System.setProperty (while stripped for system.)
    
    The probsolverintegration descriptor was updated to match the maven groupe update done from overture to overturetool.
    
    A new develop launch tab check box is added to enable the VM to print its internal settings easing debug.
    
    Trace execution has also been updated to include prob and the new env cp way of launching.

--- ide/debug/src/main/java/org/overture/ide/debug/utils/ClassPathCollector.java
+++ ide/debug/src/main/java/org/overture/ide/debug/utils/ClassPathCollector.java
@@ -29,20 +29,25 @@ public class ClassPathCollector
 
 		return entries;
 	}
-
-	public static String toCpCliArgument(Collection<? extends String> entries)
+	
+	/**
+	 * Creates a class path string from the entries using the path.seperator
+	 * @param entries
+	 * @return the class path string
+	 */
+	public static String toCpEnvString(Collection<? extends String> entries)
 	{
 		if (entries.size() > 0)
 		{
-			StringBuffer classPath = new StringBuffer(" ");
+			StringBuffer classPath = new StringBuffer("");
 			for (String cp : new HashSet<String>(entries))// remove dublicates
 			{
 				if (cp == null)
 				{
 					continue;
 				}
-				classPath.append(toPlatformPath(cp));
-				classPath.append(getCpSeperator());
+				classPath.append(cp);
+				classPath.append(System.getProperty("path.separator"));
 			}
 			classPath.deleteCharAt(classPath.length() - 1);
 			return classPath.toString().trim();
@@ -51,39 +56,60 @@ public class ClassPathCollector
 		return "";
 	}
 
-	private static String getCpSeperator()
-	{
-		if (isWindowsPlatform())
-		{
-			return ";";
-		} else
-		{
-			return ":";
-		}
-	}
-
-	public static boolean isWindowsPlatform()
-	{
-		return System.getProperty("os.name").toLowerCase().contains("win");
-	}
-
-	public static boolean isMac()
-	{
-		return (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0);
-	}
-
-	protected static String toPlatformPath(String path)
-	{
-		if (isWindowsPlatform())
-		{
-			return "\"" + path + "\"";
-		} else if(isMac())
-		{
-			return path;// Bug: 255; Since this is used in a process builder Linux/Mac must not have this escape
-						// enabled:.replace(" ", "\\ ");
-		}else
-		{
-			return path.replace(" ", "\\ ");// Bug: 255; Not sure how to fix it for Linux!
-		}
-	}
+//	public static String toCpCliArgument(Collection<? extends String> entries)
+//	{
+//		if (entries.size() > 0)
+//		{
+//			StringBuffer classPath = new StringBuffer(" ");
+//			for (String cp : new HashSet<String>(entries))// remove dublicates
+//			{
+//				if (cp == null)
+//				{
+//					continue;
+//				}
+//				classPath.append(toPlatformPath(cp));
+//				classPath.append(getCpSeperator());
+//			}
+//			classPath.deleteCharAt(classPath.length() - 1);
+//			return classPath.toString().trim();
+//
+//		}
+//		return "";
+//	}
+//
+//	private static String getCpSeperator()
+//	{
+//		if (isWindowsPlatform())
+//		{
+//			return ";";
+//		} else
+//		{
+//			return ":";
+//		}
+//	}
+//
+//	public static boolean isWindowsPlatform()
+//	{
+//		return System.getProperty("os.name").toLowerCase().contains("win");
+//	}
+//
+//	public static boolean isMac()
+//	{
+//		return (System.getProperty("os.name").toLowerCase().indexOf("mac") >= 0);
+//	}
+//
+//	protected static String toPlatformPath(String path)
+//	{
+//		if (isWindowsPlatform())
+//		{
+//			return "\"" + path + "\"";
+//		} else if(isMac())
+//		{
+//			return path;// Bug: 255; Since this is used in a process builder Linux/Mac must not have this escape
+//						// enabled:.replace(" ", "\\ ");
+//		}else
+//		{
+//			return path.replace(" ", "\\ ");// Bug: 255; Not sure how to fix it for Linux!
+//		}
+//	}
 }
