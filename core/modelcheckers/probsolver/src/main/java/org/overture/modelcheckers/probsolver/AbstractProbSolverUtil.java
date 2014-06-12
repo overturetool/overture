ba79b514800b880030f37cc06b5bd1c21b9e5fcb
George Kanakis <gkanos@gmail.com>
2014-06-12 15:33:02 +0200

    correction of the LexToken qualifier issue and the correction of pom at the interpreter

--- core/modelcheckers/probsolver/src/main/java/org/overture/modelcheckers/probsolver/AbstractProbSolverUtil.java
+++ core/modelcheckers/probsolver/src/main/java/org/overture/modelcheckers/probsolver/AbstractProbSolverUtil.java
@@ -156,7 +156,7 @@ public class AbstractProbSolverUtil
 	/**
 	 * Create the assistant factory for use in the subclasses.
 	 */
-	public AstAssistantFactory assistantFactory;// = new AstAssistantFactory();
+	public AstAssistantFactory assistantFactory;
 	
 
 	/**
