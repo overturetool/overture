eab3e208f477645386bcdbadd5137e4c6903d365
Luis Diogo Couto <ldc@eng.au.dk>
2014-06-25 15:45:36 +0100

    Add explicit Result Type getter.
    This will allow the tests to cope better with deserializing the results.

--- core/tests/src/main/java/org/overture/core/tests/ParamTestAbstract.java
+++ core/tests/src/main/java/org/overture/core/tests/ParamTestAbstract.java
@@ -3,6 +3,7 @@ package org.overture.core.tests;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Serializable;
 import java.lang.reflect.Type;
 import java.util.List;
 
@@ -30,18 +31,19 @@ import com.google.gson.reflect.TypeToken;
  * subclass of it must be annotated with
  * <code>@RunWith(Parameterized.class)</code>. <br>
  * <br>
- * Many methods in this class are parameterized on a type <code>R</code> that
- * represents the output of of the analysis of the functionality under test.
- * These types must implement {@link IResult} and you will most likely need to
- * write some kind of translation between your outputs and <code>R</code>.
+ * This class is parameterized on a type <code>R</code> that represents the
+ * output of of the analysis of the functionality under test. These types must
+ * implement {@link Serializable} and you will most likely need to write some
+ * kind of transformation between your native output type and <code>R</code>.
  * 
  * @author ldc
  */
 @RunWith(Parameterized.class)
-public abstract class ParamTestAbstract {
+public abstract class ParamTestAbstract<R extends Serializable> {
 
-	private String modelPath;
-	private String resultPath;
+	protected String modelPath;
+	protected String resultPath;
+	protected String testName;
 
 	/**
 	 * Constructor for the test. Works with outputs gotten from
@@ -58,8 +60,9 @@ public abstract class ParamTestAbstract {
 	 * @param resultParameter
 	 *            test result file
 	 */
-	public ParamTestAbstract(String _, String testParameter,
+	public ParamTestAbstract(String nameParameter, String testParameter,
 			String resultParameter) {
+		this.testName = testParameter;
 		this.modelPath = testParameter;
 		this.resultPath = resultParameter;
 	}
@@ -71,7 +74,7 @@ public abstract class ParamTestAbstract {
 	 * {@link #testCompare(Object, IResult)}.
 	 * 
 	 * @param <R>
-	 *            a result type produced by the analysed plug-in. You may need
+	 *            a result type produced by the analyzed plug-in. You may need
 	 *            to create one.
 	 * 
 	 * @throws IOException
@@ -79,8 +82,7 @@ public abstract class ParamTestAbstract {
 	 * @throws ParserException
 	 */
 	@Test
-	public <R extends IResult> void testCase() throws ParserException,
-			LexException, IOException {
+	public void testCase() throws ParserException, LexException, IOException {
 		List<INode> ast = InputProcessor.typedAst(modelPath);
 		R actual = processModel(ast);
 		R expected = deSerializeResult(resultPath);
@@ -97,7 +99,7 @@ public abstract class ParamTestAbstract {
 	 * @param expected
 	 *            the stored result
 	 */
-	public abstract <R extends IResult> void testCompare(R actual, R expected);
+	public abstract void testCompare(R actual, R expected);
 
 	/**
 	 * Analyses a model (represented by its AST). This method must be overridden
@@ -110,26 +112,38 @@ public abstract class ParamTestAbstract {
 	 *            representing the model to process
 	 * @return the output of the analysis
 	 */
-	public abstract <R extends IResult> R processModel(List<INode> ast);
+	public abstract R processModel(List<INode> ast);
 
 	/**
 	 * This method tries its best to deserialize any results file. If your
-	 * results are too complex for it to handle, you should override this method
-	 * to deal with them.
+	 * results are too complex for it to handle, you should override
+	 * {@link #getResultType()} to deal with it. If that fails, override this
+	 * entire.
 	 * 
 	 * @param resultPath
-	 * @return
+	 * @return the stored result
 	 * @throws IOException
 	 * @throws FileNotFoundException
 	 */
-	public <R extends IResult> R deSerializeResult(String resultPath)
-			throws FileNotFoundException, IOException {
+	public R deSerializeResult(String resultPath) throws FileNotFoundException,
+			IOException {
 		Gson gson = new Gson();
+		Type resultType = getResultType();
 		String json = IOUtils.toString(new FileReader(resultPath));
-		Type resultType = new TypeToken<R>() {
-		}.getType();
 		R results = gson.fromJson(json, resultType);
 		return results;
+	}
+
+	/**
+	 * Calculates the type of the result. This method does its best but doesn't
+	 * always succeed. Override it if necessary.
+	 * 
+	 * @return the {@link Type} of the result file
+	 */
+	public Type getResultType() {
+		Type resultType = new TypeToken<R>() {
+		}.getType();
+		return resultType;
 	}
 
 }
