8b55fb2f7534a6e2ab5c320326f35c575f1b09c0
George Kanakis <gkanos@gmail.com>
2014-06-25 19:19:25 +0200

    Added Java code generation for the thread id expression (initial) and added test example

---
+++ core/codegen/src/test/java/org/overture/codegen/tests/ConcurrencyTests.java
@@ -0,0 +1,26 @@
+package org.overture.codegen.tests;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.overture.ast.lex.LexLocation;
+import org.overture.test.framework.BaseTestSuite;
+import org.overture.test.framework.Properties;
+
+public class ConcurrencyTests extends BaseTestSuite
+{
+	public static final String ROOT = "src\\test\\resources\\concurrency";
+	
+	public static Test suite() throws IllegalArgumentException, SecurityException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException, IOException
+	{
+		LexLocation.absoluteToStringLocation = false;
+		Properties.recordTestResults = TestFlags.CONCURRENCY_TESTS_ON;
+		
+		String name = "Concurrency test case";
+		TestSuite test =  createTestCompleteFile(name, ROOT, SpecificationTestCase.class,"");
+		return test;
+	}
+}
