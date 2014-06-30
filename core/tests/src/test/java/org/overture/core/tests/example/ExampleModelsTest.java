3a63723e7001ff2bff74f3c6b26ffb7dfd047d60
Luis Diogo Couto <ldc@eng.au.dk>
2014-06-30 19:23:49 +0200

    Clean up documentation and names.

--- core/tests/src/test/java/org/overture/core/tests/example/ExampleModelsTest.java
+++ core/tests/src/test/java/org/overture/core/tests/example/ExampleModelsTest.java
@@ -6,31 +6,30 @@ import java.io.IOException;
 import java.util.Collection;
 
 import org.junit.Test;
-import org.overture.core.tests.AllExamplesHelper;
-import org.overture.core.tests.AllExamplesHelper.ExampleAstData;
 import org.overture.tools.examplepackager.util.ExampleTestData;
 import org.overture.tools.examplepackager.util.ExampleTestUtils;
 
 /**
- * Basic test for all the examples. Simply ensures that
- * they all parse and type-check correctly.
+ * Basic test for all the examples. Simply ensures that they all parse and type-check correctly. Examples with
+ * purposeful errors are not tested at all. This test needs to be improved to also check examples with specific errors.
  * 
- * Examples with purposeful errors (for learning) are not
- * tested at all. This test needs to be improved to
- * check for the specific errors only.
+ * 
  * @author ldc
- *
  */
-public class ExampleModelsTest {
-//FIXME make Example Models Tests test for models with errors as well.
-	
+public class ExampleModelsTest
+{
+
+	// FIXME make Example Models Tests test for models with errors as well.
+
 	@Test
-	public void testAllExamples() throws IOException{
+	public void testParseTcAllExamples() throws IOException
+	{
 		Collection<ExampleTestData> examples = ExampleTestUtils.getCorrectExamplesSources();
-		
-		for (ExampleTestData ex : examples){
+
+		for (ExampleTestData ex : examples)
+		{
 			assertNotNull("Could not Parse/TC " + ex.getName(), ex);
 		}
 	}
-	
+
 }
