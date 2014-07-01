01e7b33aeebdb6ebfffe6a34fae74da2b1a55011
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-01 19:18:50 +0200

    Working version of Parse/TC all examples tests.
    It's parameterized too!

--- core/tests/src/test/java/org/overture/core/tests/example/ExampleModelsTest.java
+++ core/tests/src/test/java/org/overture/core/tests/example/ExampleModelsTest.java
@@ -4,32 +4,56 @@ import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Vector;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.overture.core.tests.AllExamplesHelper;
+import org.overture.core.tests.AllExamplesHelper.ExampleAstData;
+import org.overture.parser.lex.LexException;
+import org.overture.parser.syntax.ParserException;
 import org.overture.tools.examplepackager.util.ExampleTestData;
-import org.overture.tools.examplepackager.util.ExampleTestUtils;
 
 /**
  * Basic test for all the examples. Simply ensures that they all parse and type-check correctly. Examples with
- * purposeful errors are not tested at all. This test needs to be improved to also check examples with specific errors.
- * 
+ * purposeful errors are not tested at all. This test needs to be improved to also check examples with expected errors.
  * 
  * @author ldc
  */
+
+@RunWith(Parameterized.class)
 public class ExampleModelsTest
 {
+	ExampleTestData testData;
 
-	// FIXME make Example Models Tests test for models with errors as well.
+	public ExampleModelsTest(String _, ExampleTestData testData)
+	{
+		this.testData = testData;
+	}
+
+	@Parameters(name = "{index} : {0}")
+	public static Collection<Object[]> testData()
+	{
+		Collection<Object[]> r = new Vector<Object[]>();
+
+		Collection<ExampleTestData> examples = AllExamplesHelper.getExamplesSources();
+
+		for (ExampleTestData e : examples)
+		{
+			r.add(new Object[] { e.getName(), e });
+		}
+
+		return r;
+	}
 
 	@Test
-	public void testParseTcAllExamples() throws IOException
+	public void testParseTcAllExamples() throws IOException, ParserException,
+			LexException
 	{
-		Collection<ExampleTestData> examples = ExampleTestUtils.getCorrectExamplesSources();
-
-		for (ExampleTestData ex : examples)
-		{
-			assertNotNull("Could not Parse/TC " + ex.getName(), ex);
-		}
+		ExampleAstData ex = AllExamplesHelper.parseExample(testData);
+		assertNotNull("Could not Parse/TC " + ex.getExampleName());
 	}
 
 }
