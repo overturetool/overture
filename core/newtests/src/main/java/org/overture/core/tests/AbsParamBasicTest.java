8ce584ffb9784984409252adfc3d919bafb33323
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-04 15:21:15 +0200

    Clean up ParamBasicTest Jdoc.

--- core/newtests/src/main/java/org/overture/core/tests/AbsParamBasicTest.java
+++ core/newtests/src/main/java/org/overture/core/tests/AbsParamBasicTest.java
@@ -18,42 +18,39 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 /**
- * The Abstract Parameterized Basic Test is the main test class for
- * the new tests. This class runs tests on VDM source models and compares them
- * with stored result files. It is meant to be subclassed as a way to
- * quickly create your own test suites.<br>
+ * The Abstract Parameterized Basic Test is the main test class for the new tests. This class runs tests on VDM source
+ * models and compares them with stored result files. It is meant to be subclassed as a way to quickly create your own
+ * test suites.<br>
  * <br>
- * A comparison method for results must be provided. Serialization of results is
- * as automated as possible but can be overridden if your results classes are too
- * complex for auto-deserialization. <br>
+ * A comparison method for results must be provided. Serialization of results is as automated as possible but can be
+ * overridden if your results classes are too complex for auto-deserialization. <br>
  * <br>
- * These tests are meant to be used as parameterized JUnit test and so any
- * subclass {@link AbsParamBasicTest} must be annotated with
- *  <code>@RunWith(Parameterized.class)</code>. <br>
+ * These tests are meant to be used as parameterized JUnit test and so any subclass {@link AbsParamBasicTest} must be
+ * annotated with <code>@RunWith(Parameterized.class)</code>. <br>
  * <br>
- * This class is parameterized on a type <code>R</code> that represents the
- * output of of the analysis of the functionality under test. These types must
- * implement {@link Serializable} and you will most likely need to write some
+ * This class is parameterized on a type <code>R</code> that represents the output of of the analysis of the
+ * functionality under test. These types must implement {@link Serializable} and you will most likely need to write some
  * kind of transformation between your native output type and <code>R</code>.
  * 
  * @author ldc
  */
-public abstract class AbsParamBasicTest<R extends Serializable> {
+public abstract class AbsParamBasicTest<R extends Serializable>
+{
 
 	protected String modelPath;
 	protected String resultPath;
 	protected String testName;
-	protected boolean updateResult = false;
+	private final boolean updateResult=false;
+
+	/** The property to be passed when updating test results. Configure this for you own tests **/
 
 	/**
-	 * Constructor for the test. Works with outputs gotten from
-	 * {@link PathsProvider} which must to supplied via a static method.
-	 * Subclasses must implement this method.<br>
+	 * Constructor for the test. Works with outputs gotten from {@link PathsProvider} which must to supplied via a
+	 * static method. Subclasses must implement this method.<br>
 	 * <br>
-	 * In order to use JUnit parameterized tests, you must annotate the
-	 * data-supplying method with <b>
-	 * <code>@Parameters(name = "{index} : {0}")</code></b>. Supporting
-	 * parameterized tests is also the reason the method must be static.
+	 * In order to use JUnit parameterized tests, you must annotate the data-supplying method with <b>
+	 * <code>@Parameters(name = "{index} : {0}")</code></b>. Supporting parameterized tests is also the reason the
+	 * method must be static.
 	 * 
 	 * @param testParameter
 	 *            filename for the VDM source to test
@@ -61,76 +58,16 @@ public abstract class AbsParamBasicTest<R extends Serializable> {
 	 *            test result file
 	 */
 	public AbsParamBasicTest(String nameParameter, String testParameter,
-			String resultParameter) {
-		this.testName = testParameter;
+			String resultParameter)
+	{
+		this.testName = nameParameter;
 		this.modelPath = testParameter;
 		this.resultPath = resultParameter;
 	}
 
 	/**
-	 * The main test executor. Constructs ASTs and processes them via
-	 * {@link #processModel(List)} and Results via
-	 * {@link #deSerializeResult(String)}. It then compares the two according to
-	 * {@link #testCompare(Object, IResult)}.
-	 * 
-	 * @param <R>
-	 *            a result type produced by the analyzed plug-in. You may need
-	 *            to create one.
-	 * 
-	 * @throws IOException
-	 * @throws LexException
-	 * @throws ParserException
-	 */
-	@Test
-	public void testCase() throws ParserException, LexException, IOException {
-		if (updateResult) {
-			testUpdate();
-		} else {
-			List<INode> ast = ParseTcFacade.typedAst(modelPath);
-			R actual = processModel(ast);
-			R expected = deSerializeResult(resultPath);
-			this.testCompare(actual, expected);
-		}
-	}
-
-	private void testUpdate() throws ParserException, LexException, IOException {
-		List<INode> ast = ParseTcFacade.typedAst(modelPath);
-		R actual = processModel(ast);
-		Gson gson = new Gson();
-		String json = gson.toJson(actual);
-		IOUtils.write(json, new FileOutputStream(resultPath));
-	}
-
-	/**
-	 * Compares output of the processed model with a previous. This method must
-	 * be overridden to implement result comparison behavior. Don't forget to
-	 * assert something.
-	 * 
-	 * @param actual
-	 *            the processed model
-	 * @param expected
-	 *            the stored result
-	 */
-	public abstract void testCompare(R actual, R expected);
-
-	/**
-	 * Analyses a model (represented by its AST). This method must be overridden
-	 * to perform whatever analysis the functionality under test performs.<br>
-	 * <br>
-	 * The output of this method must be of type <code>R</code> that implements
-	 * {@link IResult}.
-	 * 
-	 * @param ast
-	 *            representing the model to process
-	 * @return the output of the analysis
-	 */
-	public abstract R processModel(List<INode> ast);
-
-	/**
-	 * This method tries its best to deserialize any results file. If your
-	 * results are too complex for it to handle, you should override
-	 * {@link #getResultType()} to deal with it. If that fails, override this
-	 * entire.
+	 * This method tries its best to deserialize any results file. If your results are too complex for it to handle, you
+	 * should override {@link #getResultType()} to deal with it. If that fails, override this entire.
 	 * 
 	 * @param resultPath
 	 * @return the stored result
@@ -138,7 +75,8 @@ public abstract class AbsParamBasicTest<R extends Serializable> {
 	 * @throws FileNotFoundException
 	 */
 	public R deSerializeResult(String resultPath) throws FileNotFoundException,
-			IOException {
+			IOException
+	{
 		Gson gson = new Gson();
 		Type resultType = getResultType();
 		String json = IOUtils.toString(new FileReader(resultPath));
@@ -147,15 +85,88 @@ public abstract class AbsParamBasicTest<R extends Serializable> {
 	}
 
 	/**
-	 * Calculates the type of the result. This method does its best but doesn't
-	 * always succeed. Override it if necessary.
+	 * Calculates the type of the result. This method does its best but doesn't always succeed. Override it if
+	 * necessary. To do this, it's usually enough to replace the type parameter <code>R</code> with the actual type of
+	 * the result (reflection is hard).
 	 * 
 	 * @return the {@link Type} of the result file
 	 */
-	public Type getResultType() {
-		Type resultType = new TypeToken<R>() {
+	public Type getResultType()
+	{
+		Type resultType = new TypeToken<R>()
+		{
 		}.getType();
 		return resultType;
 	}
+
+	/**
+	 * Return the Java System property to update this set of tests. Should have the following naming scheme:
+	 * <code>tests.update.[module].[testId]</code>. <br>
+	 * <br>
+	 * The test ID <b>must</b> be unique to each test class. Module is just there to avoid name clashes so the name of
+	 * the module is enough.
+	 * 
+	 * @return
+	 */
+	protected abstract String getUpdatePropertyString();
+
+	/**
+	 * Analyses a model (represented by its AST). This method must be overridden to perform whatever analysis the
+	 * functionality under test performs.<br>
+	 * <br>
+	 * The output of this method must be of type <code>R</code> that implements {@link IResult}.
+	 * 
+	 * @param ast
+	 *            representing the model to process
+	 * @return the output of the analysis
+	 */
+	public abstract R processModel(List<INode> ast);
+
+	/**
+	 * The main test executor. Constructs ASTs and processes them via {@link #processModel(List)} and Results via
+	 * {@link #deSerializeResult(String)}. It then compares the two according to {@link #testCompare(Object, IResult)}.
+	 * 
+	 * @param <R>
+	 *            a result type produced by the analyzed plug-in. You may need to create one.
+	 * @throws IOException
+	 * @throws LexException
+	 * @throws ParserException
+	 */
+	@Test
+	public void testCase() throws ParserException, LexException, IOException
+	{
+		if (updateResult)
+		{
+			testUpdate();
+		} else
+		{
+			List<INode> ast = ParseTcFacade.typedAst(modelPath, testName);
+			R actual = processModel(ast);
+			R expected = deSerializeResult(resultPath);
+			this.testCompare(actual, expected);
+		}
+	}
+
+	/**
+	 * Compares output of the processed model with a previous. This method must be overridden to implement result
+	 * comparison behavior. Don't forget to assert something.
+	 * 
+	 * @param actual
+	 *            the processed model
+	 * @param expected
+	 *            the stored result
+	 */
+	public abstract void testCompare(R actual, R expected);
+
+	private void testUpdate() throws ParserException, LexException, IOException
+	{
+		List<INode> ast = ParseTcFacade.typedAst(modelPath, testName);
+		R actual = processModel(ast);
+		Gson gson = new Gson();
+		String json = gson.toJson(actual);
+		IOUtils.write(json, new FileOutputStream(resultPath));
+	}
+
+
 
 }
