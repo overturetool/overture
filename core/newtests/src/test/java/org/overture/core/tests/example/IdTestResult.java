894d28297662ec5101dc853382f84e02e99a5642
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-04 13:33:03 +0200

    Update IDTest so it doesn't use filepath in result.

--- core/newtests/src/test/java/org/overture/core/tests/example/IdTestResult.java
+++ core/newtests/src/test/java/org/overture/core/tests/example/IdTestResult.java
@@ -4,20 +4,35 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Vector;
 
+import org.overture.ast.definitions.PDefinition;
+import org.overture.ast.modules.AModuleModules;
 import org.overture.ast.node.INode;
 
 public class IdTestResult extends Vector<String> implements Serializable,
-		List<String> {
+		List<String>
+{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public static IdTestResult convert(List<INode> ast) {
+	public static IdTestResult convert(List<INode> ast)
+	{
 		IdTestResult r = new IdTestResult();
-		for (INode n : ast) {
-			r.add(n.toString());
+		for (INode n : ast)
+		{
+			if (n instanceof AModuleModules) // ModuleModules prints the file path so we skip it
+			{
+				for (PDefinition p : ((AModuleModules) n).getDefs())
+				{
+					r.add(p.toString());
+				}
+
+			} else
+			{
+				r.add(n.toString());
+			}
 		}
 		return r;
 	}
