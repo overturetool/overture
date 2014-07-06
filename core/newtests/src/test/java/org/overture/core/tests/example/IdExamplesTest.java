4cd0f34d6890095fb7bcb92c07da561163155d3d
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-06 18:53:49 +0200

    Add Id test for Overture examples.
    This completes the demo tests, though some JDoc remains.

---
+++ core/newtests/src/test/java/org/overture/core/tests/example/IdExamplesTest.java
@@ -0,0 +1,39 @@
+package org.overture.core.tests.example;
+
+import java.util.List;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.overture.ast.node.INode;
+import org.overture.core.tests.ParamExamplesTest;
+
+@RunWith(Parameterized.class)
+public class IdExamplesTest extends ParamExamplesTest<IdTestResult>
+{
+
+	public IdExamplesTest(String name, List<INode> model, String result)
+	{
+		super(name, model, result);
+	}
+
+	private static final String UPDATE_PROPERTY = "tests.update.example.ExamplesID";
+
+	@Override
+	public IdTestResult processModel(List<INode> model)
+	{
+		return IdTestResult.convert(model);
+	}
+
+	@Override
+	public void compareResults(IdTestResult actual, IdTestResult expected)
+	{
+		IdTestResult.compare(actual, expected, testName);
+	}
+
+	@Override
+	protected String getUpdatePropertyString()
+	{
+		return UPDATE_PROPERTY;
+	}
+
+}
