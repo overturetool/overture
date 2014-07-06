9593cbcd4121d9efc5c9ac9edf21f24dae8d7f2d
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-06 18:55:51 +0200

    Update PogAllExamples now that the tests support it.

--- core/pog/src/test/java/org/overture/pog/tests/newtests/PogAllExamplesTest.java
+++ core/pog/src/test/java/org/overture/pog/tests/newtests/PogAllExamplesTest.java
@@ -1,20 +1,13 @@
 package org.overture.pog.tests.newtests;
 import static org.junit.Assert.fail;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Vector;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.node.INode;
-import org.overture.core.tests.AllExamplesHelper;
-import org.overture.core.tests.AllExamplesHelper.ExampleAstData;
 import org.overture.core.tests.ParamExamplesTest;
-import org.overture.parser.lex.LexException;
-import org.overture.parser.syntax.ParserException;
 import org.overture.pog.pub.IProofObligationList;
 import org.overture.pog.pub.ProofObligationGenerator;
 
@@ -27,30 +20,12 @@ import org.overture.pog.pub.ProofObligationGenerator;
 public class PogAllExamplesTest extends ParamExamplesTest<PogTestResult>
 {
 
-	public PogAllExamplesTest(String _, List<INode> model, String result)
+	public PogAllExamplesTest(String name, List<INode> model, String result)
 	{
-		super(_, model, result);
-		// uncomment line below to update results
-		// updateResult = true;
+		super(name, model, result);
 	}
 
-	private static String EXAMPLES_RESULTS_ROOT = "src/test/resources/exampleResults/";
 
-	@Parameters(name = "{index} : {0}")
-	public static Collection<Object[]> testData() throws ParserException,
-			LexException
-	{
-		Collection<ExampleAstData> examples = AllExamplesHelper.getExamplesAsts();
-		Collection<Object[]> r = new Vector<Object[]>();
-
-		for (ExampleAstData e : examples)
-		{
-			r.add(new Object[] { e.getExampleName(), e.getModel(),
-					EXAMPLES_RESULTS_ROOT + e.getExampleName() });
-		}
-
-		return r;
-	}
 
 	@Override
 	public PogTestResult processModel(List<INode> model)
@@ -63,7 +38,7 @@ public class PogAllExamplesTest extends ParamExamplesTest<PogTestResult>
 			return actual;
 		} catch (AnalysisException e)
 		{
-			fail("Could not process model.");
+			fail("Could not process model in test " + testName);
 			e.printStackTrace();
 		}
 		return null;
@@ -74,6 +49,14 @@ public class PogAllExamplesTest extends ParamExamplesTest<PogTestResult>
 	public void compareResults(PogTestResult actual, PogTestResult expected)
 	{
 		PogTestResult.compare(actual, expected);
+	}
+
+
+
+	@Override
+	protected String getUpdatePropertyString()
+	{
+		return "tests.update.pog.allexamples";
 
 	}
 
