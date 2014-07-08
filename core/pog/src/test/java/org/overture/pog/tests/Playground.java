626bdea75d400b39e8114ce8b4c3dfee795b8d2b
Luis Diogo Couto <ldc@eng.au.dk>
2014-07-08 18:57:32 +0200

    Make playground test compatible with new test framework.

--- core/pog/src/test/java/org/overture/pog/tests/Playground.java
+++ core/pog/src/test/java/org/overture/pog/tests/Playground.java
@@ -6,17 +6,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URISyntaxException;
-import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.node.INode;
+import org.overture.core.tests.ParseTcFacade;
 import org.overture.pog.pub.IProofObligation;
 import org.overture.pog.pub.IProofObligationList;
 import org.overture.pog.pub.ProofObligationGenerator;
+import org.overture.pog.tests.newtests.PogTestResult;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -25,81 +25,82 @@ import com.google.gson.reflect.TypeToken;
  * Class for quick testing and work on the pog
  * 
  * @author ldc
- * 
  */
-public class Playground {
+public class Playground
+{
 
-	
-
-	
 	// comment this annotation out when done! no need to run the test
 	//@Test
 	public void quickTest() throws AnalysisException, IOException,
-			URISyntaxException {
+			URISyntaxException
+	{
 
 		// switch this flag to update a test result file
 		boolean write_result = false;
-//		write_result = true;
+		//write_result = true;
 
 		// switch this flag to print the stored results
 		boolean show_result = false;
-//		show_result = true;
-		
+		//show_result = true;
+
 		String model = "src/test/resources/adhoc/sandbox.vdmpp";
 		String result = "src/test/resources/adhoc/sandbox.RESULT";
 
-		List<INode> ast = TestHelper.getAstFromName(model);
+		List<INode> ast = ParseTcFacade.typedAst(model, "Playground");
 
-		IProofObligationList ipol = ProofObligationGenerator
-				.generateProofObligations(ast);
+		IProofObligationList ipol = ProofObligationGenerator.generateProofObligations(ast);
 
 		System.out.println("ACTUAL POs:");
-		for (IProofObligation po : ipol) {
-			System.out.println(po.getKindString() + " / " + po.getFullPredString());
+		for (IProofObligation po : ipol)
+		{
+			System.out.println(po.getKindString() + " / "
+					+ po.getFullPredString());
 		}
 
-		if (write_result) {
+		if (write_result)
+		{
 			this.update(ipol, result);
 		}
 
-		if (show_result) {
-			this.compareWithResults(ipol, result);
+		if (show_result)
+		{
+			this.showStored(ipol, result);
 		}
 
 	}
 
-	public void compareWithResults(IProofObligationList ipol, String resultpath)
-			throws FileNotFoundException, IOException {
+	public void showStored(IProofObligationList ipol, String resultpath)
+			throws FileNotFoundException, IOException
+	{
 
 		// read and deserialize results
 		Gson gson = new Gson();
 		String json = IOUtils.toString(new FileReader(resultpath));
-		Type datasetListType = new TypeToken<Collection<PoResult>>() {
+		Type datasetListType = new TypeToken<PogTestResult>()
+		{
 		}.getType();
-		List<PoResult> lpr = gson.fromJson(json, datasetListType);
+		PogTestResult result = gson.fromJson(json, datasetListType);
 
 		System.out.println("STORED POs:");
-		for (PoResult por : lpr) {
-			System.out.println(por.getPoKind() + " / " + por.getPoExp());
+		for (String po : result)
+		{
+			System.out.println(po);
 		}
 
 	}
 
 	private void update(IProofObligationList ipol, String resultPath)
-			throws AnalysisException, IOException, URISyntaxException {
+			throws AnalysisException, IOException, URISyntaxException
+	{
 
-		List<PoResult> prl = new LinkedList<PoResult>();
-
-		for (IProofObligation po : ipol) {
-			prl.add(new PoResult(po.getKindString(), po.getFullPredString()));
-		}
+		PogTestResult result = PogTestResult.convert(ipol);
 
 		Gson gson = new Gson();
-		String json = gson.toJson(prl);
+		String json = gson.toJson(result);
 
 		IOUtils.write(json, new FileOutputStream(resultPath));
-		
-		System.out.println("\n" +resultPath + " file updated \n");
+
+		System.out.println("\n" + resultPath + " file updated \n");
 
 	}
 
