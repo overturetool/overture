9c5c6bff8b11f1cc5d87482f3af063301c3f02b1
Luis Diogo Couto <ldc@eng.au.dk>
2014-10-17 08:01:53 +0200

    Add new interface for tagging assistants.
    [Issue: #330]

---
+++ core/ast/src/main/java/org/overture/ast/assistant/IAssistant.java
@@ -0,0 +1,12 @@
+package org.overture.ast.assistant;
+
+/**
+ * {@link IAssistant} is an empty interface that is used to identify assistants
+ * in Overture.
+ * 
+ * @author ldc
+ *
+ */
+public interface IAssistant {
+
+}
