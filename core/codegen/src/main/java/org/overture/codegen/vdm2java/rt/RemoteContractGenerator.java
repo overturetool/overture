31305e2dd427765420918869c9382919b28f372e
Miran Hasanagic <miranh89@gmail.com>
2014-10-30 09:25:05 +0100

    Update the RemoteContract generator, so it uses the data which is the output from the data generator, and not applies a visitor of is own to code generate the data. This gave problems when a function in VDM needed to be code generated.

--- core/codegen/src/main/java/org/overture/codegen/vdm2java/rt/RemoteContractGenerator.java
+++ core/codegen/src/main/java/org/overture/codegen/vdm2java/rt/RemoteContractGenerator.java
@@ -9,6 +9,7 @@ import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.definitions.AClassClassDefinition;
 import org.overture.ast.definitions.AExplicitOperationDefinition;
 import org.overture.codegen.cgast.STypeCG;
+import org.overture.codegen.cgast.declarations.AClassDeclCG;
 import org.overture.codegen.cgast.declarations.AFormalParamLocalParamCG;
 import org.overture.codegen.cgast.declarations.AMethodDeclCG;
 import org.overture.codegen.cgast.declarations.ARemoteContractDeclCG;
@@ -17,63 +18,64 @@ import org.overture.codegen.ir.IRInfo;
 
 public class RemoteContractGenerator {
 
-	private Set<AClassClassDefinition> classes;
-	private IRInfo info;
-	
-	public RemoteContractGenerator(Set<AClassClassDefinition> classes, IRInfo info) {
+	private List<AClassDeclCG> irClasses;
+
+	public RemoteContractGenerator(List<AClassDeclCG> irClasses) {
 		super();
-		this.classes = classes;
-		this.info = info;
+		this.irClasses = irClasses;
 	}
 
 	public Set<ARemoteContractDeclCG> run() throws AnalysisException {
 		// TODO Auto-generated method stub
 
 		Set<ARemoteContractDeclCG> remoteContracts = new HashSet<ARemoteContractDeclCG>();
-		
-		for(AClassClassDefinition currentClass : classes)
-		{
-			String currentName = currentClass.getName().getName();
-			
+
+		for(AClassDeclCG classCg : irClasses){
+
+			String currentName = classCg.getName().toString();
+
 			ARemoteContractDeclCG remoteContract = new ARemoteContractDeclCG();
 
 			remoteContract.setName(currentName + "_i");
-			
-			List<AExplicitOperationDefinition> vdmOperations = Util.getPublicOperations(currentClass.getDefinitions());
-			
-			for(AExplicitOperationDefinition vdmOp : vdmOperations)
-			{
-				if(vdmOp.getIsConstructor()) continue;
-				AMethodDeclCG methodSignature = (AMethodDeclCG) vdmOp.apply(info.getDeclVisitor(), info);
-				//TODO : Make afunction which transforms the parameters
-				LinkedList<AFormalParamLocalParamCG> formalParams = (LinkedList<AFormalParamLocalParamCG>) methodSignature.getFormalParams().clone();
-				
-				LinkedList<AFormalParamLocalParamCG> formalParams2 = new LinkedList<AFormalParamLocalParamCG>();
-				
-				methodSignature.setFormalParams(null);
-				
-				for(AFormalParamLocalParamCG formPar : formalParams){
-					STypeCG ty = formPar.getType();
-					if(ty instanceof AClassTypeCG){
-						AClassTypeCG classTy = (AClassTypeCG) ty;
-						String className = classTy.getName().toString();
-						classTy.setName(className + "_i");
-						formPar.setType(classTy);
-					}
-					formalParams2.add(formPar);
+
+			for(AMethodDeclCG method : classCg.getMethods()){
+
+				AMethodDeclCG methodSignature = method.clone();
+
+				if(methodSignature.getName().equals("toString")){
+					//					method.setIsRemote(true);
+					//					publicMethods.add(method);
 				}
-				methodSignature.setFormalParams(formalParams2);
-				methodSignature.setIsRemote(true);
-				methodSignature.setBody(null);
-				methodSignature.setAbstract(true);
-//				methodSignature.setAbstract(false);
-				remoteContract.getMethodSignatures().add(methodSignature);
+				else if(methodSignature.getAccess().equals("public")){
+
+					if(methodSignature.getIsConstructor()) continue;
+					
+					LinkedList<AFormalParamLocalParamCG> formalParams = (LinkedList<AFormalParamLocalParamCG>) methodSignature.getFormalParams().clone();
+
+					LinkedList<AFormalParamLocalParamCG> formalParams2 = new LinkedList<AFormalParamLocalParamCG>();
+
+					methodSignature.setFormalParams(null);
+
+					for(AFormalParamLocalParamCG formPar : formalParams){
+						STypeCG ty = formPar.getType().clone();
+						if(ty instanceof AClassTypeCG){
+							AClassTypeCG classTy = (AClassTypeCG) ty;
+							String className = classTy.getName().toString();
+							classTy.setName(className + "_i");
+							formPar.setType(classTy);
+						}
+						formalParams2.add(formPar);
+					}
+					methodSignature.setFormalParams(formalParams2);
+					methodSignature.setIsRemote(true);
+					methodSignature.setAbstract(false);
+					methodSignature.setBody(null);
+					methodSignature.setStatic(false);
+					remoteContract.getMethodSignatures().add(methodSignature);
+				}
 			}
-			
 			remoteContracts.add(remoteContract);
 		}
-		
 		return remoteContracts;
 	}
-	
 }
