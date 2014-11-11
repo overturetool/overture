4ac6b028f97764d27f91fcd4c3653ad732444a41
Luis Diogo Couto <ldc@eng.au.dk>
2014-11-11 17:26:18 +0100

    Add deserialization of results from both test frameworks.
    Still works a bit wonky and relies on old results not having .RESULT as
    extension. We need to throw the old framework tests out soon.
    
    [Issue: ]

--- core/interpreter/src/test/java/org/overture/interpreter/tests/newtests/ParamInterpreterTest.java
+++ core/interpreter/src/test/java/org/overture/interpreter/tests/newtests/ParamInterpreterTest.java
@@ -34,65 +34,54 @@ import org.w3c.dom.NodeList;
 import com.google.gson.reflect.TypeToken;
 
 public abstract class ParamInterpreterTest extends
-		ParamStandardTest<StringInterpreterResult>
-{
+		ParamStandardTest<StringInterpreterResult> {
 
 	File file;
 
 	private static final String TEST_UPDATE_PROPERTY = "tests.update.interpreter.base";
 
 	public ParamInterpreterTest(String nameParameter, String inputParameter,
-			String resultParameter)
-	{
+			String resultParameter) {
 		super(nameParameter, inputParameter, resultParameter);
 		file = new File(modelPath);
 	}
 
 	@Override
-	public StringInterpreterResult processModel(List<INode> ast)
-	{
+	public StringInterpreterResult processModel(List<INode> ast) {
 		String entry = "1+1";
-		if (getEntryFile() == null || !getEntryFile().exists())
-		{
+		if (getEntryFile() == null || !getEntryFile().exists()) {
 			entry = createEntryFile();
-			if (entry == null)
-			{
-				if (getEntryFile() == null || !getEntryFile().exists())
-				{
+			if (entry == null) {
+				if (getEntryFile() == null || !getEntryFile().exists()) {
 					Assert.fail("No entry for model (" + getEntryFile() + ")");
 				}
 			}
-		} else
-		{
-			try
-			{
+		} else {
+			try {
 				entry = getEntries().get(0);
-			} catch (IOException e)
-			{
+			} catch (IOException e) {
 				fail("Could not process entry file for " + testName);
 			}
 		}
-		try
-		{
+		try {
 			Value val = InterpreterUtil.interpret(ast, entry, Settings.dialect);
 
-			Result<String> auxResult = new Result<String>(val.toString(), new Vector<IMessage>(), new Vector<IMessage>());
+			Result<String> auxResult = new Result<String>(val.toString(),
+					new Vector<IMessage>(), new Vector<IMessage>());
 			System.out.println(file.getName() + " -> " + val);
 			return StringInterpreterResult.convert(auxResult);
-		} catch (Exception e)
-		{
+		} catch (Exception e) {
 			Result<String> auxResult = ExecutionToResultTranslator.wrap(e);
 			return StringInterpreterResult.convert(auxResult);
 		}
 	}
 
-	private List<String> getEntries() throws IOException
-	{
-		BufferedReader reader = new BufferedReader(new FileReader(getEntryFile()));
+	private List<String> getEntries() throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(
+				getEntryFile()));
 		List<String> data = new Vector<String>();
 		String text = null;
-		while ((text = reader.readLine()) != null)
-		{
+		while ((text = reader.readLine()) != null) {
 			data.add(text.trim());
 		}
 		reader.close();
@@ -100,14 +89,11 @@ public abstract class ParamInterpreterTest extends
 		return data;
 	}
 
-	protected String createEntryFile()
-	{
-		try
-		{
+	protected String createEntryFile() {
+		try {
 			String tmp = search(file.getParentFile(), file.getName());
 
-			if (tmp != null && !tmp.isEmpty())
-			{
+			if (tmp != null && !tmp.isEmpty()) {
 				file.getParentFile().mkdirs();
 				FileWriter fstream = new FileWriter(getEntryFile());
 				BufferedWriter out = new BufferedWriter(fstream);
@@ -115,49 +101,60 @@ public abstract class ParamInterpreterTest extends
 				out.close();
 				return tmp;
 			}
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 		}
 		return null;
 
 	}
 
-	protected String search(File file, String name) throws IOException
-	{
-		File readme = new File(new File(file, name.substring(0, name.length() - 2)), "README.txt");
-		if (readme.exists())
-		{
+	protected String search(File file, String name) throws IOException {
+		File readme = new File(new File(file, name.substring(0,
+				name.length() - 2)), "README.txt");
+		if (readme.exists()) {
 			BufferedReader reader = null;
-			try
-			{
+			try {
 				reader = new BufferedReader(new FileReader(readme));
 
 				String text = null;
-				while ((text = reader.readLine()) != null)
-				{
+				while ((text = reader.readLine()) != null) {
 					text = text.trim();
-					if (text.startsWith("#ENTRY_POINT"))
-					{
+					if (text.startsWith("#ENTRY_POINT")) {
 						return text.substring(text.indexOf('=') + 1).trim();
 					}
 				}
-			} finally
-			{
+			} finally {
 				reader.close();
 			}
 		}
 		return null;
 	}
 
-	protected File getEntryFile()
-	{
+	protected File getEntryFile() {
 		return new File(modelPath + ".entry");
 	}
 
 	@Override
 	public StringInterpreterResult deSerializeResult(String resultPath)
-			throws FileNotFoundException, IOException
-	{
+			throws FileNotFoundException, IOException {
+		// try to deserialize JSON file under new framework
+		// try .result and .RESULT paths since the 2 frameworks use different
+		// naming schemes
+		File resultFile = new File(resultPath);
+		StringInterpreterResult r = null;
+		if (resultFile.exists()) {
+			r = super.deSerializeResult(resultPath);
+			if (r != null) {
+				return r;
+			}
+		}
+
+		resultFile = new File(modelPath + ".result");
+
+		if (!resultFile.exists()) {
+			throw new FileNotFoundException(resultFile.getPath());
+		}
+
+		// try XML deserialization inherited form the old one
 		// File resultFile = new File(file.getAbsoluteFile()+ ".result");
 		List<IMessage> warnings = new Vector<IMessage>();
 		List<IMessage> errors = new Vector<IMessage>();
@@ -166,81 +163,69 @@ public abstract class ParamInterpreterTest extends
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		DocumentBuilder db;
 
-		File resultFile = new File(modelPath + ".result");
-
-		if (!resultFile.exists())
-		{
-			throw new FileNotFoundException(resultFile.getPath());
-		}
-
-		try
-		{
+		try {
 			db = dbf.newDocumentBuilder();
 
 			Document doc = db.parse(resultFile);
 			doc.getDocumentElement().normalize();
 			NodeList nodeLst = doc.getElementsByTagName("message");
-			for (int i = 0; i < nodeLst.getLength(); i++)
-			{
+			for (int i = 0; i < nodeLst.getLength(); i++) {
 				Node node = nodeLst.item(i);
-				String nodeType = node.getAttributes().getNamedItem("messageType").getNodeValue();
-				if (nodeType.equals("error"))
-				{
+				String nodeType = node.getAttributes()
+						.getNamedItem("messageType").getNodeValue();
+				if (nodeType.equals("error")) {
 					convertNodeToMessage(errors, node);
-				} else if (nodeType.equals("warning"))
-				{
+				} else if (nodeType.equals("warning")) {
 					convertNodeToMessage(warnings, node);
 				}
 			}
 
-			for (int i = 0; i < doc.getDocumentElement().getChildNodes().getLength(); i++)
-			{
+			for (int i = 0; i < doc.getDocumentElement().getChildNodes()
+					.getLength(); i++) {
 				Node node = doc.getDocumentElement().getChildNodes().item(i);
 				if (node.getNodeName() != null
-						&& node.getNodeName().equals("result"))
-				{
+						&& node.getNodeName().equals("result")) {
 					node.normalize();
 
 					readResult = StringInterpreterResult.decodeResult(node);
 				}
 				// System.out.println(node);
 			}
-			return StringInterpreterResult.convert(new Result<String>(readResult, warnings, errors));
-		} catch (Exception e)
-		{
+			return StringInterpreterResult.convert(new Result<String>(
+					readResult, warnings, errors));
+		} catch (Exception e) {
 			return null;
 		}
 	}
 
-	private void convertNodeToMessage(List<IMessage> set, Node node)
-	{
+	private void convertNodeToMessage(List<IMessage> set, Node node) {
 
 		NamedNodeMap nnm = node.getAttributes();
 		String resource = nnm.getNamedItem("resource").getNodeValue();
-		IMessage m = new Message(resource, Integer.parseInt(nnm.getNamedItem("number").getNodeValue()), Integer.parseInt(nnm.getNamedItem("line").getNodeValue()), Integer.parseInt(nnm.getNamedItem("column").getNodeValue()), nnm.getNamedItem("message").getNodeValue());
+		IMessage m = new Message(resource, Integer.parseInt(nnm.getNamedItem(
+				"number").getNodeValue()), Integer.parseInt(nnm.getNamedItem(
+				"line").getNodeValue()), Integer.parseInt(nnm.getNamedItem(
+				"column").getNodeValue()), nnm.getNamedItem("message")
+				.getNodeValue());
 		set.add(m);
 	}
 
 	@Override
-	public Type getResultType()
-	{
-		Type resultType = new TypeToken<StringInterpreterResult>()
-		{
+	public Type getResultType() {
+		Type resultType = new TypeToken<StringInterpreterResult>() {
 		}.getType();
 		return resultType;
 
 	}
 
 	@Override
-	protected String getUpdatePropertyString()
-	{
+	protected String getUpdatePropertyString() {
 		return TEST_UPDATE_PROPERTY;
 	}
 
 	@Override
 	public void compareResults(StringInterpreterResult actual,
-			StringInterpreterResult expected)
-	{
+			StringInterpreterResult expected) {
 		expected.assertEqualResults(actual.getResult());
 	}
 
