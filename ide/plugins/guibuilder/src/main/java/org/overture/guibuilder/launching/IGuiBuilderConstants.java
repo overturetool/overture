d5e6f31fe516c90385d2497e4705d7953c96c685
Luis Diogo Couto <ldc@eng.au.dk>
2014-12-02 17:50:29 +0100

    Change constants clas to an interface.
    [Issue: #398]

--- ide/plugins/guibuilder/src/main/java/org/overture/guibuilder/launching/IGuiBuilderConstants.java
+++ ide/plugins/guibuilder/src/main/java/org/overture/guibuilder/launching/IGuiBuilderConstants.java
@@ -5,7 +5,7 @@ package org.overture.guibuilder.launching;
  * @author ldc
  *
  */
-public class IGuiBuilderConstants {
+public interface IGuiBuilderConstants {
 
 	public static String REMOTE_CONTROL_CLASS = "org.overture.guibuilder.GuiBuilderRemote";
 	public static String GUI_BUILDER_BUNDLE_ID = "org.overture.ide.plugins.guibuilder";
