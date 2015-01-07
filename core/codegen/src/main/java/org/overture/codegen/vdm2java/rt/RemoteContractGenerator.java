9b9a742bcacf33049ab1c7c204c80799debeb6ff
Miran Hasanagic <miranh89@gmail.com>
2015-01-07 15:46:15 +0100

    Thread generation for VDM-RT using concurrency

--- core/codegen/src/main/java/org/overture/codegen/vdm2java/rt/RemoteContractGenerator.java
+++ core/codegen/src/main/java/org/overture/codegen/vdm2java/rt/RemoteContractGenerator.java
@@ -8,7 +8,11 @@ import org.overture.ast.analysis.AnalysisException;
 import org.overture.codegen.cgast.declarations.AClassDeclCG;
 import org.overture.codegen.cgast.declarations.AMethodDeclCG;
 import org.overture.codegen.cgast.declarations.ARemoteContractDeclCG;
+import org.overture.codegen.cgast.types.AMethodTypeCG;
+import org.overture.codegen.cgast.types.AVoidTypeCG;
 import org.overture.codegen.ir.IRGeneratedTag;
+
+import de.hunsicker.jalopy.storage.History.Method;
 
 /*
  * This set up the remote contracts for the generated Java code
@@ -39,13 +43,36 @@ public class RemoteContractGenerator {
 			ARemoteContractDeclCG remoteContract = new ARemoteContractDeclCG();
 
 			remoteContract.setName(currentName + "_i"); // transform name
-
+			if(classCg.getThread() != null){
+				AMethodDeclCG methodSignature = new AMethodDeclCG();
+				methodSignature.setName("start");
+				methodSignature.setIsRemote(true);
+				methodSignature.setAbstract(false);
+				methodSignature.setBody(null);
+				methodSignature.setStatic(false);
+				methodSignature.setAccess("public");
+				methodSignature.setFormalParams(null);
+				methodSignature.setIsConstructor(false);
+				AMethodTypeCG methodType = new AMethodTypeCG();
+				methodType.setParams(null);
+				methodType.setResult(new AVoidTypeCG());
+				methodSignature.setMethodType(methodType);
+				remoteContract.getMethodSignatures().add(methodSignature);
+				
+			}
+			
 			for(AMethodDeclCG method : classCg.getMethods()){
 
 				AMethodDeclCG methodSignature = method.clone();
 
 				// Skip the auto generated toString() method
 				if(methodSignature.getName().equals("toString")){
+//					methodSignature.setName("start");
+//					methodSignature.setIsRemote(true);
+//					methodSignature.setAbstract(false);
+//					methodSignature.setBody(null);
+//					methodSignature.setStatic(false);
+//					remoteContract.getMethodSignatures().add(methodSignature);
 				}
 				else if(methodSignature.getAccess().equals("public") && !isIRGenerated(methodSignature)){ // if public add to remote contract
 
