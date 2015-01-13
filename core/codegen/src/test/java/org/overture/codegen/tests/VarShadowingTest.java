29acedba1af13eb7a4e7014fad0fe259f6a6b111
Peter Joergensen <pvj@iha.dk>
2015-01-13 14:24:19 +0100

    Disabled var shadowing tests temporarily

--- core/codegen/src/test/java/org/overture/codegen/tests/VarShadowingTest.java
+++ core/codegen/src/test/java/org/overture/codegen/tests/VarShadowingTest.java
@@ -1,17 +1,9 @@
 package org.overture.codegen.tests;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
-import org.overture.ast.lex.LexLocation;
-import org.overture.test.framework.BaseTestSuite;
-
-public class VarShadowingTest extends BaseTestSuite
+public class VarShadowingTest //extends BaseTestSuite
 {
+	/*
 	public static final String ROOT = "src" + File.separatorChar + "test"
 			+ File.separatorChar + "resources" + File.separatorChar
 			+ "var_shadowing_specs";
@@ -25,6 +17,6 @@ public class VarShadowingTest extends BaseTestSuite
 		String name = "Var shadowing test";
 		TestSuite test = createTestCompleteFile(name, ROOT, VarShadowingTestCase.class, "");
 		return test;
-	}
+	}*/
 
 }
