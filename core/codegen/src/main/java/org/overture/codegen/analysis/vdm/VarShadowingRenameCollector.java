d9bd6f13a179780b6b85944654ba395166593fbc
Peter Joergensen <pvj@iha.dk>
2015-01-15 16:15:08 +0100

    Renaming of variables shadowing other variables now covers constructs that uses multiple binds

--- core/codegen/src/main/java/org/overture/codegen/analysis/vdm/VarShadowingRenameCollector.java
+++ core/codegen/src/main/java/org/overture/codegen/analysis/vdm/VarShadowingRenameCollector.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+import java.util.Vector;
 
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.analysis.DepthFirstAnalysisAdaptor;
@@ -17,18 +18,27 @@ import org.overture.ast.definitions.PDefinition;
 import org.overture.ast.definitions.SFunctionDefinition;
 import org.overture.ast.definitions.SOperationDefinition;
 import org.overture.ast.definitions.traces.ALetBeStBindingTraceDefinition;
+import org.overture.ast.expressions.AExistsExp;
+import org.overture.ast.expressions.AForAllExp;
 import org.overture.ast.expressions.ALambdaExp;
 import org.overture.ast.expressions.ALetBeStExp;
 import org.overture.ast.expressions.ALetDefExp;
+import org.overture.ast.expressions.AMapCompMapExp;
+import org.overture.ast.expressions.ASetCompSetExp;
 import org.overture.ast.expressions.AVariableExp;
+import org.overture.ast.expressions.PExp;
 import org.overture.ast.intf.lex.ILexLocation;
 import org.overture.ast.intf.lex.ILexNameToken;
 import org.overture.ast.lex.LexNameList;
 import org.overture.ast.node.INode;
+import org.overture.ast.patterns.AIdentifierPattern;
+import org.overture.ast.patterns.PMultipleBind;
+import org.overture.ast.patterns.PPattern;
 import org.overture.ast.statements.ABlockSimpleBlockStm;
 import org.overture.ast.statements.ALetBeStStm;
 import org.overture.ast.statements.ALetStm;
 import org.overture.ast.statements.PStm;
+import org.overture.ast.typechecker.NameScope;
 import org.overture.codegen.ir.TempVarNameGen;
 import org.overture.codegen.logging.Logger;
 import org.overture.typechecker.assistant.ITypeCheckerAssistantFactory;
@@ -90,13 +100,11 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getParamDefinitions());
-
-		openScope(defs, node);
+		openScope(node.getParamDefinitions(), node);
 
 		node.getBody().apply(this);
 
-		endScope(defs);
+		endScope(node.getParamDefinitions());
 	}
 
 	@Override
@@ -108,13 +116,11 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(getParamDefs(node));
-
-		openScope(defs, node);
+		openScope(getParamDefs(node), node);
 
 		node.getBody().apply(this);
 
-		endScope(defs);
+		endScope(getParamDefs(node));
 	}
 
 	@Override
@@ -126,14 +132,12 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getAssignmentDefs());
-
-		openScope(defs, node);
+		openScope(node.getAssignmentDefs(), node);
 
 		visitDefs(node.getAssignmentDefs());
 		visitStms(node.getStatements());
 
-		endScope(defs);
+		endScope(node.getAssignmentDefs());
 	}
 
 	@Override
@@ -144,14 +148,12 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getLocalDefs());
-
-		openScope(defs, node);
+		openScope(node.getLocalDefs(), node);
 
 		visitDefs(node.getLocalDefs());
 		node.getExpression().apply(this);
 
-		endScope(defs);
+		endScope(node.getLocalDefs());
 	}
 
 	@Override
@@ -162,14 +164,12 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getLocalDefs());
-
-		openScope(defs, node);
+		openScope(node.getLocalDefs(), node);
 
 		visitDefs(node.getLocalDefs());
 		node.getStatement().apply(this);
 
-		endScope(defs);
+		endScope(node.getLocalDefs());
 	}
 
 	@Override
@@ -180,9 +180,7 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getDef().getDefs());
-
-		openScope(defs, node);
+		openScope(node.getDef().getDefs(), node);
 
 		node.getDef().apply(this);
 
@@ -193,7 +191,62 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 
 		node.getValue().apply(this);
 
-		endScope(defs);
+		endScope(node.getDef().getDefs());
+	}
+
+	/*
+	 * Exists1 needs no treatment it uses only a bind
+	 */
+	
+	@Override
+	public void caseAForAllExp(AForAllExp node) throws AnalysisException
+	{
+		if (!proceed(node))
+		{
+			return;
+		}
+		
+		handleMultipleBindConstruct(node, node.getBindList(), null, node.getPredicate());
+	}
+	
+	@Override
+	public void caseAExistsExp(AExistsExp node) throws AnalysisException
+	{
+		if (!proceed(node))
+		{
+			return;
+		}
+		
+		handleMultipleBindConstruct(node, node.getBindList(), null,node.getPredicate());
+	}
+	
+	
+	/*
+	 * Sequence comp needs no treatment it uses only a bind
+	 */
+	
+	@Override
+	public void caseASetCompSetExp(ASetCompSetExp node)
+			throws AnalysisException
+	{
+		if (!proceed(node))
+		{
+			return;
+		}
+		
+		handleMultipleBindConstruct(node, node.getBindings(), node.getFirst(), node.getPredicate());
+	}
+	
+	@Override
+	public void caseAMapCompMapExp(AMapCompMapExp node)
+			throws AnalysisException
+	{
+		if (!proceed(node))
+		{
+			return;
+		}
+		
+		handleMultipleBindConstruct(node, node.getBindings(), node.getFirst(), node.getPredicate());
 	}
 
 	@Override
@@ -204,8 +257,7 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getDef().getDefs());
-		openScope(defs, node);
+		openScope(node.getDef().getDefs(), node);
 
 		node.getDef().apply(this);
 
@@ -216,9 +268,9 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 
 		node.getStatement().apply(this);
 
-		endScope(defs);
+		endScope(node.getDef().getDefs());
 	}
-
+	
 	@Override
 	public void caseALetBeStBindingTraceDefinition(
 			ALetBeStBindingTraceDefinition node) throws AnalysisException
@@ -228,9 +280,7 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getDef().getDefs());
-
-		openScope(defs, node);
+		openScope(node.getDef().getDefs(), node);
 
 		node.getDef().apply(this);
 
@@ -241,7 +291,7 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 
 		node.getBody().apply(this);
 
-		endScope(defs);
+		endScope(node.getDef().getDefs());
 	}
 
 	@Override
@@ -252,19 +302,50 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 			return;
 		}
 
-		List<PDefinition> defs = collectDefs(node.getParamDefinitions());
-
-		openScope(defs, node);
+		openScope(node.getParamDefinitions(), node);
 
 		node.getExpression().apply(this);
 
-		endScope(defs);
+		endScope(node.getParamDefinitions());
 	}
 
 	@Override
 	public void caseILexNameToken(ILexNameToken node) throws AnalysisException
 	{
 		// No need to visit names
+	}
+	
+	private void handleMultipleBindConstruct(INode node, LinkedList<PMultipleBind> bindings, PExp first, PExp pred) throws AnalysisException
+	{
+		List<PDefinition> defs = getMultipleBindDefs(bindings);
+		openScope(defs, node);
+		
+		if (first != null)
+		{
+			first.apply(this);
+		}
+		
+		if(pred != null)
+		{
+			pred.apply(this);
+		}
+		
+		endScope(defs);
+	}
+	
+	private List<PDefinition> getMultipleBindDefs(List<PMultipleBind> bindings)
+	{
+		List<PDefinition> defs = new Vector<PDefinition>();
+
+		for (PMultipleBind mb : bindings)
+		{
+			for (PPattern pattern : mb.getPlist())
+			{
+				defs.addAll(af.createPPatternAssistant().getDefinitions(pattern, af.createPMultipleBindAssistant().getPossibleType(mb), NameScope.LOCAL));
+			}
+		}
+
+		return defs;
 	}
 
 	public String computeNewName(String original)
@@ -398,7 +479,7 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 
 	public void endScope(List<? extends PDefinition> defs)
 	{
-		this.defsInScope.removeAll(defs);
+		this.defsInScope.removeAll(collectDefs(defs));
 	}
 
 	private List<PDefinition> collectDefs(List<? extends PDefinition> defs)
@@ -406,34 +487,52 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 		List<PDefinition> collectedDefs = new LinkedList<PDefinition>();
 		for (PDefinition d : defs)
 		{
-			collectedDefs.addAll(af.createPDefinitionAssistant().getDefinitions(d));
+			collectedDefs.addAll(collectDefs(d));
 		}
 		return collectedDefs;
+	}
+	
+	private List<PDefinition> collectDefs(PDefinition d)
+	{
+		return af.createPDefinitionAssistant().getDefinitions(d);
 	}
 
 	private void findRenamings(PDefinition defToRename, INode defScope,
 			List<? extends PDefinition> defsOutsideScope)
 			throws AnalysisException
 	{
-		ILexNameToken defName = getName(defToRename);
-
-		if (defName == null)
-		{
-			return;
-		}
-
-		String newName = computeNewName(defName.getName());
-
-		if (!contains(defName.getLocation()))
-		{
-			renamings.add(new Renaming(defName.getLocation(), defName.getName(), newName));
-		}
-
-		Set<AVariableExp> occurences = collectVarOccurences(defToRename, defScope, defsOutsideScope);
+		List<PDefinition> localDefsOusideScope = collectDefs(defsOutsideScope);
+		List<PDefinition> defsToRename = collectDefs(defToRename);
 		
-		for (AVariableExp varExp : occurences)
+		for(PDefinition currentDef : defsToRename)
 		{
-			registerRenaming(varExp.getName(), newName);
+			ILexNameToken currentDefName = getName(currentDef);
+
+			if (currentDefName == null)
+			{
+				return;
+			}
+			
+			String newName = computeNewName(currentDefName.getName());
+
+			if (!contains(currentDefName.getLocation()))
+			{
+				renamings.add(new Renaming(currentDefName.getLocation(), currentDefName.getName(), newName));
+			}
+
+			Set<AVariableExp> occurences = collectVarOccurences(currentDef, defScope, localDefsOusideScope);
+			
+			for (AVariableExp varExp : occurences)
+			{
+				registerRenaming(varExp.getName(), newName);
+			}
+			
+			Set<AIdentifierPattern> patternsOcc = collectIdOccurences(currentDefName, defToRename);
+			
+			for(AIdentifierPattern p : patternsOcc)
+			{
+				registerRenaming(p.getName(), newName);
+			}
 		}
 	}
 
@@ -467,6 +566,15 @@ public class VarShadowingRenameCollector extends DepthFirstAnalysisAdaptor
 		defScope.apply(collector);
 
 		return collector.getVars();
+	}
+	
+	private Set<AIdentifierPattern> collectIdOccurences(ILexNameToken name, PDefinition def) throws AnalysisException
+	{
+		IdOccurencesCollector collector = new IdOccurencesCollector(name, def);
+		
+		def.apply(collector);
+		
+		return collector.getIdOccurences();
 	}
 
 	private boolean contains(PDefinition defToCheck)
