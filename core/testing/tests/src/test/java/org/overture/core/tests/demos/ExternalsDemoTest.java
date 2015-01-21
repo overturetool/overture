b2ea60834c341c1f40dbc8241a9f93f1d3277219
Luis Diogo Couto <ldc@eng.au.dk>
2015-01-21 12:02:58 +0100

    Use filename only in DemoTest error messages.
    [Issue: ]

--- core/testing/tests/src/test/java/org/overture/core/tests/demos/ExternalsDemoTest.java
+++ core/testing/tests/src/test/java/org/overture/core/tests/demos/ExternalsDemoTest.java
@@ -10,6 +10,7 @@ import org.junit.runners.Parameterized;
 import org.overture.ast.definitions.SClassDefinition;
 import org.overture.ast.factory.AstFactoryTC;
 import org.overture.ast.lex.Dialect;
+import org.overture.ast.lex.LexLocation;
 import org.overture.ast.modules.AModuleModules;
 import org.overture.config.Settings;
 import org.overture.core.tests.ParamExternalsTest;
@@ -20,7 +21,6 @@ import org.overture.parser.syntax.ParserException;
 import org.overture.parser.util.ParserUtil;
 import org.overture.parser.util.ParserUtil.ParserResult;
 import org.overture.typechecker.ClassTypeChecker;
-import org.overture.typechecker.ModuleTypeChecker;
 import org.overture.typechecker.assistant.ITypeCheckerAssistantFactory;
 import org.overture.typechecker.assistant.TypeCheckerAssistantFactory;
 import org.overture.typechecker.util.TypeCheckerUtil;
@@ -29,35 +29,37 @@ import org.overture.typechecker.util.TypeCheckerUtil.TypeCheckResult;
 import com.google.gson.reflect.TypeToken;
 
 /**
- * A simple test to demo the use of external test inputs. This class reuses {@link IdTestResult} but rather than
- * printing the entire test model, it just prints a success/failure message regarding the type checking of the test
+ * A simple test to demo the use of external test inputs. This class reuses
+ * {@link IdTestResult} but rather than printing the entire test model, it just
+ * prints a success/failure message regarding the type checking of the test
  * source. <br>
- * Also note that since this test works with external inputs, the data provider is already set up in
- * {@link ParamExternalsTest}. To launch these tests simply use the property
- * <code>-DexternalTestsPath=/path/to/files/</code>. This test is intended to run on the tc external tests only.<br>
+ * Also note that since this test works with external inputs, the data provider
+ * is already set up in {@link ParamExternalsTest}. To launch these tests simply
+ * use the property <code>-DexternalTestsPath=/path/to/files/</code>. This test
+ * is intended to run on the tc external tests only.<br>
  * <br>
- * <b>Note:</b> Due to some quirks with Parameterized JUnit tests, if the property is not set, the test will still
- * launch, only with 0 cases. It's fine in Maven but in Eclipse you will get a single test run that does nothing.
+ * <b>Note:</b> Due to some quirks with Parameterized JUnit tests, if the
+ * property is not set, the test will still launch, only with 0 cases. It's fine
+ * in Maven but in Eclipse you will get a single test run that does nothing.
  * 
  * @author ldc
  */
 @RunWith(Parameterized.class)
-public class ExternalsDemoTest extends ParamExternalsTest<IdTestResult>
-{
+public class ExternalsDemoTest extends ParamExternalsTest<IdTestResult> {
 
 	// the update property for this test
 	private static final String UPDATE_PROPERTY = "tests.update.example.ExternalsDemo";
 
 	/**
-	 * As usual in the new tests, the constructor only needs to pass the parameters up to super.
+	 * As usual in the new tests, the constructor only needs to pass the
+	 * parameters up to super.
 	 * 
 	 * @param nameParameter
 	 * @param testParameter
 	 * @param resultParameter
 	 */
 	public ExternalsDemoTest(String nameParameter, String testParameter,
-			String resultParameter)
-	{
+			String resultParameter) {
 		super(nameParameter, testParameter, resultParameter);
 	}
 
@@ -65,70 +67,65 @@ public class ExternalsDemoTest extends ParamExternalsTest<IdTestResult>
 	 * Main comparison method. Simply call on {@link IdTestResult}.
 	 */
 	@Override
-	public void compareResults(IdTestResult actual, IdTestResult expected)
-	{
+	public void compareResults(IdTestResult actual, IdTestResult expected) {
 		IdTestResult.compare(actual, expected, testName);
 	}
 
 	/**
-	 * Return the update property for this test. In general, it's good practice to do put it in a constant and return
-	 * that.
+	 * Return the update property for this test. In general, it's good practice
+	 * to do put it in a constant and return that.
 	 */
 	@Override
-	protected String getUpdatePropertyString()
-	{
+	protected String getUpdatePropertyString() {
 		return UPDATE_PROPERTY;
 	}
 
 	/**
-	 * Return the {@link Type} or reulst for this test. This is needed to help out with reflection in the
-	 * deserialization of results.
+	 * Return the {@link Type} or reulst for this test. This is needed to help
+	 * out with reflection in the deserialization of results.
 	 */
 	@Override
-	public Type getResultType()
-	{
-		Type resultType = new TypeToken<IdTestResult>()
-		{
+	public Type getResultType() {
+		Type resultType = new TypeToken<IdTestResult>() {
 		}.getType();
 		return resultType;
 	}
 
 	/**
-	 * Process the VDM source. External inputs can be negative so we control the parsing and type checking ourselves.
-	 * This makes the method much longer and more complex. If we were certain the sources were correct, we could just
-	 * call a method from {@link ParseTcFacade}.<br>
+	 * Process the VDM source. External inputs can be negative so we control the
+	 * parsing and type checking ourselves. This makes the method much longer
+	 * and more complex. If we were certain the sources were correct, we could
+	 * just call a method from {@link ParseTcFacade}.<br>
 	 * <br>
-	 * Remember that you must always track the VDM dialect of the source and the only way to do this with external
-	 * inputs is by placing them in dialect folders as we have done here. <br>
+	 * Remember that you must always track the VDM dialect of the source and the
+	 * only way to do this with external inputs is by placing them in dialect
+	 * folders as we have done here. <br>
 	 * <br>
-	 * Because of this extra size and complexity it's a good idea to split up the method according to dialect and do
-	 * some dispatching.
+	 * Because of this extra size and complexity it's a good idea to split up
+	 * the method according to dialect and do some dispatching.
 	 * 
-	 * @return an {@link IdTestResult} with a list of error messages or an all clear message
+	 * @return an {@link IdTestResult} with a list of error messages or an all
+	 *         clear message
 	 */
 	@Override
-	public IdTestResult processSource()
-	{
+	public IdTestResult processSource() {
+		// Use file names only in error messages
+		LexLocation.absoluteToStringLocation = false;
+
 		IdTestResult r = new IdTestResult();
-		if (modelPath.contains("sltest"))
-		{
+		if (modelPath.contains("sltest")) {
 			return processSl();
 		}
-		if (modelPath.contains("rttest"))
-		{
-			try
-			{
+		if (modelPath.contains("rttest")) {
+			try {
 				return processRt();
-			} catch (ParserException e)
-			{
+			} catch (ParserException e) {
 				r.add("Error performing RT parsing in " + testName);
-			} catch (LexException e)
-			{
+			} catch (LexException e) {
 				r.add("Error performing RT lexing in " + testName);
 			}
 		}
-		if (modelPath.contains("pptest"))
-		{
+		if (modelPath.contains("pptest")) {
 			return processPp();
 		}
 
@@ -138,100 +135,85 @@ public class ExternalsDemoTest extends ParamExternalsTest<IdTestResult>
 		return r;
 	}
 
-	private IdTestResult processPp()
-	{
+	private IdTestResult processPp() {
 		Settings.dialect = Dialect.VDM_PP;
 		IdTestResult r = new IdTestResult();
 
-		ParserResult<List<SClassDefinition>> pr = ParserUtil.parseOo(new File(modelPath));
-		if (pr.errors.isEmpty())
-		{
-			TypeCheckResult<List<SClassDefinition>> tr = TypeCheckerUtil.typeCheck(pr, pr.result, new ClassTypeChecker(pr.result));
-			if (tr.errors.isEmpty())
-			{
+		ParserResult<List<SClassDefinition>> pr = ParserUtil.parseOo(new File(
+				modelPath));
+		if (pr.errors.isEmpty()) {
+			TypeCheckResult<List<SClassDefinition>> tr = TypeCheckerUtil
+					.typeCheck(pr, pr.result, new ClassTypeChecker(pr.result));
+			if (tr.errors.isEmpty()) {
 				r.add(testName + " parses and type checks");
-			} else
-			{
-				for (VDMError e : tr.errors)
-				{
+			} else {
+				for (VDMError e : tr.errors) {
 					r.add(makeErrorMsg(e));
 				}
 			}
-		} else
-		{
-			for (VDMError e : pr.errors)
-			{
+		} else {
+			for (VDMError e : pr.errors) {
 				r.add(makeErrorMsg(e));
 			}
 		}
 		return r;
 	}
 
-	private IdTestResult processRt() throws ParserException, LexException
-	{
+	private IdTestResult processRt() throws ParserException, LexException {
 		Settings.dialect = Dialect.VDM_RT;
 		IdTestResult r = new IdTestResult();
 
-		ParserResult<List<SClassDefinition>> pr = ParserUtil.parseOo(new File(modelPath));
-		if (pr.errors.isEmpty())
-		{
+		ParserResult<List<SClassDefinition>> pr = ParserUtil.parseOo(new File(
+				modelPath));
+		if (pr.errors.isEmpty()) {
 			ITypeCheckerAssistantFactory af = new TypeCheckerAssistantFactory();
 			List<SClassDefinition> classes = new Vector<SClassDefinition>();
 			classes.addAll(pr.result);
 			classes.add(AstFactoryTC.newACpuClassDefinition(af));
 			classes.add(AstFactoryTC.newABusClassDefinition(af));
-			TypeCheckResult<List<SClassDefinition>> tr = TypeCheckerUtil.typeCheck(pr, pr.result, new ClassTypeChecker(pr.result));
-			if (tr.errors.isEmpty())
-			{
+			TypeCheckResult<List<SClassDefinition>> tr = TypeCheckerUtil
+					.typeCheck(pr, pr.result, new ClassTypeChecker(pr.result));
+			if (tr.errors.isEmpty()) {
 				r.add(testName + "parses and type checks");
-			} else
-			{
-				for (VDMError e : tr.errors)
-				{
+			} else {
+				for (VDMError e : tr.errors) {
 					r.add(makeErrorMsg(e));
 				}
 			}
-		} else
-		{
-			for (VDMError e : pr.errors)
-			{
+		} else {
+			for (VDMError e : pr.errors) {
 				r.add(makeErrorMsg(e));
 			}
 		}
 		return r;
 	}
 
-	private IdTestResult processSl()
-	{
+	private IdTestResult processSl() {
 		Settings.dialect = Dialect.VDM_SL;
 		IdTestResult r = new IdTestResult();
 
-		ParserResult<List<AModuleModules>> pr = ParserUtil.parseSl(new File(modelPath));
-		if (pr.errors.isEmpty())
-		{
-			TypeCheckResult<List<AModuleModules>> tr = TypeCheckerUtil.typeCheck(pr, pr.result, new ModuleTypeChecker(pr.result));
-			if (tr.errors.isEmpty())
-			{
+		File modelFile = new File(modelPath);
+
+		ParserResult<List<AModuleModules>> pr = ParserUtil.parseSl(modelFile);
+		if (pr.errors.isEmpty()) {
+			TypeCheckResult<List<AModuleModules>> tr = TypeCheckerUtil
+					.typeCheckSl(modelFile);
+			if (tr.errors.isEmpty()) {
 				r.add(testName + "parses and type checks");
-			} else
-			{
-				for (VDMError e : tr.errors)
-				{
+			} else {
+				for (VDMError e : tr.errors) {
 					r.add(makeErrorMsg(e));
 				}
 			}
-		} else
-		{
-			for (VDMError e : pr.errors)
-			{
+		} else {
+			for (VDMError e : pr.errors) {
 				r.add(makeErrorMsg(e));
 			}
 		}
 		return r;
 	}
 
-	private String makeErrorMsg(VDMError e)
-	{
+	private String makeErrorMsg(VDMError e) {
 		StringBuilder sb = new StringBuilder();
 
 		sb.append("Error ");
