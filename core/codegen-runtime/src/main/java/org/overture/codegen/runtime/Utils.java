17350b04dcc1f1e2433365c33b067883196e1de5
Peter Joergensen <pvj@iha.dk>
2015-02-04 19:52:37 +0100

    Small fix to the 'toString(..)' method in the codegen runtime Utils class

--- core/codegen-runtime/src/main/java/org/overture/codegen/runtime/Utils.java
+++ core/codegen-runtime/src/main/java/org/overture/codegen/runtime/Utils.java
@@ -102,6 +102,10 @@ public class Utils
 				return Double.toString(n.doubleValue());
 			}
 		}
+		else if(obj instanceof Character)
+		{
+			return "'" + obj + "'";
+		}
 		else if(obj instanceof String)
 		{
 			return "\"" + obj.toString() + "\"";
