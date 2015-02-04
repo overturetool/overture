a7bb6acd905cc2f6b7ec94685a6460565c670bd3
Peter Joergensen <pvj@iha.dk>
2015-02-04 21:53:40 +0100

    Added more utility functionality to the store assistant used in the traces transformation

--- core/codegen/src/main/java/org/overture/codegen/traces/StoreAssistant.java
+++ core/codegen/src/main/java/org/overture/codegen/traces/StoreAssistant.java
@@ -5,41 +5,31 @@ import java.util.Map;
 import org.overture.codegen.cgast.SExpCG;
 import org.overture.codegen.cgast.STypeCG;
 import org.overture.codegen.cgast.declarations.AVarDeclCG;
+import org.overture.codegen.cgast.expressions.ACastUnaryExpCG;
 import org.overture.codegen.cgast.expressions.AIdentifierVarExpCG;
 import org.overture.codegen.cgast.statements.ABlockStmCG;
 import org.overture.codegen.cgast.statements.ACallObjectExpStmCG;
 import org.overture.codegen.cgast.types.AClassTypeCG;
 import org.overture.codegen.cgast.types.ANatNumericBasicTypeCG;
-import org.overture.codegen.ir.IRInfo;
 import org.overture.codegen.trans.assistants.TransAssistantCG;
 
-public class StoreRegistrationAssistant
+public class StoreAssistant
 {
-	private IRInfo info;
 	private TraceNames tracePrefixes;
 	private Map<String, String> idConstNameMap;
 	private TransAssistantCG transAssistant;
 	
-	public StoreRegistrationAssistant(IRInfo info, TraceNames tracePrefixes,
+	public StoreAssistant(TraceNames tracePrefixes,
 			Map<String, String> idConstNameMap, TransAssistantCG transAssistant)
 	{
 		super();
-		this.info = info;
 		this.tracePrefixes = tracePrefixes;
 		this.idConstNameMap = idConstNameMap;
 		this.transAssistant = transAssistant;
 	}
 
-	public void appendStoreRegStms(ABlockStmCG declBlock, STypeCG varType, String varName)
+	public void appendStoreRegStms(ABlockStmCG declBlock, STypeCG varType, String varName, String idConstName)
 	{
-		String idConstName = idConstNameMap.get(varName);
-		
-		if(idConstName == null)
-		{
-			idConstName = info.getTempVarNameGen().nextVarName(tracePrefixes.idConstNamePrefix());
-		}
-		
-		declBlock.getStatements().add(transAssistant.wrap(consIdConstDecl(idConstName)));
 		declBlock.getStatements().add(consStoreRegistration(idConstName, varType, varName));
 		idConstNameMap.put(varName, idConstName);
 	}
@@ -65,5 +55,22 @@ public class StoreRegistrationAssistant
 		
 		return transAssistant.consInstanceCallStm(storageType, storeVarName, tracePrefixes.storeRegisterMethodName(),
 				idVarExp, transAssistant.consIdentifierVar(varName, varType));
+	}
+	
+	public ACastUnaryExpCG consStoreLookup(AIdentifierVarExpCG node)
+	{
+		AClassTypeCG storeType = transAssistant.consClassType(tracePrefixes.storeClassName());
+		
+		AIdentifierVarExpCG idArg = transAssistant.consIdentifierVar(idConstNameMap.get(node.getName()), 
+				new ANatNumericBasicTypeCG());
+		
+		SExpCG call = transAssistant.consInstanceCall(storeType, tracePrefixes.storeVarName(), 
+				node.getType(), tracePrefixes.storeGetValueMethodName(), idArg);
+		
+		ACastUnaryExpCG cast = new ACastUnaryExpCG();
+		cast.setType(node.getType().clone());
+		cast.setExp(call);
+		
+		return cast;
 	}
 }
