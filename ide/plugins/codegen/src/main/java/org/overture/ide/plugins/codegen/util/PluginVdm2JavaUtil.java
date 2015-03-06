ec5140faa0700186fe0f1a5b790efcb302f55df1
Peter Joergensen <pvj@iha.dk>
2015-03-06 19:08:23 +0100

    Added convenience methods to access different folders in the generated Eclipse project

--- ide/plugins/codegen/src/main/java/org/overture/ide/plugins/codegen/util/PluginVdm2JavaUtil.java
+++ ide/plugins/codegen/src/main/java/org/overture/ide/plugins/codegen/util/PluginVdm2JavaUtil.java
@@ -60,6 +60,7 @@ import org.overture.codegen.assistant.LocationAssistantCG;
 import org.overture.codegen.ir.VdmNodeInfo;
 import org.overture.codegen.utils.GeneralCodeGenUtils;
 import org.overture.codegen.vdm2java.JavaCodeGenUtil;
+import org.overture.codegen.vdm2java.JavaSettings;
 import org.overture.ide.core.resources.IVdmProject;
 import org.overture.ide.core.resources.IVdmSourceUnit;
 import org.overture.ide.core.utility.FileUtility;
@@ -70,18 +71,21 @@ import org.overture.ide.plugins.codegen.commands.Vdm2JavaCommand;
 
 public class PluginVdm2JavaUtil
 {
-	public static final String JAVA_FOLDER = "java";
 	public static final String QUOTES_FOLDER = "quotes";
 	public static final String UTILS_FOLDER = "utils";
-	public static final String CODEGEN_RUNTIME_BIN_FILE_NAME = "codegen-runtime.jar";
-	public static final String CODEGEN_RUNTIME_SOURCES_FILE_NAME = "codegen-runtime-sources.jar";
-	public static final String ECLIPSE_CLASSPATH_TEMPLATE_FILE_NAME = "cg.classpath";
-	public static final String ECLIPSE_PROJECT_TEMPLATE_FILE_NAME = "cg.project";
-	public static final String ECLIPSE_CLASSPATH_FILE_NAME = ".classpath";
-	public static final String ECLIPSE_PROJECT_FILE_NAME = ".project";
-	public static final String ECLIPSE_RES_FILES_FOLDER_NAME = "eclipsefiles";
-	public static final String CODEGEN_RUNTIME_SRC_FOLDER_NAME = "src";
-	public static final String CODEGEN_RUNTIME_LIB_FOLDER_NAME = "lib";
+	
+	public static final String CODEGEN_RUNTIME_BIN_FILE = "codegen-runtime.jar";
+	public static final String CODEGEN_RUNTIME_SOURCES_FILE = "codegen-runtime-sources.jar";
+	public static final String CODEGEN_RUNTIME_LIB_FOLDER = "lib";
+	
+	public static final String ECLIPSE_CLASSPATH_TEMPLATE_FILE = "cg.classpath";
+	public static final String ECLIPSE_PROJECT_TEMPLATE_FILE = "cg.project";
+
+	public static final String ECLIPSE_PROJECT_ROOT_FOLDER = "java";
+	public static final String ECLIPSE_CLASSPATH_FILE = ".classpath";
+	public static final String ECLIPSE_PROJECT_FILE = ".project";
+	public static final String ECLIPSE_RES_FILES_FOLDER = "eclipsefiles";
+	public static final String ECLIPSE_PROJECT_SRC_FOLDER = "src"; 
 
 	private PluginVdm2JavaUtil()
 	{
@@ -91,9 +95,9 @@ public class PluginVdm2JavaUtil
 	{
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IPath location = Path.fromOSString(file.getAbsolutePath());
-		IFile ifile = workspace.getRoot().getFileForLocation(location);
+		IFile iFile = workspace.getRoot().getFileForLocation(location);
 
-		return ifile;
+		return iFile;
 	}
 
 	public static boolean isSupportedVdmDialect(IVdmProject vdmProject)
@@ -145,24 +149,42 @@ public class PluginVdm2JavaUtil
 		return mergedParseLists;
 	}
 
-	public static File getOutputFolder(IVdmProject project)
+	public static File getEclipseProjectFolder(IVdmProject project)
+	{
+		return new File(getProjectDir(project), ECLIPSE_PROJECT_ROOT_FOLDER);
+	}
+	
+
+	public static File getCodeGenRuntimeLibFolder(IVdmProject project)
+	{
+		return getFolder(getEclipseProjectFolder(project), CODEGEN_RUNTIME_LIB_FOLDER);
+	}
+	
+	public static File getJavaCodeOutputFolder(IVdmProject project, JavaSettings settings)
 			throws CoreException
 	{
-		File outputDir = getProjectDir(project);
-		outputDir = new File(outputDir, JAVA_FOLDER);
-		outputDir.mkdirs();
+		File outputDir = getEclipseProjectFolder(project);
+		outputDir = getFolder(outputDir, ECLIPSE_PROJECT_SRC_FOLDER);
+		
+		String javaPackage = settings.getJavaRootPackage();
+		if(GeneralCodeGenUtils.isValidJavaPackage(javaPackage))
+		{
+			outputDir = getFolder(outputDir, GeneralCodeGenUtils
+					.getFolderFromJavaRootPackage(javaPackage));
+		}
+		
 		return outputDir;
 	}
 
-	public static File getQuotesFolder(IVdmProject project)
+	public static File getQuotesFolder(IVdmProject project, JavaSettings settings)
 			throws CoreException
 	{
-		return getFolder(getOutputFolder(project), QUOTES_FOLDER);
+		return getFolder(getJavaCodeOutputFolder(project, settings), QUOTES_FOLDER);
 	}
 
-	public static File getUtilsFolder(IVdmProject project) throws CoreException
+	public static File getUtilsFolder(IVdmProject project, JavaSettings settings) throws CoreException
 	{
-		return getFolder(getOutputFolder(project), UTILS_FOLDER);
+		return getFolder(getJavaCodeOutputFolder(project, settings), UTILS_FOLDER);
 	}
 
 	public static void addMarkers(String generalMessage,
@@ -172,7 +194,7 @@ public class PluginVdm2JavaUtil
 
 		for (Violation violation : list)
 		{
-			IFile ifile = PluginVdm2JavaUtil.convert(violation.getLocation().getFile());
+			IFile ifile = convert(violation.getLocation().getFile());
 			FileUtility.addMarker(ifile, generalMessage + ": "
 					+ violation.getDescripton(), violation.getLocation(), IMarker.PRIORITY_NORMAL, ICodeGenConstants.PLUGIN_ID, -1);
 		}
@@ -241,7 +263,7 @@ public class PluginVdm2JavaUtil
 			return;
 		}
 
-		IFile ifile = PluginVdm2JavaUtil.convert(location.getFile());
+		IFile ifile = convert(location.getFile());
 
 		String reason = nodeInfo.getReason();
 
@@ -261,6 +283,7 @@ public class PluginVdm2JavaUtil
 	{
 		File resultingFolder = new File(parent, folder);
 		resultingFolder.mkdirs();
+		
 		return resultingFolder;
 	}
 
