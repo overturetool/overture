bec899ac39ecef2d67700a9c4dbdee6249bfa657
Luis Diogo Couto <ldc@eng.au.dk>
2015-03-10 15:25:18 +0100

    Add stubMRF transformation
    For now, only converts base classes to extended versions.
    Function grouping comes next.
    
    [Issue: #428]

--- core/codegen/isagen/src/main/java/org/overturetool/cgisa/transformations/GroupMutRecs.java
+++ core/codegen/isagen/src/main/java/org/overturetool/cgisa/transformations/GroupMutRecs.java
@@ -1,18 +1,49 @@
 package org.overturetool.cgisa.transformations;
 
 import org.overture.cgisa.extast.analysis.DepthFirstAnalysisIsaAdaptor;
-import org.overture.cgisa.extast.declarations.AExtClassDeclCG;
+import org.overture.cgisa.extast.declarations.AIsaClassDeclCG;
 import org.overture.codegen.cgast.analysis.AnalysisException;
+import org.overture.codegen.cgast.declarations.AClassDeclCG;
+import org.overture.codegen.trans.ITotalTransformation;
 
-public class GroupMutRecs extends DepthFirstAnalysisIsaAdaptor
+public class GroupMutRecs extends DepthFirstAnalysisIsaAdaptor implements
+		ITotalTransformation
 {
 
-	
+	private AIsaClassDeclCG result = null;
+
 	@Override
-	public void caseAExtClassDeclCG(AExtClassDeclCG node)
-			throws AnalysisException
+	public void caseAClassDeclCG(AClassDeclCG node) throws AnalysisException
 	{
-		// TODO Auto-generated method stub
-		super.caseAExtClassDeclCG(node);
+		// clone original class into extended class
+		result = new AIsaClassDeclCG();
+		result.setAbstract(node.getAbstract());
+		result.setAccess(node.getAccess());
+		result.setFields(node.getFields());
+		result.setFunctions(node.getFunctions());
+		result.setInnerClasses(node.getInnerClasses());
+		result.setInterfaces(node.getInterfaces());
+		result.setMethods(node.getMethods());
+		result.setMutexSyncs(node.getMutexSyncs());
+		result.setName(node.getName());
+		result.setPackage(node.getPackage());
+		result.setPerSyncs(node.getPerSyncs());
+		result.setSourceNode(node.getSourceNode());
+		result.setStatic(node.getStatic());
+		result.setSuperName(node.getSuperName());
+		result.setTag(node.getTag());
+		result.setThread(node.getThread());
+		result.setTraces(node.getTraces());
+		result.setTypeDecls(node.getTypeDecls());
+
+		// now compute the mr func groups
+
 	}
+
+	@Override
+	public AIsaClassDeclCG getResult()
+	{
+		return result;
+	}
+
 }
