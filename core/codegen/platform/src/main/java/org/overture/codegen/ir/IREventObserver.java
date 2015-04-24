71341813e4eba9e357cb2c446575bccb30395fff
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-04-23 22:33:23 -0400

    Added support for IR event handlers
    IR event handlers enable observers to intercept and analyse the state of the IR in different stages of the code generation process

---
+++ core/codegen/platform/src/main/java/org/overture/codegen/ir/IREventObserver.java
@@ -0,0 +1,11 @@
+package org.overture.codegen.ir;
+
+import java.util.List;
+
+import org.overture.codegen.cgast.INode;
+
+public interface IREventObserver
+{
+	public void initialIRConstructed(List<IRStatus<INode>> ast);
+	public void finalIRConstructed(List<IRStatus<INode>> ast);
+}
