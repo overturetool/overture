59621d3e998b1f8c331dbe11ee021d9775e46abd
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-04-27 21:07:02 -0400

    IR event handlers now require IRInfo to be passed also

--- core/codegen/platform/src/main/java/org/overture/codegen/ir/IREventObserver.java
+++ core/codegen/platform/src/main/java/org/overture/codegen/ir/IREventObserver.java
@@ -6,6 +6,6 @@ import org.overture.codegen.cgast.INode;
 
 public interface IREventObserver
 {
-	public void initialIRConstructed(List<IRStatus<INode>> ast);
-	public void finalIRConstructed(List<IRStatus<INode>> ast);
+	public void initialIRConstructed(List<IRStatus<INode>> ast, IRInfo info);
+	public void finalIRConstructed(List<IRStatus<INode>> ast, IRInfo info);
 }
