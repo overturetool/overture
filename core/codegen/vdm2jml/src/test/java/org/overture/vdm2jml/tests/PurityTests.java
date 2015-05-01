b4abc3ce123f9ccc8521c726ac033b88643c10e8
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-05-01 14:06:23 -0400

    Added tests to exercise JML annotation of functions and operations with no pre and post conditions

---
+++ core/codegen/vdm2jml/src/test/java/org/overture/vdm2jml/tests/PurityTests.java
@@ -0,0 +1,58 @@
+package org.overture.vdm2jml.tests;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.overture.ast.analysis.AnalysisException;
+import org.overture.codegen.analysis.violations.UnsupportedModelingException;
+import org.overture.codegen.cgast.declarations.AMethodDeclCG;
+
+public class PurityTests extends AnnotationTestsBase
+{
+	@BeforeClass
+	public static void init() throws AnalysisException,
+			UnsupportedModelingException
+	{
+		AnnotationTestsBase.init("FuncsOpsOnly");
+	}
+
+	@Before
+	public void prepareTest()
+	{
+		validGeneratedModule();
+	}
+
+	@Test
+	public void testNoModuleStateAnnotations()
+	{
+		Assert.assertTrue("Expected no module state annotations", genModule.getFields().isEmpty());
+	}
+
+	@Test
+	public void testGenModuleFuncsArePure()
+	{
+		AnnotationTestsBase.assertGenFuncsPure(AnnotationTestsBase.getGenFunctions(genModule.getMethods()));
+	}
+
+	@Test
+	public void operationsNotAnnotated()
+	{
+		List<AMethodDeclCG> genOps = AnnotationTestsBase.getGenMethods(genModule.getMethods());
+
+		Assert.assertTrue("Expected the generated module to have operations", !genOps.isEmpty());
+
+		for (AMethodDeclCG op : genOps)
+		{
+			Assert.assertTrue("Expected operations for this generated module to have no annotations", op.getMetaData().isEmpty());
+		}
+	}
+
+	@Test
+	public void testNoStateInvInGenModule()
+	{
+		Assert.assertTrue("Expected no state annotations", genModule.getMetaData().isEmpty());
+	}
+}
