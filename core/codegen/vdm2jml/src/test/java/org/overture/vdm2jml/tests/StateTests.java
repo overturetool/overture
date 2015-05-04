6bbe77138fe793fb72d45b8e3adaf1aa9bcb3c92
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-05-04 15:40:11 -0400

    Changes to the way invariants are generated require updates to related test results

--- core/codegen/vdm2jml/src/test/java/org/overture/vdm2jml/tests/StateTests.java
+++ core/codegen/vdm2jml/src/test/java/org/overture/vdm2jml/tests/StateTests.java
@@ -60,7 +60,7 @@ public class StateTests extends StateTestBase
 		
 		String fieldName = genModule.getFields().getFirst().getName();
 		
-		String expected = String.format("//@ invariant inv_%1$s(%1$s);", fieldName);
+		String expected = String.format("//@ static invariant St == null || inv_%1$s(%1$s);", fieldName);
 		
 		Assert.assertEquals("Got unexpected invariant", expected, genModule.getMetaData().get(0).value);
 	}
