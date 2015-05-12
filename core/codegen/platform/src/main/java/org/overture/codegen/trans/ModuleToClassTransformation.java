bab12464ba7435c432417893a70c74790c82554d
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-05-12 19:57:18 -0400

    Fix to the module to class transformation
    Prior to this fix field reads were not made explicit in pre and post condition functions

--- core/codegen/platform/src/main/java/org/overture/codegen/trans/ModuleToClassTransformation.java
+++ core/codegen/platform/src/main/java/org/overture/codegen/trans/ModuleToClassTransformation.java
@@ -242,12 +242,51 @@ public class ModuleToClassTransformation extends DepthFirstAnalysisAdaptor
 
 		module.apply(new DepthFirstAnalysisAdaptor()
 		{
+			private boolean inPreOrPost = false;
+
+			@Override
+			public void caseAFuncDeclCG(AFuncDeclCG node)
+					throws AnalysisException
+			{
+				handleMethodOrFunc(node.getBody(), node.getPreCond(), node.getPostCond());
+			}
+			
+			@Override
+			public void caseAMethodDeclCG(AMethodDeclCG node)
+					throws AnalysisException
+			{
+				handleMethodOrFunc(node.getBody(), node.getPreCond(), node.getPostCond());
+			}
+
+			private void handleMethodOrFunc(INode body, SDeclCG preCond,
+					SDeclCG postCond) throws AnalysisException
+			{
+				if(body != null)
+				{
+					body.apply(this);
+				}
+				
+				handleCond(preCond);
+				handleCond(postCond);
+			}
+
+			private void handleCond(SDeclCG cond) throws AnalysisException
+			{
+				if(cond != null)
+				{
+					inPreOrPost = true;
+					cond.apply(this);
+					inPreOrPost = false;
+				}
+			}
+			
 			@Override
 			public void caseAIdentifierVarExpCG(AIdentifierVarExpCG node)
 					throws AnalysisException
 			{
-				if ((info.getExpAssistant().isOld(node.getName()) && !node.getName().equals("_" + stateDecl.getName())) ||
-						!node.getIsLocal() && !node.getName().equals(stateDecl.getName()))
+				if (isOldFieldRead(stateDecl, node) /*1*/
+						|| isFieldRead(stateDecl, node) /*2*/
+						|| isFieldReadInPreOrPost(stateDecl, node) /*3*/)
 				{
 					// Given a state named 'St' with a field named 'f'
 					// Note: If a variable is 'old' then it is local since state is passed
@@ -256,13 +295,24 @@ public class ModuleToClassTransformation extends DepthFirstAnalysisAdaptor
 					// Also note that the IR uses underscore to denote old names: _St corresponds to St~ (in VDM)
 					// and _f corresponds to f~ (in VDM)
 					
-					// In case we are in a post condition the left hand side
-					// of the OR checks if the variable expression is implicit access
-					// to a field of the old state in the post condition, i.e._f (or f~ in VDM)
+					// /*1*/ In case we are in a post condition the variable expression may represent a
+					// field of the old state, i.e._f (or f~ in VDM)
 					//
-					// Any other place in the model: The right hand side of the OR checks if
-					// the variable expression is implicit access to a field of the current state
-					
+					// /*3*/ Another possibility is that the variable expression represents
+					// a field of the current state in a pre or post condition of a function or
+					// an operation. This is a special case since the state field can be
+					// read as 'f' although it is only the entire state that is being passed as an argument
+					// to the function. Below is a Java example showin this (result, old state,
+					// current state):
+					//
+					// public static Boolean post_op(final Number RESULT, final St _St, final St St)
+					//
+					// /*2*/ Any other place in the model:
+					// The variable expression represents a field of the current state
+					//
+					// If /*1*/ or /*2*/ or /*3*/ is true we make the field access explicit since IR modules
+					// use a single field to represent state.
+					//
 					AFieldExpCG fieldExp = new AFieldExpCG();
 					fieldExp.setSourceNode(node.getSourceNode());
 					fieldExp.setTag(node.getTag());
@@ -271,6 +321,41 @@ public class ModuleToClassTransformation extends DepthFirstAnalysisAdaptor
 
 					transAssistant.replaceNodeWith(node, fieldExp);
 				}
+			}
+
+			private boolean isFieldReadInPreOrPost(
+					final AStateDeclCG stateDecl, AIdentifierVarExpCG node)
+			{
+				if(!inPreOrPost)
+				{
+					return false;
+				}
+				
+				boolean matches = false;
+				for(AFieldDeclCG f : stateDecl.getFields())
+				{
+					if(f.getName().equals(node.getName()))
+					{
+						matches = true;
+						break;
+					}
+				}
+				
+				return matches;
+			}
+			
+			private boolean isFieldRead(
+					final AStateDeclCG stateDecl, AIdentifierVarExpCG node)
+			{
+				return !node.getIsLocal()
+						&& !node.getName().equals(stateDecl.getName());
+			}
+
+			private boolean isOldFieldRead(final AStateDeclCG stateDecl,
+					AIdentifierVarExpCG node)
+			{
+				return info.getExpAssistant().isOld(node.getName())
+						&& !node.getName().equals("_" + stateDecl.getName());
 			}
 
 			private void setFieldNames(AFieldExpCG field, AStateDeclCG stateDecl, String name)
