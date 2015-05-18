bf2ed158ce73d911f3b299dc033f5dd660c23e09
Luis Diogo Couto <ldc@eng.au.dk>
2015-05-18 15:07:53 +0200

    Add playground test to run multi-file modes.

--- core/pog/src/test/java/org/overture/pog/tests/Playground.java
+++ core/pog/src/test/java/org/overture/pog/tests/Playground.java
@@ -1,21 +1,28 @@
 package org.overture.pog.tests;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.overture.ast.analysis.AnalysisException;
+import org.overture.ast.lex.Dialect;
 import org.overture.ast.node.INode;
 import org.overture.config.Release;
 import org.overture.config.Settings;
 import org.overture.core.tests.ParseTcFacade;
+import org.overture.parser.lex.LexException;
+import org.overture.parser.syntax.ParserException;
 import org.overture.pog.pub.IProofObligation;
 import org.overture.pog.pub.IProofObligationList;
 import org.overture.pog.pub.ProofObligationGenerator;
@@ -37,10 +44,22 @@ public class Playground
 		quickPog("src/test/resources/adhoc/sandbox.vdmsl");
 	}
 
-	@Ignore
 	@Test
 	public void sandboxPp() throws AnalysisException, IOException, URISyntaxException{
 		quickPog("src/test/resources/adhoc/sandbox.vdmpp");
+	}
+	
+	@Test
+	public void sandboxProject() throws FileNotFoundException, AnalysisException, IOException, URISyntaxException{
+		Settings.release = Release.CLASSIC;
+		
+		Collection<File> x = FileUtils.listFiles(new File("src/test/resources/adhoc/proj"), new String[]{"vdmpp"}, false);
+		
+		List<File> sources= new LinkedList<File>();
+		sources.addAll(x);
+		
+		List<INode> ast = ParseTcFacade.typedAstNoRetry(sources, "Playground", Dialect.VDM_PP);
+		processAst(true, true, "src/test/resources/adhoc/sandboxproj.result", ast);
 	}
 	
 	public void quickPog(String model) throws AnalysisException, IOException,
@@ -60,6 +79,14 @@ public class Playground
 		Settings.release = Release.VDM_10;
 		List<INode> ast = ParseTcFacade.typedAst(model, "Playground");
 
+		processAst(write_result, show_result, result, ast);
+
+	}
+
+	private void processAst(boolean write_result, boolean show_result,
+			String result, List<INode> ast) throws AnalysisException,
+			IOException, URISyntaxException, FileNotFoundException
+	{
 		IProofObligationList ipol = ProofObligationGenerator.generateProofObligations(ast);
 
 		System.out.println("ACTUAL POs:");
@@ -78,7 +105,6 @@ public class Playground
 		{
 			this.showStored(ipol, result);
 		}
-
 	}
 
 	public void showStored(IProofObligationList ipol, String resultpath)
