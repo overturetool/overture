9499e4559643ec3f3ad75573a87ebe67e0b144a0
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-05-25 12:34:39 -0400

    Two updates to 'LeafTypeInfo':
    1) Functionality to compute leaf types
    2) Functionality to construct the IR type from a VDM type

--- core/codegen/vdm2jml/src/main/java/org/overture/codegen/vdm2jml/LeafTypeInfo.java
+++ core/codegen/vdm2jml/src/main/java/org/overture/codegen/vdm2jml/LeafTypeInfo.java
@@ -1,6 +1,13 @@
 package org.overture.codegen.vdm2jml;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.types.PType;
+import org.overture.codegen.cgast.STypeCG;
+import org.overture.codegen.ir.IRInfo;
+import org.overture.codegen.logging.Logger;
 
 public class LeafTypeInfo extends AbstractTypeInfo
 {
@@ -34,5 +41,32 @@ public class LeafTypeInfo extends AbstractTypeInfo
 		{
 			return type.toString();
 		}
+	}
+	
+	public STypeCG toIrType(IRInfo info)
+	{
+		try
+		{
+			return type.apply(info.getTypeVisitor(), info);
+		} catch (AnalysisException e)
+		{
+			Logger.getLog().printErrorln("Problems encountered while attempting "
+					+ "to construct the IR type from a VDM type: "
+					+ e.getMessage()
+					+ " in '"
+					+ this.getClass().getSimpleName() + "'");
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	public List<LeafTypeInfo> getLeafTypesRecursively()
+	{
+		List<LeafTypeInfo> types = new LinkedList<LeafTypeInfo>();
+		types.add(this);
+		
+		return types;
 	}
 }
