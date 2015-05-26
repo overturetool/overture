080b3ce0e6b6106a7bac89ce7518bbbd0e6e11a4
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-05-25 21:58:20 -0400

    Added test to exercise parameter name generation

---
+++ core/codegen/vdm2jml/src/test/java/org/overture/vdm2jml/tests/NameGenTests.java
@@ -0,0 +1,55 @@
+package org.overture.vdm2jml.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.overture.codegen.cgast.declarations.AClassDeclCG;
+import org.overture.codegen.cgast.declarations.AFieldDeclCG;
+import org.overture.codegen.vdm2jml.NameGen;
+
+public class NameGenTests
+{
+	public static final String MSG = "Got unexpected name suggestion from name generator";
+	
+	private NameGen nameGen;
+	
+	@Before
+	public void init()
+	{
+		AClassDeclCG testClass = new AClassDeclCG();
+		
+		AFieldDeclCG field = new AFieldDeclCG();
+		field.setName("field");
+		
+		testClass.getFields().add(field);
+		
+		this.nameGen = new NameGen(testClass); 
+	}
+	
+	@Test
+	public void notTaken()
+	{
+		Assert.assertEquals(MSG, "x", nameGen.getName("x"));
+	}
+	
+	@Test
+	public void suggestSameNameTwice()
+	{
+		nameGen.getName("x");
+		Assert.assertEquals(MSG, "x_1", nameGen.getName("x"));
+	}
+	
+	@Test
+	public void suggestSameNameThreeTimes()
+	{
+		nameGen.getName("x");
+		nameGen.getName("x");
+		Assert.assertEquals(MSG, "x_2", nameGen.getName("x"));
+	}
+	
+	@Test
+	public void suggestField()
+	{
+		Assert.assertEquals(MSG, "field_1",  nameGen.getName("field")); 
+	}
+}
