0083c8f2f8a536486b3c5c7e020b662d82de5a2a
Luis Diogo Couto <ldc@eng.au.dk>
2015-06-02 12:43:00 +0200

    Add new test for translating expressions.

---
+++ core/codegen/isagen/src/test/java/org/overturetool/cgisa/IsaGenExpTest.java
@@ -0,0 +1,63 @@
+package org.overturetool.cgisa;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.overture.ast.analysis.AnalysisException;
+import org.overture.ast.expressions.PExp;
+import org.overture.codegen.utils.GeneratedModule;
+import org.overture.core.tests.ParseTcFacade;
+
+@RunWith(Parameterized.class)
+public class IsaGenExpTest
+
+{
+
+	String expression;
+	String expected;
+
+	public IsaGenExpTest(String input, String result)
+	{
+		this.expression = input;
+		this.expected = result;
+	}
+
+	@Parameters(name = "{index}: exp: {0}")
+	public static Collection<Object[]> getParams()
+	{
+		Object[] a1 = { "1+1", "(1 + 1)" };
+		Object[] a2 = { "1-1", "(1 - 1)" };
+		Object[] a3 = { "1/1", "1/1" };
+		Object[] a4 = { "1*1", "1*1" };
+
+		return Arrays.asList(new Object[][] { a1,a2,a3,a4 });
+
+	}
+
+	@Test
+	public void testCase() throws AnalysisException,
+			org.overture.codegen.cgast.analysis.AnalysisException
+	{
+		PExp exp = ParseTcFacade.parseTcExpressionString(expression);
+		IsaGen isagen = new IsaGen();
+		GeneratedModule actual = isagen.generateIsabelleSyntax(exp);
+		
+		if (actual.hasMergeErrors()){
+			fail(actual.getMergeErrors().toString());
+		}
+		
+		if (actual.hasUnsupportedTargLangNodes()){
+			fail(actual.getUnsupportedInTargLang().toString());
+		}
+		
+		assertEquals(expected, actual.getContent());
+
+	}
+
+}
