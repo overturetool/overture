6bf4d2d7a67ae0f63eb17bcba7f841bf429ddaa5
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-24 14:37:14 +0200

    Added toString to Execution result

--- core/codegen/javagen/src/test/java/org/overture/codegen/tests/exec/ExecutionResult.java
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/exec/ExecutionResult.java
@@ -4,19 +4,27 @@ public class ExecutionResult
 {
 	private String strRepresentation;
 	private Object executionResult;
-	
+
 	public ExecutionResult(String strRepresentation, Object executionResult)
 	{
 		this.strRepresentation = strRepresentation;
 		this.executionResult = executionResult;
 	}
-	
+
 	public String getStrRepresentation()
 	{
 		return strRepresentation;
 	}
+
 	public Object getExecutionResult()
 	{
 		return executionResult;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "String representation: " + strRepresentation
+				+ ".\nExecution result: " + executionResult;
 	}
 }
