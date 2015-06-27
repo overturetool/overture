830d2a3f81a89c0536a3529de091b2ce0dd7c9d1
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-27 19:00:06 +0200

    Updating concurrency tests to use the testing framework

---
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/ConcurrencyOutputTest.java
@@ -0,0 +1,45 @@
+package org.overture.codegen.tests.output;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.overture.codegen.ir.IRSettings;
+import org.overture.core.tests.PathsProvider;
+
+@RunWith(Parameterized.class)
+public class ConcurrencyOutputTest extends OutputTestPp
+{
+	public ConcurrencyOutputTest(String nameParameter, String inputParameter,
+			String resultParameter)
+	{
+		super(nameParameter, inputParameter, resultParameter);
+	}
+
+	public static final String ROOT = "src" + File.separatorChar + "test"
+			+ File.separatorChar + "resources" + File.separatorChar
+			+ "concurrency";
+	
+	@Override
+	public IRSettings getIrSettings()
+	{
+		IRSettings irSettings = new IRSettings();
+		irSettings.setGenerateConc(true);
+
+		return irSettings;
+	}
+	
+	@Parameters(name = "{index} : {0}")
+	public static Collection<Object[]> testData()
+	{
+		return PathsProvider.computePaths(ROOT);
+	}
+
+	@Override
+	protected String getUpdatePropertyString()
+	{
+		return UPDATE_PROPERTY_PREFIX + "concurrency";
+	}
+}
