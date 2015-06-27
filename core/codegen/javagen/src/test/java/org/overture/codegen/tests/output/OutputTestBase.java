0db166b34e2991ff334d3b3a00d1971060166bb1
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-27 19:57:43 +0200

    Refactoring 'output' test base classes

--- core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/OutputTestBase.java
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/OutputTestBase.java
@@ -9,12 +9,17 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.node.INode;
+import org.overture.codegen.analysis.violations.InvalidNamesResult;
 import org.overture.codegen.analysis.violations.UnsupportedModelingException;
 import org.overture.codegen.ir.IRSettings;
+import org.overture.codegen.logging.Logger;
+import org.overture.codegen.utils.GeneralCodeGenUtils;
 import org.overture.codegen.utils.GeneralUtils;
 import org.overture.codegen.utils.GeneratedData;
+import org.overture.codegen.utils.GeneratedModule;
 import org.overture.codegen.vdm2java.JavaCodeGen;
 import org.overture.codegen.vdm2java.JavaSettings;
 import org.overture.core.tests.ParamStandardTest;
@@ -43,6 +48,16 @@ public abstract class OutputTestBase extends ParamStandardTest<String>
 	{
 		super(nameParameter, inputParameter, resultParameter);
 	}
+	
+	@Before
+	public void init()
+	{
+		Logger.getLog().setSilent(true);
+
+		vdmCodGen.clear();
+		vdmCodGen.setSettings(getIrSettings());
+		vdmCodGen.setJavaSettings(getJavaSettings());
+	}
 
 	public IRSettings getIrSettings()
 	{
@@ -58,6 +73,62 @@ public abstract class OutputTestBase extends ParamStandardTest<String>
 		javaSettings.setDisableCloning(false);
 
 		return javaSettings;
+	}
+	
+	@Override
+	public String processModel(List<INode> ast)
+	{
+		try
+		{
+			StringBuilder generatedCode = new StringBuilder();
+			GeneratedData data = genCode(ast);
+			List<GeneratedModule> classes = data.getClasses();
+
+			for (GeneratedModule classCg : classes)
+			{
+				generatedCode.append(classCg.getContent());
+				generatedCode.append(MODULE_DELIMITER);
+			}
+
+			List<GeneratedModule> quoteData = data.getQuoteValues();
+
+			if (quoteData != null && !quoteData.isEmpty())
+			{
+				generatedCode.append(QUOTE_INDICATOR + LINE_SEPARATOR);
+				for (int i = 0; i < quoteData.size(); i++)
+				{
+					GeneratedModule q = quoteData.get(i);
+
+					generatedCode.append(q.getName());
+
+					if (i + 1 < quoteData.size())
+					{
+						generatedCode.append(QUOTE_SEPARATOR);
+					}
+
+				}
+				generatedCode.append(MODULE_DELIMITER);
+			}
+
+			InvalidNamesResult invalidNames = data.getInvalidNamesResult();
+
+			if (invalidNames != null && !invalidNames.isEmpty())
+			{
+				generatedCode.append(NAME_VIOLATION_INDICATOR + LINE_SEPARATOR);
+				generatedCode.append(LINE_SEPARATOR
+						+ GeneralCodeGenUtils.constructNameViolationsString(invalidNames));
+				generatedCode.append(MODULE_DELIMITER);
+			}
+
+			return generatedCode.toString();
+
+		} catch (UnsupportedModelingException | AnalysisException e)
+		{
+			Assert.fail("Unexpected problem encountered when attempting to code generate VDM model: "
+					+ e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	abstract public GeneratedData genCode(List<INode> ast)
--- core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/PpSpecificationTest.java
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/PpSpecificationTest.java
@@ -9,12 +9,8 @@ import org.overture.ast.analysis.AnalysisException;
 import org.overture.ast.definitions.SClassDefinition;
 import org.overture.ast.lex.Dialect;
 import org.overture.ast.node.INode;
-import org.overture.codegen.analysis.violations.InvalidNamesResult;
 import org.overture.codegen.analysis.violations.UnsupportedModelingException;
-import org.overture.codegen.logging.Logger;
-import org.overture.codegen.utils.GeneralCodeGenUtils;
 import org.overture.codegen.utils.GeneratedData;
-import org.overture.codegen.utils.GeneratedModule;
 import org.overture.config.Release;
 import org.overture.config.Settings;
 
@@ -29,13 +25,9 @@ abstract public class PpSpecificationTest extends OutputTestBase
 	@Before
 	public void init()
 	{
+		super.init();
 		Settings.dialect = Dialect.VDM_PP;
 		Settings.release = Release.VDM_10;
-		Logger.getLog().setSilent(true);
-
-		vdmCodGen.clear();
-		vdmCodGen.setSettings(getIrSettings());
-		vdmCodGen.setJavaSettings(getJavaSettings());
 	}
 
 	public GeneratedData genCode(List<INode> ast) throws AnalysisException,
@@ -59,60 +51,4 @@ abstract public class PpSpecificationTest extends OutputTestBase
 	}
 
 	abstract protected String getUpdatePropertyString();
-	
-	@Override
-	public String processModel(List<INode> ast)
-	{
-		try
-		{
-			StringBuilder generatedCode = new StringBuilder();
-			GeneratedData data = genCode(ast);
-			List<GeneratedModule> classes = data.getClasses();
-
-			for (GeneratedModule classCg : classes)
-			{
-				generatedCode.append(classCg.getContent());
-				generatedCode.append(MODULE_DELIMITER);
-			}
-
-			List<GeneratedModule> quoteData = data.getQuoteValues();
-
-			if (quoteData != null && !quoteData.isEmpty())
-			{
-				generatedCode.append(QUOTE_INDICATOR + LINE_SEPARATOR);
-				for (int i = 0; i < quoteData.size(); i++)
-				{
-					GeneratedModule q = quoteData.get(i);
-
-					generatedCode.append(q.getName());
-
-					if (i + 1 < quoteData.size())
-					{
-						generatedCode.append(QUOTE_SEPARATOR);
-					}
-
-				}
-				generatedCode.append(MODULE_DELIMITER);
-			}
-
-			InvalidNamesResult invalidNames = data.getInvalidNamesResult();
-
-			if (invalidNames != null && !invalidNames.isEmpty())
-			{
-				generatedCode.append(NAME_VIOLATION_INDICATOR + LINE_SEPARATOR);
-				generatedCode.append(LINE_SEPARATOR
-						+ GeneralCodeGenUtils.constructNameViolationsString(invalidNames));
-				generatedCode.append(MODULE_DELIMITER);
-			}
-
-			return generatedCode.toString();
-
-		} catch (UnsupportedModelingException | AnalysisException e)
-		{
-			Assert.fail("Unexpected problem encountered when attempting to code generate VDM model: "
-					+ e.getMessage());
-			e.printStackTrace();
-			return null;
-		}
-	}
 }
