e124353d7d4b751b1291661724b97cc4cc53b592
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-27 19:36:55 +0200

    Updating pre/post tests to use the testing framework

--- core/codegen/javagen/src/test/java/org/overture/codegen/tests/exec/PrePostJavaGenTest.java
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/exec/PrePostJavaGenTest.java
@@ -9,7 +9,7 @@ import org.junit.runners.Parameterized.Parameters;
 import org.overture.ast.lex.Dialect;
 import org.overture.codegen.execution.tests.CommonJavaGenCheckerTest;
 import org.overture.codegen.ir.IRSettings;
-import org.overture.codegen.tests.PrePostTest;
+import org.overture.codegen.tests.output.PrePostOutputTest;
 import org.overture.config.Release;
 
 @RunWith(value = Parameterized.class)
@@ -23,7 +23,7 @@ public class PrePostJavaGenTest extends CommonJavaGenCheckerTest
 	@Parameters(name = "{0}")
 	public static Collection<Object[]> getData()
 	{
-		return collectTests(new File(PrePostTest.ROOT),new ExecutableSpecTestHandler(Release.VDM_10, Dialect.VDM_PP));
+		return collectTests(new File(PrePostOutputTest.ROOT),new ExecutableSpecTestHandler(Release.VDM_10, Dialect.VDM_PP));
 	}
 
 	@Override
---
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/PrePostOutputTest.java
@@ -0,0 +1,49 @@
+package org.overture.codegen.tests.output;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.overture.codegen.ir.IRSettings;
+import org.overture.core.tests.PathsProvider;
+
+@RunWith(Parameterized.class)
+public class PrePostOutputTest extends PpSpecificationTest
+{
+	public static final String ROOT = "src" + File.separatorChar + "test"
+			+ File.separatorChar + "resources" + File.separatorChar
+			+ "pre_post_specs";
+
+	public PrePostOutputTest(String nameParameter, String inputParameter,
+			String resultParameter)
+	{
+		super(nameParameter, inputParameter, resultParameter);
+	}
+	
+	@Override
+	public IRSettings getIrSettings()
+	{
+		IRSettings irSettings = new IRSettings();
+
+		irSettings.setGeneratePreConds(true);
+		irSettings.setGeneratePreCondChecks(true);
+		irSettings.setGeneratePostConds(true);
+		irSettings.setGeneratePostCondChecks(true);
+
+		return irSettings;
+	}
+
+	@Parameters(name = "{index} : {0}")
+	public static Collection<Object[]> testData()
+	{
+		return PathsProvider.computePaths(ROOT);
+	}
+
+	@Override
+	protected String getUpdatePropertyString()
+	{
+		return UPDATE_PROPERTY_PREFIX + "prepost";
+	}
+}
