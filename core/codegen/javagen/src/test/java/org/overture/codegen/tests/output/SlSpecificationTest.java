79d7ef4391f07fa9049057a6b2c98d1a6dfda05f
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-27 19:55:49 +0200

    Introducing SL 'output' test base class

---
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/SlSpecificationTest.java
@@ -0,0 +1,51 @@
+package org.overture.codegen.tests.output;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.overture.ast.analysis.AnalysisException;
+import org.overture.ast.lex.Dialect;
+import org.overture.ast.modules.AModuleModules;
+import org.overture.ast.node.INode;
+import org.overture.codegen.analysis.violations.UnsupportedModelingException;
+import org.overture.codegen.utils.GeneratedData;
+import org.overture.config.Release;
+import org.overture.config.Settings;
+
+abstract public class SlSpecificationTest extends OutputTestBase
+{
+	public SlSpecificationTest(String nameParameter, String inputParameter,
+			String resultParameter)
+	{
+		super(nameParameter, inputParameter, resultParameter);
+	}
+
+	@Before
+	public void init()
+	{
+		super.init();
+		Settings.dialect = Dialect.VDM_SL;
+		Settings.release = Release.VDM_10;
+	}
+
+	public GeneratedData genCode(List<INode> ast) throws AnalysisException,
+			UnsupportedModelingException
+	{
+		List<AModuleModules> modules = new LinkedList<AModuleModules>();
+
+		for (INode c : ast)
+		{
+			if (c instanceof AModuleModules)
+			{
+				modules.add((AModuleModules) c);
+			} else
+			{
+				Assert.fail("Expected only modules got " + c);
+			}
+		}
+
+		return vdmCodGen.generateJavaFromVdmModules(modules);
+	}
+}
