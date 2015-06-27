c7413dc21402faa71544547929b6fbe50d8c5017
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-27 19:25:50 +0200

    Updating function value tests to use the testing framework

---
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/FunctionValueOutputTest.java
@@ -0,0 +1,35 @@
+package org.overture.codegen.tests.output;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.overture.core.tests.PathsProvider;
+
+@RunWith(Parameterized.class)
+public class FunctionValueOutputTest extends PpSpecificationTest
+{
+	public static final String ROOT = "src" + File.separatorChar + "test"
+			+ File.separatorChar + "resources" + File.separatorChar
+			+ "function_value_specs";
+
+	public FunctionValueOutputTest(String nameParameter, String inputParameter,
+			String resultParameter)
+	{
+		super(nameParameter, inputParameter, resultParameter);
+	}
+
+	@Parameters(name = "{index} : {0}")
+	public static Collection<Object[]> testData()
+	{
+		return PathsProvider.computePaths(ROOT);
+	}
+
+	@Override
+	protected String getUpdatePropertyString()
+	{
+		return UPDATE_PROPERTY_PREFIX + "functionvalue";
+	}
+}
