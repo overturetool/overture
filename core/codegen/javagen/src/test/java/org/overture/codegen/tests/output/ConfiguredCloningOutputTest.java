52b30dc9f6921c1d5d731217eb60f706f20193d2
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-06-27 19:04:47 +0200

    Updating configured cloning tests to use the testing framework

---
+++ core/codegen/javagen/src/test/java/org/overture/codegen/tests/output/ConfiguredCloningOutputTest.java
@@ -0,0 +1,55 @@
+package org.overture.codegen.tests.output;
+
+import java.io.File;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.overture.codegen.vdm2java.JavaSettings;
+import org.overture.config.Release;
+import org.overture.config.Settings;
+import org.overture.core.tests.PathsProvider;
+
+@RunWith(Parameterized.class)
+public class ConfiguredCloningOutputTest extends OutputTestPp
+{
+	public static final String ROOT = "src" + File.separatorChar + "test"
+			+ File.separatorChar + "resources" + File.separatorChar
+			+ "cloning_specs";
+	
+	public ConfiguredCloningOutputTest(String nameParameter,
+			String inputParameter, String resultParameter)
+	{
+		super(nameParameter, inputParameter, resultParameter);
+	}
+	
+	@Before
+	public void init()
+	{
+		super.init();
+		Settings.release = Release.CLASSIC;
+	}
+
+	@Override
+	public JavaSettings getJavaSettings()
+	{
+		JavaSettings javaSettings = new JavaSettings();
+		javaSettings.setDisableCloning(true);
+
+		return javaSettings;
+	}
+	
+	@Parameters(name = "{index} : {0}")
+	public static Collection<Object[]> testData()
+	{
+		return PathsProvider.computePaths(ROOT);
+	}
+
+	@Override
+	protected String getUpdatePropertyString()
+	{
+		return UPDATE_PROPERTY_PREFIX + "configuredcloning";
+	}
+}
