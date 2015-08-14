3f2a7e25c4bd69f8d4e61f78848686573194db0b
Nick Battle <nick.battle@gmail.com>
2015-08-14 10:29:48 +0100

    Prohibit time and threadid within functions

--- core/typechecker/src/main/java/org/overture/typechecker/visitor/TypeCheckerExpVisitor.java
+++ core/typechecker/src/main/java/org/overture/typechecker/visitor/TypeCheckerExpVisitor.java
@@ -2674,6 +2674,11 @@ public class TypeCheckerExpVisitor extends AbstractTypeCheckVisitor
 			TypeCheckerErrors.report(3346, "Cannot use threadid in pure operations", node.getLocation(), node);
 		}
 
+		if (Settings.release == Release.VDM_10 && question.env.isFunctional())
+		{
+			TypeCheckerErrors.report(3348, "Cannot use threadid in a functional context", node.getLocation(), node);
+		}
+
 		node.setType(AstFactory.newANatNumericBasicType(node.getLocation()));
 		return question.assistantFactory.createPTypeAssistant().checkConstraint(question.constraint, node.getType(), node.getLocation());
 	}
@@ -2686,6 +2691,11 @@ public class TypeCheckerExpVisitor extends AbstractTypeCheckVisitor
 		if (encl != null && encl.getAccess().getPure())
 		{
 			TypeCheckerErrors.report(3346, "Cannot use time in pure operations", node.getLocation(), node);
+		}
+
+		if (Settings.release == Release.VDM_10 && question.env.isFunctional())
+		{
+			TypeCheckerErrors.report(3348, "Cannot use time in a functional context", node.getLocation(), node);
 		}
 		
 		node.setType(AstFactory.newANatNumericBasicType(node.getLocation()));
