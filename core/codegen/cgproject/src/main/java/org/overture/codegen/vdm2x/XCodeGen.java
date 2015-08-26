fed1c501a3a8b1f3bb1a31ecca21686ed7e45227
mha <miranh89@gmail.com>
2015-08-26 16:41:14 +0200

    Minor changes

--- core/codegen/cgproject/src/main/java/org/overture/codegen/vdm2x/XCodeGen.java
+++ core/codegen/cgproject/src/main/java/org/overture/codegen/vdm2x/XCodeGen.java
@@ -9,26 +9,24 @@ import org.overture.ast.definitions.SClassDefinition;
 import org.overture.codegen.cgast.declarations.AClassDeclCG;
 import org.overture.codegen.ir.CodeGenBase;
 import org.overture.codegen.ir.IRStatus;
-import org.overture.codegen.merging.MergeVisitor;
-import org.overture.codegen.merging.TemplateCallable;
-import org.overture.codegen.merging.TemplateManager;
-import org.overture.codegen.merging.TemplateStructure;
 import org.overture.codegen.utils.GeneratedData;
 import org.overture.codegen.utils.GeneratedModule;
 
 public class XCodeGen extends CodeGenBase {
-
 	
-	
-	public GeneratedData generateJavaFromVdm(List<SClassDefinition> ast)
+	public GeneratedData generateXFromVdm(List<SClassDefinition> ast)
 			throws AnalysisException
 	{
 		List<IRStatus<org.overture.codegen.cgast.INode>> statuses = new LinkedList<>();
+		
+		// This is run pr. class
 		for(SClassDefinition node : ast)
 		{	
 			// Try to produce the IR
 			IRStatus<org.overture.codegen.cgast.INode> status = generator.generateFrom(node);
+			System.out.println("Running..");
 			
+			// If it was successful, then status is different from null
 			if(status != null)
 			{
 				statuses.add(status);
--- core/codegen/cgproject/src/main/java/org/overture/codegen/vdm2x/XGenMain.java
+++ core/codegen/cgproject/src/main/java/org/overture/codegen/vdm2x/XGenMain.java
@@ -16,7 +16,6 @@ public class XGenMain {
 	public static void main(String[] args) {
 
 		// Se i JavaCodeGenMain
-		System.out.println("Test?");
 
 		File file = new File(args[0]);
 
@@ -28,7 +27,7 @@ public class XGenMain {
 					files, Dialect.VDM_PP);
 
 			XCodeGen xGen = new XCodeGen();
-			GeneratedData data = xGen.generateJavaFromVdm(ast);
+			GeneratedData data = xGen.generateXFromVdm(ast);
 		
 			for (GeneratedModule module : data.getClasses()) {
 				
