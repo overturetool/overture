5f3968282ac161f5d15344d072f1caf7ac6c3c8b
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-10 21:13:30 +0200

    Test checking of reals

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/basictype/RealParamNil/Entry.java
@@ -0,0 +1,39 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        Number r = 1.23;
+        //@ assert (r == null || Utils.is_real(r));
+        IO.println("Before valid use.");
+        doSkip(r);
+        r = null;
+        //@ assert (r == null || Utils.is_real(r));
+        IO.println("After valid use.");
+        IO.println("Before invalid use.");
+        doSkip(r);
+        IO.println("After invalid use.");
+
+        return 0L;
+    }
+
+    public static void doSkip(final Number ignorePattern_1) {
+        //@ assert Utils.is_real(ignorePattern_1);
+
+        /* skip */
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
