0314c380ccbdcbe24a19eceb49e5955d4b7c2e9a
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-10 18:38:43 +0200

    Test checking of the int type

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/basictype/IntAssignNonInt/Entry.java
@@ -0,0 +1,33 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        Number i = -1L;
+        //@ assert Utils.is_int(i);
+        IO.println("Before valid use.");
+        i = 1L;
+        //@ assert Utils.is_int(i);
+        IO.println("After valid use.");
+        IO.println("Before invalid use.");
+        i = i.longValue() + 0.5;
+        //@ assert Utils.is_int(i);
+        IO.println("After invalid use.");
+
+        return 0L;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
