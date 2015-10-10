3e4c9c8b16876bd7fd667b23be7e56d2654dfc26
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-10 18:22:57 +0200

    Add test to exercise checking of the nat type

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/basictype/NatParamNil/Entry.java
@@ -0,0 +1,44 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        Number n = 0L;
+        //@ assert (n == null || Utils.is_nat(n));
+        IO.println("Before valid use.");
+        n = 1L;
+        //@ assert (n == null || Utils.is_nat(n));
+        n = null;
+        //@ assert (n == null || Utils.is_nat(n));
+        IO.println("After valid use.");
+        IO.println("Before invalid use.");
+        n = idNat(n);
+        //@ assert (n == null || Utils.is_nat(n));
+        IO.println("After invalid use.");
+
+        return 0L;
+    }
+
+    /*@ pure @*/
+    public static Number idNat(final Number x) {
+        //@ assert Utils.is_nat(x);
+        Number ret_1 = x;
+
+        //@ assert Utils.is_nat(ret_1);
+        return ret_1;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
