7e68e5ee77c5d6c6a858a44747ac57ffb60719bc
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-10 22:28:48 +0200

    Test checking quote types

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/basictype/QuoteAssignNil/Entry.java
@@ -0,0 +1,53 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        IO.println("Before valid use.");
+
+        {
+            project.quotes.AQuote aOpt = null;
+
+            //@ assert (aOpt == null || Utils.is_(aOpt,project.quotes.AQuote.class));
+
+            /* skip */
+        }
+
+        IO.println("After valid use.");
+        IO.println("Before invalid use.");
+
+        {
+            project.quotes.AQuote a = Nil();
+
+            //@ assert Utils.is_(a,project.quotes.AQuote.class);
+
+            /* skip */
+        }
+
+        IO.println("After invalid use.");
+
+        return 0L;
+    }
+
+    /*@ pure @*/
+    public static project.quotes.AQuote Nil() {
+        project.quotes.AQuote ret_1 = null;
+
+        //@ assert (ret_1 == null || Utils.is_(ret_1,project.quotes.AQuote.class));
+        return ret_1;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
