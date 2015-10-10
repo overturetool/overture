d2a024b8cbdeb13f3fc06d3688aa1a038f00a0a6
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-10 20:02:12 +0200

    Add test to exercise handling of rational numbers

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/basictype/RatAssignBool/Entry.java
@@ -0,0 +1,41 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        Number i = 123.456;
+        //@ assert Utils.is_rat(i);
+        IO.println("Before valid use.");
+        i = i.doubleValue() * i.doubleValue();
+        //@ assert Utils.is_rat(i);
+        IO.println("After valid use.");
+        IO.println("Before invalid use.");
+        i = ratOpt();
+        //@ assert Utils.is_rat(i);
+        IO.println("After invalid use.");
+
+        return 0L;
+    }
+
+    /*@ pure @*/
+    public static Number ratOpt() {
+        Number ret_1 = null;
+
+        //@ assert (ret_1 == null || Utils.is_rat(ret_1));
+        return ret_1;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
