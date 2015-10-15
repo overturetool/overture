089d7c5ebdec6d42b7da62438f76f9305f775e52
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-15 10:23:58 +0200

    Add test exercising checking of tuple types of different size

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/tupletype/TupleSizeMismatch/Entry.java
@@ -0,0 +1,61 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        IO.println("Before legal use");
+
+        {
+            Tuple ignorePattern_1 = ((Tuple) Tup4());
+
+            //@ assert (V2J.isTup(ignorePattern_1,4) && Utils.is_nat(V2J.field(ignorePattern_1,0)) && Utils.is_nat(V2J.field(ignorePattern_1,1)) && Utils.is_char(V2J.field(ignorePattern_1,2)) && Utils.is_bool(V2J.field(ignorePattern_1,3)));
+
+            /* skip */
+        }
+
+        IO.println("After legal use");
+        IO.println("Before illegal use");
+
+        {
+            Tuple ignorePattern_2 = ((Tuple) Tup3());
+
+            //@ assert (V2J.isTup(ignorePattern_2,4) && Utils.is_nat(V2J.field(ignorePattern_2,0)) && Utils.is_nat(V2J.field(ignorePattern_2,1)) && Utils.is_char(V2J.field(ignorePattern_2,2)) && Utils.is_bool(V2J.field(ignorePattern_2,3)));
+
+            /* skip */
+        }
+
+        IO.println("After illegal use");
+
+        return 0L;
+    }
+
+    /*@ pure @*/
+    public static Object Tup3() {
+        Object ret_1 = Tuple.mk_(1L, 'a', true);
+
+        //@ assert ((V2J.isTup(ret_1,3) && Utils.is_nat(V2J.field(ret_1,0)) && Utils.is_char(V2J.field(ret_1,1)) && Utils.is_bool(V2J.field(ret_1,2))) || (V2J.isTup(ret_1,4) && Utils.is_nat(V2J.field(ret_1,0)) && Utils.is_nat(V2J.field(ret_1,1)) && Utils.is_char(V2J.field(ret_1,2)) && Utils.is_bool(V2J.field(ret_1,3))));
+        return ret_1;
+    }
+
+    /*@ pure @*/
+    public static Object Tup4() {
+        Object ret_2 = Tuple.mk_(1L, 2L, 'b', false);
+
+        //@ assert ((V2J.isTup(ret_2,3) && Utils.is_nat(V2J.field(ret_2,0)) && Utils.is_char(V2J.field(ret_2,1)) && Utils.is_bool(V2J.field(ret_2,2))) || (V2J.isTup(ret_2,4) && Utils.is_nat(V2J.field(ret_2,0)) && Utils.is_nat(V2J.field(ret_2,1)) && Utils.is_char(V2J.field(ret_2,2)) && Utils.is_bool(V2J.field(ret_2,3))));
+        return ret_2;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
