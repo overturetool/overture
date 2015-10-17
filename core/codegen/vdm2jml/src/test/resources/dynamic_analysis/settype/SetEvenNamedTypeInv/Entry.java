383be796096b4351c9be561f3088f1cccfbf173d
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-17 11:04:31 +0200

    Add more tests to exercise JML generation of set types

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/settype/SetEvenNamedTypeInv/Entry.java
@@ -0,0 +1,66 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        IO.println("Before legal use");
+
+        {
+            VDMSet ignorePattern_1 = SetUtil.set(2L, 4L, 6L);
+
+            //@ assert ((V2J.isSet(ignorePattern_1) && (\forall int i; 0 <= i && i < V2J.size(ignorePattern_1); (Utils.is_nat(V2J.get(ignorePattern_1,i)) && inv_Entry_Even(V2J.get(ignorePattern_1,i))))) && inv_Entry_SetEven(ignorePattern_1));
+
+            /* skip */
+        }
+
+        IO.println("After legal use");
+        IO.println("Before illegal use");
+
+        {
+            VDMSet xs = SetUtil.set(2L);
+
+            //@ assert ((V2J.isSet(xs) && (\forall int i; 0 <= i && i < V2J.size(xs); (Utils.is_nat(V2J.get(xs,i)) && inv_Entry_Even(V2J.get(xs,i))))) && inv_Entry_SetEven(xs));
+            VDMSet ys = SetUtil.set(1L);
+
+            //@ assert (V2J.isSet(ys) && (\forall int i; 0 <= i && i < V2J.size(ys); Utils.is_nat(V2J.get(ys,i))));
+            VDMSet ignorePattern_2 = SetUtil.union(Utils.copy(xs),
+                    Utils.copy(ys));
+
+            //@ assert ((V2J.isSet(ignorePattern_2) && (\forall int i; 0 <= i && i < V2J.size(ignorePattern_2); (Utils.is_nat(V2J.get(ignorePattern_2,i)) && inv_Entry_Even(V2J.get(ignorePattern_2,i))))) && inv_Entry_SetEven(ignorePattern_2));
+
+            /* skip */
+        }
+
+        IO.println("After illegal use");
+
+        return 0L;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+
+    /*@ pure @*/
+    /*@ helper @*/
+    public static Boolean inv_Entry_SetEven(final Object check_elem) {
+        return true;
+    }
+
+    /*@ pure @*/
+    /*@ helper @*/
+    public static Boolean inv_Entry_Even(final Object check_e) {
+        Number e = ((Number) check_e);
+
+        return Utils.equals(Utils.mod(e.longValue(), 2L), 0L);
+    }
+}
---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/settype/SetPassNill/Entry.java
@@ -0,0 +1,57 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        IO.println("Before legal use");
+
+        {
+            VDMSet r = idSet(SetUtil.set(true, false));
+
+            //@ assert (V2J.isSet(r) && (\forall int i; 0 <= i && i < V2J.size(r); Utils.is_bool(V2J.get(r,i))));
+
+            /* skip */
+        }
+
+        IO.println("After legal use");
+        IO.println("Before illegal use");
+
+        {
+            VDMSet xs = null;
+
+            //@ assert ((xs == null) || (V2J.isSet(xs) && (\forall int i; 0 <= i && i < V2J.size(xs); Utils.is_bool(V2J.get(xs,i)))));
+            VDMSet r = idSet(Utils.copy(xs));
+
+            //@ assert (V2J.isSet(r) && (\forall int i; 0 <= i && i < V2J.size(r); Utils.is_bool(V2J.get(r,i))));
+
+            /* skip */
+        }
+
+        IO.println("After illegal use");
+
+        return 0L;
+    }
+
+    /*@ pure @*/
+    public static VDMSet idSet(final VDMSet xs) {
+        //@ assert (V2J.isSet(xs) && (\forall int i; 0 <= i && i < V2J.size(xs); Utils.is_bool(V2J.get(xs,i))));
+        VDMSet ret_1 = Utils.copy(xs);
+
+        //@ assert (V2J.isSet(ret_1) && (\forall int i; 0 <= i && i < V2J.size(ret_1); Utils.is_bool(V2J.get(ret_1,i))));
+        return Utils.copy(ret_1);
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
