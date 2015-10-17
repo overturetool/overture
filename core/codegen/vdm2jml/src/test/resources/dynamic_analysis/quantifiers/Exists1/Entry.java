59366d2ed9b72a627eb1f86ad54880249a2956e1
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-17 18:02:28 +0200

    Add tests to exercise handling of quantified expressions

---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/quantifiers/Exists1/Entry.java
@@ -0,0 +1,51 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        Boolean ignorePattern_1 = f();
+        //@ assert Utils.is_bool(ignorePattern_1);
+        {
+            IO.println("Done! Expected no errors");
+
+            return 0L;
+        }
+    }
+
+    /*@ pure @*/
+    public static Boolean f() {
+        Long exists1Counter_1 = 0L;
+
+        VDMSet set_1 = SetUtil.set(1L, 2L, 3L);
+
+        //@ assert (V2J.isSet(set_1) && (\forall int i; 0 <= i && i < V2J.size(set_1); Utils.is_nat1(V2J.get(set_1,i))));
+        for (Iterator iterator_1 = set_1.iterator();
+                iterator_1.hasNext() && (exists1Counter_1.longValue() < 2L);) {
+            Number x = ((Number) iterator_1.next());
+
+            //@ assert Utils.is_nat1(x);
+            if (x.longValue() > 0L) {
+                exists1Counter_1++;
+            }
+        }
+
+        Boolean ret_1 = Utils.equals(exists1Counter_1, 1L);
+
+        //@ assert Utils.is_bool(ret_1);
+        return ret_1;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
---
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/quantifiers/ForAll/Entry.java
@@ -0,0 +1,51 @@
+package project;
+
+import org.overture.codegen.runtime.*;
+
+import java.util.*;
+
+
+//@ nullable_by_default
+@SuppressWarnings("all")
+final public class Entry {
+    /*@ public ghost static boolean invChecksOn = true; @*/
+    private Entry() {
+    }
+
+    public static Object Run() {
+        Boolean ignorePattern_1 = f();
+        //@ assert Utils.is_bool(ignorePattern_1);
+        {
+            IO.println("Done! Expected no errors");
+
+            return 0L;
+        }
+    }
+
+    /*@ pure @*/
+    public static Boolean f() {
+        Boolean forAllExpResult_1 = true;
+
+        //@ assert Utils.is_bool(forAllExpResult_1);
+        VDMSet set_1 = SetUtil.set(1L, 2L, 3L);
+
+        //@ assert (V2J.isSet(set_1) && (\forall int i; 0 <= i && i < V2J.size(set_1); Utils.is_nat1(V2J.get(set_1,i))));
+        for (Iterator iterator_1 = set_1.iterator();
+                iterator_1.hasNext() && forAllExpResult_1;) {
+            Number x = ((Number) iterator_1.next());
+            //@ assert Utils.is_nat1(x);
+            forAllExpResult_1 = x.longValue() > 0L;
+
+            //@ assert Utils.is_bool(forAllExpResult_1);
+        }
+
+        Boolean ret_1 = forAllExpResult_1;
+
+        //@ assert Utils.is_bool(ret_1);
+        return ret_1;
+    }
+
+    public String toString() {
+        return "Entry{}";
+    }
+}
