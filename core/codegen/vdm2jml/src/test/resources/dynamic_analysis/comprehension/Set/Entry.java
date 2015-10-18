0eea4de51693a5653c46975fb1357196387cea7e
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-10-18 11:19:00 +0200

    Cleaning up comprehension tests

--- core/codegen/vdm2jml/src/test/resources/dynamic_analysis/comprehension/Set/Entry.java
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/comprehension/Set/Entry.java
@@ -121,14 +121,6 @@ final public class Entry {
         return 0L;
     }
 
-    /*@ pure @*/
-    public static VDMSet xs() {
-        VDMSet ret_1 = SetUtil.set();
-
-        //@ assert (V2J.isSet(ret_1) && (\forall int i; 0 <= i && i < V2J.size(ret_1); Utils.is_nat(V2J.get(ret_1,i))));
-        return Utils.copy(ret_1);
-    }
-
     public String toString() {
         return "Entry{}";
     }
