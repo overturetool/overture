95252ba288aa58994a5f4e416c1e5b3c4922ced2
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-11-12 22:58:10 +0100

    Small fix to the command-line JML generator frontend Exit on wrong usage

--- core/codegen/vdm2jml/src/main/java/org/overture/codegen/vdm2jml/JmlGenMain.java
+++ core/codegen/vdm2jml/src/main/java/org/overture/codegen/vdm2jml/JmlGenMain.java
@@ -149,5 +149,6 @@ public class JmlGenMain
 		Logger.getLog().printErrorln(FOLDER_ARG + " <folder path>: a folder containing input .vdmsl files");
 		Logger.getLog().printErrorln(INVARIANT_FOR
 				+ ": to check record invariants explicitly using JML's invariant_for");
+		System.exit(1);
 	}
 }
