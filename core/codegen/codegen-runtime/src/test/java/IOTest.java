20001055cd9591e948158b6f4d026b8d28f58108
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2015-11-27 10:27:00 +0100

    Test file access via the Java code generator runtime library

--- core/codegen/codegen-runtime/src/test/java/IOTest.java
+++ core/codegen/codegen-runtime/src/test/java/IOTest.java
@@ -1,24 +1,36 @@
-import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.overture.codegen.runtime.IO;
 import org.overture.codegen.runtime.SeqUtil;
 import org.overture.codegen.runtime.Tuple;
 import org.overture.codegen.runtime.VDMSeq;
 
-
 public class IOTest
 {
 	@Test
 	public void testSprintfStr()
 	{
-		VDMSeq args = SeqUtil.seq("tuple", Tuple.mk_(1,2));
+		VDMSeq args = SeqUtil.seq("tuple", Tuple.mk_(1, 2));
 		String format = "A %s: %s";
-		
+
 		@SuppressWarnings("unchecked")
 		String actual = IO.sprintf(format, args);
 		String expected = "A tuple: mk_(1, 2)";
-		
-		assertEquals("Unexpected string returned from IO.sprintf", expected, actual);
+
+		Assert.assertEquals("Unexpected string returned from IO.sprintf", expected, actual);
+	}
+
+	@Test
+	public void fechoNoParentDirs()
+	{
+		String filename = "target/path/to/file";
+
+		new IO().fecho(filename, "Some message", null /* Works like passing <start> */);
+
+		File file = new File(filename);
+		Assert.assertTrue("Expected " + file.getAbsolutePath() + " to exists", file.exists());
 	}
 }
