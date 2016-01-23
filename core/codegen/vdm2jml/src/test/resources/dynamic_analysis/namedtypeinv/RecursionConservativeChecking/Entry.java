8bb35a4109bcf762e8c37113966789d35dd2c1ae
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-01-23 18:41:48 +0100

    Updates to 'vdm2jml' result files
    Expect more object copying

--- core/codegen/vdm2jml/src/test/resources/dynamic_analysis/namedtypeinv/RecursionConservativeChecking/Entry.java
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/namedtypeinv/RecursionConservativeChecking/Entry.java
@@ -17,28 +17,28 @@ final public class Entry {
         Object ret_1 = 1L;
 
         //@ assert ((Utils.is_nat(ret_1) || (V2J.isSeq(ret_1) && (\forall int i; 0 <= i && i < V2J.size(ret_1); false))) && inv_Entry_T(ret_1));
-        return ret_1;
+        return Utils.copy(ret_1);
     }
 
     public static Object tSeq() {
         Object ret_2 = SeqUtil.seq(SeqUtil.seq(SeqUtil.seq(1L)));
 
         //@ assert ((Utils.is_nat(ret_2) || (V2J.isSeq(ret_2) && (\forall int i; 0 <= i && i < V2J.size(ret_2); false))) && inv_Entry_T(ret_2));
-        return ret_2;
+        return Utils.copy(ret_2);
     }
 
     public static Object t1Nat() {
         Object ret_3 = 1L;
 
         //@ assert (((V2J.isTup(ret_3,2) && Utils.is_nat(V2J.field(ret_3,0)) && false) || Utils.is_nat(ret_3)) && inv_Entry_T1(ret_3));
-        return ret_3;
+        return Utils.copy(ret_3);
     }
 
     public static Object t1Tup() {
         Object ret_4 = Tuple.mk_(1L, 2L);
 
         //@ assert (((V2J.isTup(ret_4,2) && Utils.is_nat(V2J.field(ret_4,0)) && false) || Utils.is_nat(ret_4)) && inv_Entry_T1(ret_4));
-        return ret_4;
+        return Utils.copy(ret_4);
     }
 
     public static Object Run() {
