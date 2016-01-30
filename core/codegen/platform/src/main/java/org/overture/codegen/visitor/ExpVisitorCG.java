04c914ee9da688a32656f5fa449658e109a4b547
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-01-30 13:00:58 +0100

    Guard against missing types for string literals

--- core/codegen/platform/src/main/java/org/overture/codegen/visitor/ExpVisitorCG.java
+++ core/codegen/platform/src/main/java/org/overture/codegen/visitor/ExpVisitorCG.java
@@ -157,8 +157,11 @@ import org.overture.codegen.cgast.expressions.AXorBoolBinaryExpCG;
 import org.overture.codegen.cgast.name.ATypeNameCG;
 import org.overture.codegen.cgast.patterns.ASetBindCG;
 import org.overture.codegen.cgast.types.ABoolBasicTypeCG;
+import org.overture.codegen.cgast.types.ACharBasicTypeCG;
 import org.overture.codegen.cgast.types.AClassTypeCG;
 import org.overture.codegen.cgast.types.ARecordTypeCG;
+import org.overture.codegen.cgast.types.ASeqSeqTypeCG;
+import org.overture.codegen.cgast.types.AStringTypeCG;
 import org.overture.codegen.cgast.utils.AHeaderLetBeStCG;
 import org.overture.codegen.ir.IRInfo;
 import org.overture.codegen.logging.Logger;
@@ -1722,7 +1725,21 @@ public class ExpVisitorCG extends AbstractVisitorCG<IRInfo, SExpCG>
 		{
 			PType type = node.getType();
 
-			STypeCG typeCg = type.apply(question.getTypeVisitor(), question);
+			STypeCG typeCg;
+			
+			/**
+			 * The operation argument passed to the 'setPriority' of the CPU class (VDM-RT) gets transformed into a
+			 * string literal where the type is missing: CPU1.setPriority(B`op, 4). The check below is really to
+			 * cirumvent this issue.
+			 */
+			if(type != null)
+			{
+				typeCg = type.apply(question.getTypeVisitor(), question);
+			}
+			else
+			{
+				typeCg = new AStringTypeCG();
+			}
 
 			AStringLiteralExpCG stringLiteral = new AStringLiteralExpCG();
 
@@ -1734,8 +1751,23 @@ public class ExpVisitorCG extends AbstractVisitorCG<IRInfo, SExpCG>
 
 		} else
 		{
-			STypeCG seqTypeCg = node.getType().apply(question.getTypeVisitor(), question);
-			return question.getExpAssistant().consCharSequence(seqTypeCg, value);
+			PType type = node.getType();
+			STypeCG strType;
+			
+			// Same issue with the 'setPriority' operation (see above)
+			if(type != null)
+			{
+				strType = type.apply(question.getTypeVisitor(), question);
+			}
+			else
+			{
+				ASeqSeqTypeCG seqTypeCg = new ASeqSeqTypeCG();
+				seqTypeCg.setEmpty(false);
+				seqTypeCg.setSeqOf(new ACharBasicTypeCG());
+				strType = seqTypeCg;
+			}
+			
+			return question.getExpAssistant().consCharSequence(strType, value);
 		}
 	}
 
