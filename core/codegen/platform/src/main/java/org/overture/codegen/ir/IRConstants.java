f8a9d7069a4ffdc6f678aad22cb7dff553ca3c25
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-01-30 11:57:35 +0100

    Remove template callables and utility functionality that is not needed anymore

--- core/codegen/platform/src/main/java/org/overture/codegen/ir/IRConstants.java
+++ core/codegen/platform/src/main/java/org/overture/codegen/ir/IRConstants.java
@@ -32,16 +32,9 @@ public class IRConstants
 	public static final String CONSTRUCTOR_FORMAL_PREFIX = "_";
 
 	public static final String QUOTES_INTERFACE_NAME = "Quotes";
-
-	public static final String IO_CLASS_NAME = "IO";
-	public static final String MATH_CLASS_NAME = "MATH";
-	public static final String UTIL_CLASS_NAME = "VDMUtil";
 	
 	public static final String TEST_CASE = "TestCase";
 	public static final String TEST_CASE_RUN_FULL_SUITE = "runFullSuite";
-
-	public static final String[] UTIL_NAMES = { IO_CLASS_NAME, MATH_CLASS_NAME,
-			UTIL_CLASS_NAME };
 
 	public static final String[] CLASS_NAMES_USED_IN_VDM = { "CSV", "IO",
 			"MATH", "VDMUnit", "Throwable", "Error", "AssertionFailedError",
