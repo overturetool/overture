e9a0c2d76fec16a53ee5da94521132de10416d8d
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2016-03-10 15:29:28 +0100

    Update JML generator result files

--- core/codegen/vdm2jml/src/test/resources/dynamic_analysis/namedtypeinv/NamedTypeInvValues/Entry.java
+++ core/codegen/vdm2jml/src/test/resources/dynamic_analysis/namedtypeinv/NamedTypeInvValues/Entry.java
@@ -9,10 +9,10 @@ import java.util.*;
 //@ nullable_by_default
 @SuppressWarnings("all")
 final public class Entry {
-    //@ public static invariant (((Utils.is_char(fOk) && inv_Entry_C(fOk)) || (Utils.is_nat(fOk) && inv_Entry_N(fOk))) && inv_Entry_CN(fOk));
+    //@ public static invariant ((fOk == null) || ((Utils.is_char(fOk) && inv_Entry_C(fOk)) || (Utils.is_nat(fOk) && inv_Entry_N(fOk))) && inv_Entry_CN(fOk));
     public static final Object fOk = 'a';
 
-    //@ public static invariant (((Utils.is_char(fBreak) && inv_Entry_C(fBreak)) || (Utils.is_nat(fBreak) && inv_Entry_N(fBreak))) && inv_Entry_CN(fBreak));
+    //@ public static invariant ((fBreak == null) || ((Utils.is_char(fBreak) && inv_Entry_C(fBreak)) || (Utils.is_nat(fBreak) && inv_Entry_N(fBreak))) && inv_Entry_CN(fBreak));
     public static final Object fBreak = 'b';
 
     /*@ public ghost static boolean invChecksOn = true; @*/
