2a9c15b52a492c450d6fd317ee0e55f3b9f8025b
Miran <miranh89@gmail.com>
2016-03-13 17:55:37 +0000

    Added templates for the Java RMI code generator

---
+++ core/codegen/rt2rmi/src/main/java/org/overture/codegen/rt2rmi/trans/FunctionToRemoteTrans.java
@@ -0,0 +1,30 @@
+package org.overture.codegen.rt2rmi.trans;
+
+import org.overture.codegen.ir.analysis.AnalysisException;
+import org.overture.codegen.ir.analysis.DepthFirstAnalysisAdaptor;
+import org.overture.codegen.ir.declarations.AMethodDeclIR;
+import org.overture.codegen.ir.expressions.AExplicitVarExpIR;
+import org.overture.codegen.ir.expressions.ANewExpIR;
+import org.overture.codegen.ir.types.AClassTypeIR;
+import org.overture.codegen.ir.types.AExternalTypeIR;
+import org.overture.codegen.ir.IRInfo;
+
+public class FunctionToRemoteTrans extends DepthFirstAnalysisAdaptor
+{
+	
+	private String systemClassName;
+	private IRInfo info;
+	
+	public FunctionToRemoteTrans(String systemClassName, IRInfo info) {
+		this.systemClassName=systemClassName;
+		this.info=info;
+	}
+	
+	@Override
+	public void caseAMethodDeclIR(AMethodDeclIR node) throws AnalysisException {
+		// TODO Auto-generated method stub
+		AExternalTypeIR runtimeExpType = new AExternalTypeIR();
+		runtimeExpType.setName("RemoteException");
+		node.getRaises().add(runtimeExpType);
+	}
+}
