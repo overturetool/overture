f18919f845a6bee8d60a36d95f49d02a1ba41893
Luis Diogo Couto <ldcouto@gmail.com>
2016-05-22 21:09:35 +0100

    Update Test Result class so it invokes assertEquals and reports full test information.

--- core/testing/framework/src/main/java/org/overture/core/tests/AbsResultTest.java
+++ core/testing/framework/src/main/java/org/overture/core/tests/AbsResultTest.java
@@ -30,6 +30,7 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.overture.parser.lex.LexException;
 import org.overture.parser.syntax.ParserException;
 
@@ -40,151 +41,182 @@ import com.google.gson.reflect.TypeToken;
  * Top level class for new tests framework. Provides common result handling code to all other test classes. This class
  * should <b>not</b> be subclassed directly. Use one of its existing subclasses instead. Test results are always stored
  * with UTF-8 encoding.
- * 
+ *
+ * @param <R> the (user-provided) type of results this test operates on
+ * @author ldc
  * @see ParamExternalsTest
  * @see ParamFineGrainTest
  * @see ParamStandardTest
- * @author ldc
- * @param <R>
- *            the (user-provided) type of results this test operates on
  */
-public abstract class AbsResultTest<R>
-{
+public abstract class AbsResultTest<R> {
 
-	protected boolean updateResult;
-	protected String resultPath;
-	protected String testName;
+    protected boolean updateResult;
+    protected String resultPath;
+    protected String testName;
 
-	/**
-	 * Deserialize test results. This method is capable of deserializing most results, provided the correct type
-	 * information is provided via getResultType(). If your results are too complex for this method or if you are not
-	 * using JSON to store then, them you must override the entire method.
-	 * 
-	 * @param resultPath
-	 *            the file path to the stored result file
-	 * @return the deserialized stored result
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 */
-	public R deSerializeResult(String resultPath) throws FileNotFoundException,
-			IOException
-	{
-		Gson gson = new Gson();
-		Type resultType = getResultType();
+    /**
+     * Deserialize test results. This method is capable of deserializing most results, provided the correct type
+     * information is provided via getResultType(). If your results are too complex for this method or if you are not
+     * using JSON to store then, them you must override the entire method.
+     *
+     * @param resultPath the file path to the stored result file
+     * @return the deserialized stored result
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    public R deSerializeResult(String resultPath) throws FileNotFoundException,
+            IOException {
+        Gson gson = new Gson();
+        Type resultType = getResultType();
 
-		// check if exists
-		File f = new File(resultPath);
-		if (!f.exists())
-		{
-			f.getParentFile().mkdirs();
-			throw new FileNotFoundException(resultPath);
-		}
+        // check if exists
+        File f = new File(resultPath);
+        if (!f.exists()) {
+            f.getParentFile().mkdirs();
+            throw new FileNotFoundException(resultPath);
+        }
 
-		InputStreamReader reader = new InputStreamReader(new FileInputStream(new File(resultPath)), ParseTcFacade.UTF8);
-		String json = IOUtils.toString(reader);
-		R result = gson.fromJson(json, resultType);
-		return result;
-	}
+        InputStreamReader reader = new InputStreamReader(new FileInputStream(new File(resultPath)), ParseTcFacade.UTF8);
+        String json = IOUtils.toString(reader);
+        R result = gson.fromJson(json, resultType);
+        return result;
+    }
 
-	/**
-	 * Calculate the type of the test result. This method must be overridden to provide the specific result type for
-	 * each test. When doing so, you can use the snippet below (replacing <code>R</code> with the actual type of your
-	 * result). Keep in mind this does not work for wildcards or type parameters. You <b>must</b> declare the actual
-	 * type. <pre>{@code
-	 * Type resultType = new TypeToken< R >() {}.getType(); <br> 
-	 * return resultType; 
-	 * }</pre>
-	 * 
-	 * @see TypeToken
-	 * @return the {@link Type} of the result file
-	 */
-	abstract public Type getResultType();
+    /**
+     * Calculate the type of the test result. This method must be overridden to provide the specific result type for
+     * each test. When doing so, you can use the snippet below (replacing <code>R</code> with the actual type of your
+     * result). Keep in mind this does not work for wildcards or type parameters. You <b>must</b> declare the actual
+     * type. <pre>{@code
+     * Type resultType = new TypeToken< R >() {}.getType(); <br>
+     * return resultType;
+     * }</pre>
+     *
+     * @return the {@link Type} of the result file
+     * @see TypeToken
+     */
+    abstract public Type getResultType();
 
-	/**
-	 * Return the Java System property to update this set of tests. Should have the following naming scheme:
-	 * <code>tests.update.[module].[testId]</code>. <br>
-	 * <br>
-	 * The test ID <b>must</b> be unique to each test class. Module is just there to avoid name clashes so the name of
-	 * the module is enough.
-	 * 
-	 * @return
-	 */
-	protected abstract String getUpdatePropertyString();
+    /**
+     * Return the Java System property to update this set of tests. Should have the following naming scheme:
+     * <code>tests.update.[maven-module].[test-class].[testcase/input-file]>. <br>
+     * <br>
+     * The test ID <b>must</b> be unique to each test.
+     * the module is enough.
+     *
+     * @return the test Update Property, as a {@link String}
+     */
+    protected abstract String getUpdatePropertyString();
 
-	/**
-	 * Returns a message on how to update test results. Should be used in {@link #compareResults(Object, Object)}. * @return
-	 * the result update message
-	 */
-	protected String getTestResultUpdateMessage()
-	{
-		StringBuilder sb = new StringBuilder();
-		sb.append("Use -D\"");
-		sb.append(getUpdatePropertyString());
-		sb.append("\" to update the result file");
+    /**
+     * Returns a message on how to update test results. Should be used in {@link #compareResults(Object, Object)}. * @return
+     * the result update message.
+     * <p/>>
+     * This method is deprecated. Use testInfo instead.
+     */
+    @Deprecated
+    protected String getTestResultUpdateMessage() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Use -D\"");
+        sb.append(getUpdatePropertyString());
+        sb.append("\" to update the result file");
 
-		return sb.toString();
-	}
+        return sb.toString();
+    }
 
-	/**
-	 * Update the result file for this test. Result serialization is done with JSON and this should adequate for most
-	 * users. If you need an alternative format, you may override this method.
-	 * 
-	 * @param actual
-	 *            the new result to be saved
-	 * @throws ParserException
-	 * @throws LexException
-	 * @throws IOException
-	 */
-	protected void testUpdate(R actual) throws ParserException, LexException,
-			IOException
-	{
-		Gson gson = new Gson();
-		String json = gson.toJson(actual);
+    /**
+     * Update the result file for this test. Result serialization is done with JSON and this should adequate for most
+     * users. If you need an alternative format, you may override this method.
+     *
+     * @param actual the new result to be saved
+     * @throws ParserException
+     * @throws LexException
+     * @throws IOException
+     */
+    protected void testUpdate(R actual) throws ParserException, LexException,
+            IOException {
+        Gson gson = new Gson();
+        String json = gson.toJson(actual);
 
-		// Make sure file can be created
-		File f = new File(resultPath);
-		if (!f.exists())
-		{
-			f.getParentFile().mkdirs();
-		}
+        // Make sure file can be created
+        File f = new File(resultPath);
+        if (!f.exists()) {
+            f.getParentFile().mkdirs();
+        }
 
-		IOUtils.write(json, new FileOutputStream(resultPath), ParseTcFacade.UTF8);
-	}
+        IOUtils.write(json, new FileOutputStream(resultPath), ParseTcFacade.UTF8);
+    }
 
-	/**
-	 * Check if test running in result update mode. This is done by consulting the update property as returned by {
-	 * {@link #getUpdatePropertyString()}.
-	 * 
-	 * @return true if test is running in update mode. False otherwise
-	 */
-	protected boolean updateCheck()
-	{
-		String update_results_property = getUpdatePropertyString();
+    /**
+     * Computes an information message to be used with test failures. This message reports
+     * on:
+     * <li>Test Input Source path</li>
+     * <li>Result File path</li>
+     * <li>Test Override property</li>
+     *
+     * @return the information message, as a {@link String}
+     */
+    protected String testInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[Input: ");
+        sb.append(getInputPath());
+        sb.append(", Result:");
+        sb.append(resultPath);
+        sb.append(", Update Property:");
+        sb.append(getUpdatePropertyString());
+        sb.append(".");
+        sb.append(testName);
+        sb.append("]");
+        return sb.toString();
+    }
 
-		// check update this test
-		if (System.getProperty(update_results_property + "." + testName) != null)
-		{
-			return true;
-		}
+    /**
+     * Returns the path to the input used to run this test. This is left up to individual subclasses since
+     * there are various ways of grabbing inputs.
+     * method.
+     *
+     * @return path to the input file, as a String.
+     */
+    protected abstract String getInputPath();
 
-		// check update all
-		if (System.getProperty(update_results_property) != null)
-		{
-			return true;
-		}
-		return false;
-	}
+    /**
+     * Check if test running in result update mode. This is done by consulting the update property as returned by {
+     * {@link #getUpdatePropertyString()}.
+     *
+     * @return True if test is running in update mode. False otherwise
+     */
+    protected boolean updateCheck() {
+        String update_results_property = getUpdatePropertyString();
 
-	/**
-	 * Compare output of the processed model with previously stored result. This method must be overridden to implement
-	 * result comparison behavior. Don't forget to assert something! In case of test failures, use {@link #getResultType()}
-	 * to tell the tester how to update the result file. 
-	 * 
-	 * @param actual
-	 *            the processed model
-	 * @param expected
-	 *            the stored result
-	 */
-	public abstract void compareResults(R actual, R expected);
+        // check update this test
+        if (System.getProperty(update_results_property + "." + testName) != null) {
+            return true;
+        }
+
+        // check update all
+        if (System.getProperty(update_results_property) != null) {
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * This is the first method to be invoked in the test case. It provides a convenient place to place
+     * {@link org.junit.Assume} assumptions.
+     */
+    protected void checkAssumptions() {
+    }
+
+    /**
+     * Compare output of the processed model with previously stored result. This method may be overridden to implement
+     * more sophisticated result comparison behavior. By default, it applies assertEquals. If you override
+     * this method, please call {@link #testInfo()} in the failure message.
+     *
+     * @param actual   the result processed model
+     * @param expected the stored result
+     */
+    public void compareResults(R actual, R expected){
+        Assert.assertEquals(this.testInfo(),actual,expected);
+    }
 
 }
