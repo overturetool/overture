b5e13210e0391c87f85c19b9f74d6737abc2fc48
Kenneth Lausdahl <lausdahl@eng.au.dk>
2016-05-31 01:33:44 +0200

    changed commandline into proper artifact that can be used as dependency

---
+++ core/commandline/src/main/resources/cmd/overture.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+REM
+REM Author: Rasmus Lauritsen, Joey Coleman
+REM Created: 2013-03-20
+REM 
+REM Run the Overture commandline tool
+REM
+
+set BASE=%~dp0
+set VERSION=${project.version}
+set NAME=${project.artifactId}
+set JAR=%BASE%\%NAME%-%VERSION%-jar-with-dependencies.jar
+
+IF NOT EXIST %JAR% GOTO ERR_NOJAR
+
+java -jar %JAR% %*
+GOTO END
+
+:ERR_NOJAR
+echo Executable jar: %JAR% does not exist, please ensure it is in the same directory as this command.
+
+:END
