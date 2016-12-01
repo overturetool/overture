93bd331d89bb1cb6f80dcdef9fb74db150d09821
Magnus Pedersen <louvmand@gmail.com>
2016-12-01 16:11:14 +0100

    found line number and line offset

--- ide/ui/src/main/java/org/overture/ide/ui/refactoringCommand/RefactoringCommand.java
+++ ide/ui/src/main/java/org/overture/ide/ui/refactoringCommand/RefactoringCommand.java
@@ -3,15 +3,74 @@ package org.overture.ide.ui.refactoringCommand;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.JFaceTextUtil;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 public class RefactoringCommand  extends AbstractHandler{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
-		System.out.println("TEST WORKS");
-		// TODO Auto-generated method stub
+		IWorkbench workbench = PlatformUI.getWorkbench();
+		IWorkbenchWindow window = 
+		        workbench == null ? null : workbench.getActiveWorkbenchWindow();
+		IWorkbenchPage activePage = 
+		        window == null ? null : window.getActivePage();
+
+		IEditorPart editorPart = 
+		        activePage == null ? null : activePage.getActiveEditor();
+		IEditorInput input = 
+				editorPart == null ? null : editorPart.getEditorInput();
+		IPath path = input instanceof FileEditorInput 
+		        ? ((FileEditorInput)input).getPath()
+		        : null;
+		if (path != null)
+		{
+		    // Do something with path.
+		}
+		
+		ITextEditor editor = (ITextEditor) editorPart.getAdapter(ITextEditor.class);
+		IDocumentProvider provider = editor.getDocumentProvider();
+		IDocument document = provider.getDocument(editorPart
+		        .getEditorInput());
+		ITextSelection textSelection = (ITextSelection) editorPart
+		        .getSite().getSelectionProvider().getSelection();
+		
+		int caretOffset = textSelection.getOffset();
+		
+		try {
+			if(document != null && caretOffset != -1){
+				int lineNumber = document.getLineOfOffset(caretOffset); // one off need +1
+				IRegion lineInfo = document.getLineInformation(lineNumber);
+
+				int lineOffset = (caretOffset - lineInfo.getOffset()) + 1;
+				System.out.println("line nr: " + (lineNumber + 1) + " offset: " + lineOffset);
+			} else{
+				System.out.println("error in line");
+			}
+		} catch (BadLocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	
+		
 		return null;
 	}
-
 }
