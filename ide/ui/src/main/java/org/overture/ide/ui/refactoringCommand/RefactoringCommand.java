a25f2727527345d54d37294d4a043a3a02b7a56a
Magnus Pedersen <louvmand@gmail.com>
2016-12-01 14:50:27 +0100

    added command and keybinding, for refactoring

---
+++ ide/ui/src/main/java/org/overture/ide/ui/refactoringCommand/RefactoringCommand.java
@@ -0,0 +1,17 @@
+package org.overture.ide.ui.refactoringCommand;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class RefactoringCommand  extends AbstractHandler{
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		System.out.println("TEST WORKS");
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
