c6147c42f25da024b7d6ec2286dfe798afdd75e0
Kenneth Lausdahl <kgl@mjolner.dk>
2018-02-06 14:23:45 +0100

    added crc check to detect input specification file change and only re-generate if inputs change by content or count

--- core/codegen/codegen-maven-plugin/src/main/java/org/overture/codegen/mojocg/Vdm2JavaMojo.java
+++ core/codegen/codegen-maven-plugin/src/main/java/org/overture/codegen/mojocg/Vdm2JavaMojo.java
@@ -1,12 +1,9 @@
 package org.overture.codegen.mojocg;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
-
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.overture.ast.analysis.AnalysisException;
@@ -30,266 +27,293 @@ import org.overture.config.Settings;
 import org.overture.typechecker.util.TypeCheckerUtil;
 import org.overture.typechecker.util.TypeCheckerUtil.TypeCheckResult;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 /**
  * VDM-to-Java code generator mojo
- * 
+ *
  * @goal generate
  * @phase generate-sources
  * @requiresDependencyResolution compile
  */
-public class Vdm2JavaMojo extends Vdm2JavaBaseMojo
-{
-	public static final String VDM_PP = "pp";
-	public static final String VDM_SL = "sl";
-	public static final String VDM_RT = "rt";
+public class Vdm2JavaMojo extends Vdm2JavaBaseMojo {
+    public static final String VDM_PP = "pp";
+    public static final String VDM_SL = "sl";
+    public static final String VDM_RT = "rt";
 
-	public static final String VDM_10 = "vdm10";
-	public static final String VDM_CLASSIC = "classic";
+    public static final String VDM_10 = "vdm10";
+    public static final String VDM_CLASSIC = "classic";
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException
-	{
-		MsgPrinter.setPrinter(new AbstractPrinter() {
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        MsgPrinter.setPrinter(new AbstractPrinter() {
 
-			@Override
-			public void println(String msg) {
+            @Override
+            public void println(String msg) {
 
-				print(msg);
-			}
+                print(msg);
+            }
 
-			@Override
-			public void print(String msg) {
+            @Override
+            public void print(String msg) {
 
-				if (!silent) {
-					getLog().info(msg);
-				}
-			}
+                if (!silent) {
+                    getLog().info(msg);
+                }
+            }
 
-			@Override
-			public void errorln(String msg) {
+            @Override
+            public void errorln(String msg) {
 
-				error(msg);
-			}
+                error(msg);
+            }
 
-			@Override
-			public void error(String msg) {
+            @Override
+            public void error(String msg) {
 
-				getLog().error(msg);
-			}
-		});
-		
-		if (!verbose)
-		{
-			MsgPrinter.getPrinter().setSilent(true);
-		}
+                getLog().error(msg);
+            }
+        });
 
-		if (outputDirectory != null && outputDirectory.exists())
-		{
-			getLog().info(String.format("Skipping Java code generation. Output folder '%s' already exists.", outputDirectory.getAbsolutePath()));
-			return;
-		}
+        if (!verbose) {
+            MsgPrinter.getPrinter().setSilent(true);
+        }
 
-		getLog().info("Starting the VDM-to-Java code generator...");
+        if (outputDirectory != null && outputDirectory.exists()) {
+            getLog().debug("Deleting Java code generation output folder: " + outputDirectory.getAbsolutePath());
+            try {
+                FileUtils.deleteDirectory(outputDirectory);
+            } catch (IOException e) {
+                getLog().error(e);
+                throw new MojoFailureException("Failed to delete Java Generation output folder: " + outputDirectory.getAbsolutePath());
 
-		// Let's make sure that maven knows to look in the output directory
-		project.addCompileSourceRoot(outputDirectory.getPath());
+            }
+        }
 
-		IRSettings irSettings = new IRSettings();
-		irSettings.setCharSeqAsString(true);
-		irSettings.setGeneratePreConds(false);
-		irSettings.setGeneratePreCondChecks(false);
-		irSettings.setGeneratePostConds(false);
-		irSettings.setGeneratePostCondChecks(false);
-		irSettings.setGenerateConc(genConcurrency);
+        getLog().info("Starting the VDM-to-Java code generator...");
 
-		JavaSettings javaSettings = new JavaSettings();
-		javaSettings.setDisableCloning(false);
-		javaSettings.setFormatCode(formatCode);
-		javaSettings.setGenJUnit4tests(genJUnit4Tests);
-		javaSettings.setPrintVdmLocations(printVdmLocations);
-		javaSettings.setGenSystemClass(genSystemClass);
-		
-		if(modulesToSkip != null && !modulesToSkip.isEmpty())
-		{
-			javaSettings.setModulesToSkip(modulesToSkip);
-		}
+        // Let's make sure that maven knows to look in the output directory
+        project.addCompileSourceRoot(outputDirectory.getPath());
 
-		if (JavaCodeGenUtil.isValidJavaPackage(packageName))
-		{
-			javaSettings.setJavaRootPackage(packageName);
-		} else
-		{
-			if (packageName != null)
-			{
-				getLog().warn(String.format("%s is not a valid Java package.", packageName));
-			}
-		}
+        IRSettings irSettings = new IRSettings();
+        irSettings.setCharSeqAsString(true);
+        irSettings.setGeneratePreConds(false);
+        irSettings.setGeneratePreCondChecks(false);
+        irSettings.setGeneratePostConds(false);
+        irSettings.setGeneratePostCondChecks(false);
+        irSettings.setGenerateConc(genConcurrency);
 
-		List<File> files = new LinkedList<File>();
+        JavaSettings javaSettings = new JavaSettings();
+        javaSettings.setDisableCloning(false);
+        javaSettings.setFormatCode(formatCode);
+        javaSettings.setGenJUnit4tests(genJUnit4Tests);
+        javaSettings.setPrintVdmLocations(printVdmLocations);
+        javaSettings.setGenSystemClass(genSystemClass);
 
-		if (specificationDir != null && specificationDir.isDirectory())
-		{
-			findVdmSources(files, specificationDir);
-		}
+        if (modulesToSkip != null && !modulesToSkip.isEmpty()) {
+            javaSettings.setModulesToSkip(modulesToSkip);
+        }
 
-		if (files == null || files.isEmpty())
-		{
-			throw new MojoFailureException("Nothing to generate, no specification files.");
-		}
+        if (JavaCodeGenUtil.isValidJavaPackage(packageName)) {
+            javaSettings.setJavaRootPackage(packageName);
+        } else {
+            if (packageName != null) {
+                getLog().warn(String.format("%s is not a valid Java package.", packageName));
+            }
+        }
 
-		outputDirectory.mkdirs();
+        List<File> files = new LinkedList<File>();
 
-		List<File> tmp = new Vector<File>();
-		tmp.addAll(files);
+        if (specificationDir != null && specificationDir.isDirectory()) {
+            findVdmSources(files, specificationDir);
+        }
 
-		if (release.equals(VDM_10))
-		{
-			Settings.release = Release.VDM_10;
-		} else if (release.equals(VDM_CLASSIC))
-		{
-			Settings.release = Release.CLASSIC;
-		} else
-		{
-			throw new MojoFailureException(String.format("Expected VDM version to be '%s' or '%s'", VDM_10, VDM_CLASSIC));
-		}
+        if (files == null || files.isEmpty()) {
+            throw new MojoFailureException("Nothing to generate, no specification files.");
+        }
 
-		JavaCodeGen javaCodeGen = new JavaCodeGen();
-		javaCodeGen.setSettings(irSettings);
-		javaCodeGen.setJavaSettings(javaSettings);
+        if (!isReGenerationRequired(files)) {
+            getLog().info("No change in specification files. Skipping generating.");
+            return;
+        }
 
-		addDelegateTrans(javaCodeGen);
+        outputDirectory.mkdirs();
 
-		GeneratedData genData = null;
+        List<File> tmp = new Vector<File>();
+        tmp.addAll(files);
 
-		if (dialect.equals(VDM_PP))
-		{
-			Settings.dialect = Dialect.VDM_PP;
-			TypeCheckResult<List<SClassDefinition>> tcResult = TypeCheckerUtil.typeCheckPp(files);
+        if (release.equals(VDM_10)) {
+            Settings.release = Release.VDM_10;
+        } else if (release.equals(VDM_CLASSIC)) {
+            Settings.release = Release.CLASSIC;
+        } else {
+            throw new MojoFailureException(String.format("Expected VDM version to be '%s' or '%s'", VDM_10, VDM_CLASSIC));
+        }
 
-			validateTcResult(tcResult);
+        JavaCodeGen javaCodeGen = new JavaCodeGen();
+        javaCodeGen.setSettings(irSettings);
+        javaCodeGen.setJavaSettings(javaSettings);
 
-			try
-			{
-				genData = javaCodeGen.generate(CodeGenBase.getNodes(tcResult.result));
-			} catch (AnalysisException e)
-			{
-				e.printStackTrace();
-				throw new MojoExecutionException("Got unexpected error when trying to code generate VDM++ model: "
-						+ e.getMessage());
-			}
-		} else if (dialect.equals(VDM_SL))
-		{
-			Settings.dialect = Dialect.VDM_SL;
-			TypeCheckResult<List<AModuleModules>> tcResult = TypeCheckerUtil.typeCheckSl(files);
+        addDelegateTrans(javaCodeGen);
 
-			validateTcResult(tcResult);
+        GeneratedData genData = null;
 
-			try
-			{
-				genData = javaCodeGen.generate(CodeGenBase.getNodes(tcResult.result));
+        if (dialect.equals(VDM_PP)) {
+            Settings.dialect = Dialect.VDM_PP;
+            TypeCheckResult<List<SClassDefinition>> tcResult = TypeCheckerUtil.typeCheckPp(files);
 
-			} catch (AnalysisException e)
-			{
-				e.printStackTrace();
-				throw new MojoExecutionException("Got unexpected error when trying to code generate VDM-SL model: "
-						+ e.getMessage());
-			}
+            validateTcResult(tcResult);
 
-		} else if (dialect.equals(VDM_RT))
-		{
-			try
-			{
-				Settings.dialect = Dialect.VDM_RT;
-				TypeCheckResult<List<SClassDefinition>> tcResult = TypeCheckerUtil.typeCheckRt(files);
+            try {
+                genData = javaCodeGen.generate(CodeGenBase.getNodes(tcResult.result));
+            } catch (AnalysisException e) {
+                e.printStackTrace();
+                throw new MojoExecutionException("Got unexpected error when trying to code generate VDM++ model: "
+                        + e.getMessage());
+            }
+        } else if (dialect.equals(VDM_SL)) {
+            Settings.dialect = Dialect.VDM_SL;
+            TypeCheckResult<List<AModuleModules>> tcResult = TypeCheckerUtil.typeCheckSl(files);
 
-				validateTcResult(tcResult);
+            validateTcResult(tcResult);
 
-				javaSettings.setMakeClassesSerializable(true);
+            try {
+                genData = javaCodeGen.generate(CodeGenBase.getNodes(tcResult.result));
 
-				genData = javaCodeGen.generate(CodeGenBase.getNodes(tcResult.result));
+            } catch (AnalysisException e) {
+                e.printStackTrace();
+                throw new MojoExecutionException("Got unexpected error when trying to code generate VDM-SL model: "
+                        + e.getMessage());
+            }
 
-			} catch (AnalysisException e)
-			{
-				e.printStackTrace();
-				throw new MojoExecutionException("Got unexpected error when trying to code generate VDM-RT model: "
-						+ e.getMessage());
-			}
-		} else
-		{
-			throw new MojoExecutionException(String.format("Expected dialect to be '%s' or '%s'", VDM_SL, VDM_PP));
-		}
+        } else if (dialect.equals(VDM_RT)) {
+            try {
+                Settings.dialect = Dialect.VDM_RT;
+                TypeCheckResult<List<SClassDefinition>> tcResult = TypeCheckerUtil.typeCheckRt(files);
 
-		if (genData != null)
-		{
-			JavaCodeGenMain.processData(false, outputDirectory, javaCodeGen, genData, separateTestCode);
+                validateTcResult(tcResult);
 
-			if (genData.hasErrors())
-			{
-				throw new MojoExecutionException("Could not code generate model.");
-			}
-		}
+                javaSettings.setMakeClassesSerializable(true);
 
-		getLog().info("Code generation completed.");
-	}
+                genData = javaCodeGen.generate(CodeGenBase.getNodes(tcResult.result));
 
-	private Map<String, String> buidDelegateMap()
-	{
-		if (delegates == null)
-		{
-			return new HashMap<>();
-		}
+            } catch (AnalysisException e) {
+                e.printStackTrace();
+                throw new MojoExecutionException("Got unexpected error when trying to code generate VDM-RT model: "
+                        + e.getMessage());
+            }
+        } else {
+            throw new MojoExecutionException(String.format("Expected dialect to be '%s' or '%s'", VDM_SL, VDM_PP));
+        }
 
-		Map<String, String> map = new HashMap<String, String>();
-		for (String key : delegates.stringPropertyNames())
-		{
-			map.put(key, delegates.getProperty(key));
-		}
+        if (genData != null) {
+            JavaCodeGenMain.processData(false, outputDirectory, javaCodeGen, genData, separateTestCode);
 
-		return map;
-	}
+            if (genData.hasErrors()) {
+                throw new MojoExecutionException("Could not code generate model.");
+            }
+        }
 
-	private void addDelegateTrans(JavaCodeGen javaCodeGen)
-	{
-		if (delegates != null && !delegates.isEmpty())
-		{
-			Map<String, String> delegateMap = buidDelegateMap();
+        getLog().info("Code generation completed.");
+    }
 
-			getLog().debug("Found following bridge/delegate pairs:");
+    private boolean isReGenerationRequired(List<File> files) throws MojoFailureException {
+        File crcFile = new File(getProjectOutputDirectory(), "javagen.crc");
 
-			for (String entry : delegateMap.keySet())
-			{
-				getLog().debug("  Bridge class: " + entry + ". Delegate class: "
-						+ delegateMap.get(entry));
-			}
+        Set<String> crcs = new HashSet<>();
+        if (crcFile.exists()) {
+            try {
+                for (Object l : FileUtils.readLines(crcFile, "UTF-8")) {
+                    String line = l + "";
+                    if (!line.trim().isEmpty()) {
+                        crcs.add(line);
+                    }
+                }
+            } catch (IOException e) {
+                getLog().error(e);
+                throw new MojoFailureException("Failed to read CRC lines for Java Generation from: " + crcFile.getAbsolutePath());
+            }
+        }
 
-			javaCodeGen.getTransSeries().getSeries().add(new DelegateTrans(delegateMap, javaCodeGen.getTransAssistant(), getLog()));
-		}
-	}
 
-	private void findVdmSources(List<File> files, File specificationRoot)
-	{
-		for (File f : GeneralUtils.getFilesRecursively(specificationRoot))
-		{
-			if (GeneralCodeGenUtils.isVdmSourceFile(f))
-			{
-				files.add(f);
-			}
-		}
-	}
+        Set<String> inputCrcs = new HashSet<>();
+        for (File f : files) {
+            HashCode hc = null;
+            try {
+                hc = Files.asByteSource(f).hash(Hashing.sha1());
+                String crc = "SHA-1: " + hc.toString();
+                inputCrcs.add(crc);
+                getLog().debug("Calculating CRC: " + crc + " " + f.getPath());
+            } catch (IOException e) {
+                getLog().error(e);
+                throw new MojoFailureException("Failed to calculate CRC for Java Generation from: " + f.getAbsolutePath());
+            }
+        }
 
-	private void validateTcResult(TypeCheckResult<?> tcResult)
-			throws MojoExecutionException
-	{
-		if (!tcResult.parserResult.errors.isEmpty()
-				|| !tcResult.errors.isEmpty())
-		{
-			String PARSE_TYPE_CHECK_ERR_MSG = "Could not parse or type check VDM model";
-			getLog().error(PARSE_TYPE_CHECK_ERR_MSG + ":\n"
-					+ GeneralCodeGenUtils.errorStr(tcResult));
+        if (crcs.size() == inputCrcs.size() && crcs.equals(inputCrcs)) {
+            //no changes
+            return false;
+        }
 
-			throw new MojoExecutionException(PARSE_TYPE_CHECK_ERR_MSG);
-		}
+        try {
+            FileUtils.writeLines(crcFile, "UTF-8", inputCrcs, "\n");
+        } catch (IOException e) {
+            getLog().error(e);
+            throw new MojoFailureException("Failed to write CRC lines for Java Generation to: " + crcFile.getAbsolutePath());
+        }
+        return true;
 
-		// No type errors
-	}
+    }
+
+    private Map<String, String> buidDelegateMap() {
+        if (delegates == null) {
+            return new HashMap<>();
+        }
+
+        Map<String, String> map = new HashMap<String, String>();
+        for (String key : delegates.stringPropertyNames()) {
+            map.put(key, delegates.getProperty(key));
+        }
+
+        return map;
+    }
+
+    private void addDelegateTrans(JavaCodeGen javaCodeGen) {
+        if (delegates != null && !delegates.isEmpty()) {
+            Map<String, String> delegateMap = buidDelegateMap();
+
+            getLog().debug("Found following bridge/delegate pairs:");
+
+            for (String entry : delegateMap.keySet()) {
+                getLog().debug("  Bridge class: " + entry + ". Delegate class: "
+                        + delegateMap.get(entry));
+            }
+
+            javaCodeGen.getTransSeries().getSeries().add(new DelegateTrans(delegateMap, javaCodeGen.getTransAssistant(), getLog()));
+        }
+    }
+
+    private void findVdmSources(List<File> files, File specificationRoot) {
+        for (File f : GeneralUtils.getFilesRecursively(specificationRoot)) {
+            if (GeneralCodeGenUtils.isVdmSourceFile(f)) {
+                files.add(f);
+            }
+        }
+    }
+
+    private void validateTcResult(TypeCheckResult<?> tcResult)
+            throws MojoExecutionException {
+        if (!tcResult.parserResult.errors.isEmpty()
+                || !tcResult.errors.isEmpty()) {
+            String PARSE_TYPE_CHECK_ERR_MSG = "Could not parse or type check VDM model";
+            getLog().error(PARSE_TYPE_CHECK_ERR_MSG + ":\n"
+                    + GeneralCodeGenUtils.errorStr(tcResult));
+
+            throw new MojoExecutionException(PARSE_TYPE_CHECK_ERR_MSG);
+        }
+
+        // No type errors
+    }
 }
