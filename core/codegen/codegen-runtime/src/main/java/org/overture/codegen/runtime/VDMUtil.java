589450b832567e4c351fe83d9c14081f5998ac9e
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-03-20 13:45:19 +0100

    Support Java code-generation of VDMUtil`set2seq
    Closes #679

--- core/codegen/codegen-runtime/src/main/java/org/overture/codegen/runtime/VDMUtil.java
+++ core/codegen/codegen-runtime/src/main/java/org/overture/codegen/runtime/VDMUtil.java
@@ -2,6 +2,21 @@ package org.overture.codegen.runtime;
 
 public class VDMUtil
 {
+	@SuppressWarnings("unchecked")
+	public static VDMSeq set2seq(Object obj)
+	{
+		if (!(obj instanceof VDMSet))
+		{
+			throw new IllegalArgumentException("Expected a set but got: " + Utils.toString(obj));
+		}
+		
+		VDMSet set = (VDMSet) obj;
+		
+		VDMSeq seq = SeqUtil.seq();
+		seq.addAll(set);
+		return seq;
+	}
+	
 	public static String val2seq_of_char(Object value)
 	{
 		return Utils.toString(value);
