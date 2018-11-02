02c887cab284aa648c4aa079d372ed149decaaba
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-02 19:46:40 +0100

    Java CG: fix issue with seq-to-string conversion

--- core/codegen/platform/src/main/java/org/overture/codegen/trans/SeqConvTrans.java
+++ core/codegen/platform/src/main/java/org/overture/codegen/trans/SeqConvTrans.java
@@ -1,5 +1,11 @@
 package org.overture.codegen.trans;
 
+import org.apache.log4j.Logger;
+import org.overture.ast.definitions.ARenamedDefinition;
+import org.overture.ast.definitions.PDefinition;
+import org.overture.ast.expressions.AVariableExp;
+import org.overture.ast.node.INode;
+import org.overture.codegen.assistant.AssistantBase;
 import org.overture.codegen.ir.SExpIR;
 import org.overture.codegen.ir.STypeIR;
 import org.overture.codegen.ir.analysis.AnalysisException;
@@ -7,24 +13,21 @@ import org.overture.codegen.ir.analysis.DepthFirstAnalysisAdaptor;
 import org.overture.codegen.ir.declarations.AFieldDeclIR;
 import org.overture.codegen.ir.declarations.AMethodDeclIR;
 import org.overture.codegen.ir.declarations.AVarDeclIR;
-import org.overture.codegen.ir.expressions.AEqualsBinaryExpIR;
-import org.overture.codegen.ir.expressions.AFieldNumberExpIR;
-import org.overture.codegen.ir.expressions.ANotEqualsBinaryExpIR;
-import org.overture.codegen.ir.expressions.ASeqToStringUnaryExpIR;
-import org.overture.codegen.ir.expressions.AStringToSeqUnaryExpIR;
-import org.overture.codegen.ir.expressions.SBinaryExpIR;
+import org.overture.codegen.ir.declarations.SClassDeclIR;
+import org.overture.codegen.ir.expressions.*;
 import org.overture.codegen.ir.statements.AAssignToExpStmIR;
 import org.overture.codegen.ir.statements.AForAllStmIR;
 import org.overture.codegen.ir.statements.AReturnStmIR;
-import org.overture.codegen.ir.types.ACharBasicTypeIR;
-import org.overture.codegen.ir.types.AMethodTypeIR;
-import org.overture.codegen.ir.types.ASeqSeqTypeIR;
-import org.overture.codegen.ir.types.AStringTypeIR;
-import org.overture.codegen.ir.types.SSeqTypeIR;
+import org.overture.codegen.ir.types.*;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class SeqConvTrans extends DepthFirstAnalysisAdaptor
 {
+	protected Logger log = Logger.getLogger(this.getClass().getName());
+
 	private TransAssistantIR transformationAssistant;
 
 	public SeqConvTrans(TransAssistantIR transformationAssistant)
@@ -131,6 +134,140 @@ public class SeqConvTrans extends DepthFirstAnalysisAdaptor
 		if (exp != null)
 		{
 			exp.apply(this);
+		}
+	}
+
+	@Override
+	public void caseAApplyExpIR(AApplyExpIR node) throws AnalysisException {
+		super.caseAApplyExpIR(node);
+
+		SExpIR root = node.getRoot();
+		STypeIR type = root.getType();
+
+		String moduleName;
+		String fieldName;
+
+		if(root instanceof AMethodInstantiationExpIR)
+		{
+			root = ((AMethodInstantiationExpIR) root).getFunc();
+		}
+
+		if(root instanceof AExplicitVarExpIR)
+		{
+			AExplicitVarExpIR expVar = (AExplicitVarExpIR) root;
+			STypeIR classType = expVar.getClassType();
+
+			if(classType instanceof AClassTypeIR)
+			{
+				moduleName = ((AClassTypeIR) classType).getName();
+			}
+			else
+			{
+				return;
+			}
+			fieldName = expVar.getName();
+		}
+		else if(root instanceof AIdentifierVarExpIR)
+		{
+			fieldName = ((AIdentifierVarExpIR) root).getName();
+
+			INode var = AssistantBase.getVdmNode(node);
+
+			if(var instanceof AVariableExp) {
+				PDefinition def = ((AVariableExp) var).getVardef();
+
+				if (def instanceof ARenamedDefinition) {
+					ARenamedDefinition renamedDef = (ARenamedDefinition) def;
+
+					moduleName = renamedDef.getDef().getName().getModule();
+				}
+				else
+				{
+					SClassDeclIR encClass = node.getAncestor(SClassDeclIR.class);
+					if(encClass != null)
+					{
+						moduleName = encClass.getName();
+					}
+					else
+					{
+						log.warn("Could not find enclosing class for " + node);
+						return;
+					}
+
+				}
+			}
+			else
+			{
+				SClassDeclIR encClass = node.getAncestor(SClassDeclIR.class);
+				if(encClass != null)
+				{
+					moduleName = encClass.getName();
+				}
+				else
+				{
+					log.warn("Could not find enclosing class for " + node);
+					return;
+				}
+			}
+		}
+		else if(root instanceof AFieldExpIR)
+		{
+			AFieldExpIR fieldExp = (AFieldExpIR) root;
+			fieldName = fieldExp.getMemberName();
+
+			STypeIR classType = ((AFieldExpIR) root).getObject().getType();
+
+			if(classType instanceof AClassTypeIR)
+			{
+				moduleName = ((AClassTypeIR) classType).getName();
+			}
+			else
+			{
+				return;
+			}
+		}
+		else
+		{
+			return;
+		}
+
+		if(!(type instanceof AMethodTypeIR))
+		{
+			return;
+		}
+
+		AMethodTypeIR rootType = (AMethodTypeIR) type;
+
+		// All arguments -- except type arguments (polymorphic types)
+		List<SExpIR> args = new LinkedList<>();
+		for(int i = 0; i < rootType.getParams().size(); i++)
+		{
+			args.add(node.getArgs().get(i));
+		}
+
+		AMethodTypeIR methodType = transformationAssistant.getInfo().getTypeAssistant().
+				getMethodType(transformationAssistant.getInfo(), moduleName, fieldName, args);
+
+		if(methodType == null)
+		{
+			return;
+		}
+
+		for(int i = 0; i < methodType.getParams().size(); i ++)
+		{
+			STypeIR paramType = methodType.getParams().get(i);
+			STypeIR argType = node.getArgs().get(i).getType();
+
+			if (paramType instanceof AStringTypeIR
+					&& argType instanceof SSeqTypeIR)
+			{
+				correctExpToString(node.getArgs().get(i));
+
+			} else if (paramType instanceof SSeqTypeIR
+					&& argType instanceof AStringTypeIR)
+			{
+				correctExpToSeq(node.getArgs().get(i), paramType);
+			}
 		}
 	}
 
