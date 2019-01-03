bbbc49507212d9c1a2b5d072e6db83d1157b2cd4
nick_battle <nick.battle@gmail.com>
2019-01-03 22:38:03 +0000

    Correct a warning

--- core/parser/src/main/java/org/overture/parser/syntax/DefinitionReader.java
+++ core/parser/src/main/java/org/overture/parser/syntax/DefinitionReader.java
@@ -52,7 +52,6 @@ import org.overture.ast.intf.lex.ILexLocation;
 import org.overture.ast.intf.lex.ILexNameToken;
 import org.overture.ast.intf.lex.ILexToken;
 import org.overture.ast.lex.Dialect;
-import org.overture.ast.lex.LexCommentList;
 import org.overture.ast.lex.LexIdentifierToken;
 import org.overture.ast.lex.LexIntegerToken;
 import org.overture.ast.lex.LexLocation;
