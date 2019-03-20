22aa508134fdf33ef05ff77f8587987f5ba5ee9b
Jamie <j.simm@ncl.ac.uk>
2019-03-20 16:52:15 +0000

    Printing successful Nat1 translation

--- core/codegen/isagen/src/main/java/org/overturetool/cgisa/transformations/IsaBasicTypesConv.java
+++ core/codegen/isagen/src/main/java/org/overturetool/cgisa/transformations/IsaBasicTypesConv.java
@@ -3,7 +3,9 @@ package org.overturetool.cgisa.transformations;
 import org.overture.cgisa.isair.analysis.DepthFirstAnalysisIsaAdaptor;
 import org.overture.codegen.ir.*;
 import org.overture.codegen.ir.declarations.*;
+import org.overture.codegen.ir.types.ABoolBasicTypeIR;
 import org.overture.codegen.ir.types.AIntNumericBasicTypeIR;
+import org.overture.codegen.ir.types.ANat1NumericBasicTypeIR;
 import org.overture.codegen.trans.assistants.TransAssistantIR;
 
 import java.util.Map;
@@ -19,6 +21,7 @@ public class IsaBasicTypesConv extends DepthFirstAnalysisIsaAdaptor {
     private final AModuleDeclIR vdmToolkitModuleIR;
     private final IRInfo info;
     private final static String isa_VDMInt = "isa_VDMInt";
+    private final static String isa_VDMNat1 = "isa_VDMNat1";
 
     public IsaBasicTypesConv(IRInfo info, TransAssistantIR t, AModuleDeclIR vdmToolkitModuleIR) {
         this.t = t;
@@ -48,4 +51,19 @@ public class IsaBasicTypesConv extends DepthFirstAnalysisIsaAdaptor {
         }
 
     }
+    //transform nat1 to isa_VDMNat1
+    public void caseANat1NumericBasicTypeIR(ANat1NumericBasicTypeIR t){
+        if(t.getNamedInvType() == null)
+        {
+            ANat1NumericBasicTypeIR a = new ANat1NumericBasicTypeIR();
+            // Retrieve isa_VDMInt from VDMToolkit
+            ATypeDeclIR isa_td = isaTypeDeclIRMap.get(this.isa_VDMNat1);
+
+            t.setNamedInvType((ANamedTypeDeclIR)isa_td.getDecl().clone());
+            System.out.println(t.getNamedInvType().toString());
+        }
+
+    }
+    
+    
 }
