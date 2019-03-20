76ab06dacdb317573fee79b908e3bfe0ca266939
nick_battle <nick.battle@gmail.com>
2019-03-20 12:43:57 +0000

    Fix comments in Ghost annotation

--- core/annotations/examples/src/main/java/org/overture/annotations/examples/GhostAnnotation.java
+++ core/annotations/examples/src/main/java/org/overture/annotations/examples/GhostAnnotation.java
@@ -4,20 +4,7 @@
  *
  *	Author: Nick Battle
  *
- *	This file is part of VDMJ.
- *
- *	VDMJ is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
- *
- *	VDMJ is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with VDMJ.  If not, see <http://www.gnu.org/licenses/>.
+ *	This file is part of Overture.
  *
  ******************************************************************************/
 
@@ -49,7 +36,7 @@ public class GhostAnnotation extends ASTAnnotationAdapter
 	
 	public boolean typecheckArgs()
 	{
-		return false;		// Ghosts must be ner variables and so non-existent anyway
+		return false;		// Ghosts must be new variables and so non-existent anyway
 	}
 
 	/**
